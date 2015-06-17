00e699942f761638fb99015ba0e115e74c6e91d0
Piratdu52 <Piratdu52@users.noreply.github.com>
2015-06-17 23:44:40 +0200

    Implementation of FurnaceConsumFuelEvent
    Actually i can't test this because furnace.getData throw an abstract method error,  so in the state is more like a WIP don't hesitate to provide advice and require modifications.

--- src/main/java/org/spongepowered/common/mixin/core/block/tiles/MixinTileEntityFurnace.java
+++ src/main/java/org/spongepowered/common/mixin/core/block/tiles/MixinTileEntityFurnace.java
@@ -22,23 +22,38 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package org.spongepowered.common.mixin.core.block.tiles;
 
 import static org.spongepowered.api.data.DataQuery.of;
 
+import net.minecraft.tileentity.TileEntityFurnace;
 import org.spongepowered.api.block.tileentity.TileEntityType;
 import org.spongepowered.api.block.tileentity.TileEntityTypes;
 import org.spongepowered.api.block.tileentity.carrier.Furnace;
 import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.manipulator.tileentity.FurnaceData;
+import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.block.tileentity.FurnaceConsumeFuelEvent;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.item.inventory.ItemStackBuilder;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.common.Sponge;
 
 @NonnullByDefault
 @Mixin(net.minecraft.tileentity.TileEntityFurnace.class)
 public abstract class MixinTileEntityFurnace extends MixinTileEntityLockable implements Furnace {
 
     @Shadow private String furnaceCustomName;
+    @Shadow private int currentItemBurnTime;
+    @Shadow private int furnaceBurnTime;
+    @Shadow private net.minecraft.item.ItemStack[] furnaceItemStacks;
 
     @Override
     public TileEntityType getType() {
@@ -56,4 +71,51 @@ public abstract class MixinTileEntityFurnace extends MixinTileEntityLockable imp
         }
         return container;
     }
+
+    /**
+     * A method to automatically fire FurnaceConsumeFuelEvent
+     *
+     * @param ci  A callback info needed to Inject the method
+     */
+    @Inject(method = "update",at = @At(
+            target = "Lnet/minecraft/tileentity/TileEntityFurnace;getItemBurnTime(Lnet/minecraft/item/ItemStack;)I", value ="INVOKE_ASSIGN"))
+    public void callFurnaceConsumeFuelEvent(CallbackInfo ci)
+    {
+        Furnace furnace=(Furnace) (Object) this;
+        ItemStack fuelStack=(ItemStack)(Object)this.furnaceItemStacks[1];
+        ItemStack burnedItem=fuelStack;
+        burnedItem.setQuantity(1);
+        ItemStack remainingFuel=fuelStack;
+        remainingFuel.setQuantity(remainingFuel.getQuantity()-1);//remove one for present the event like already done
+        if (remainingFuel.getQuantity()<=0) {
+            remainingFuel = null;
+        }
+        int itemBurnTime = TileEntityFurnace.getItemBurnTime((net.minecraft.item.ItemStack) fuelStack);
+        if (itemBurnTime>0){
+            final FurnaceConsumeFuelEvent event = SpongeEventFactory.createFurnaceConsumeFuel(Sponge.getGame(), furnace,
+                    (FurnaceData)  getData(FurnaceData.class)
+                    ,burnedItem, remainingFuel,
+                    null, furnace.getInventory(), furnace.getBlock());
+            if (!Sponge.getGame().getEventManager().post(event)) { //if not cancelled
+                this.furnaceBurnTime=this.currentItemBurnTime= TileEntityFurnace.getItemBurnTime((net.minecraft.item.ItemStack) event.getBurnedItem());
+                if(event.getRemainingFuel().isPresent()) {
+                    remainingFuel= event.getRemainingFuel().get();
+                    remainingFuel.setQuantity(remainingFuel.getQuantity()+1); //add one because The mojang class will decrement this
+                }
+                else {
+                    ItemStackBuilder itemStackBuilder= Sponge.getGame().getRegistry().getItemBuilder();
+                    remainingFuel= itemStackBuilder.itemType(ItemTypes.SPONGE).quantity(1).build(); //set  a arbitrary itemStack with quantity=1,
+                                                                                                    // the Mojang class will delete it
+                }
+                this.furnaceItemStacks[1] = (net.minecraft.item.ItemStack) remainingFuel;
+                return;
+            } else {
+                this.furnaceBurnTime=this.currentItemBurnTime=0;
+                return;
+            }
+        }
+        this.furnaceBurnTime=this.currentItemBurnTime= itemBurnTime;
+        return;
+    }
+
 }
