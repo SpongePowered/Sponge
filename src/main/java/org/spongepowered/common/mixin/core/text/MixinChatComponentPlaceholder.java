cfc97e0e3fd6ebc5f73a7caf6ba030c852253ebf
ST-DDT <ST-DDT@gmx.de>
2015-08-10 19:54:34 +0200

    Cleanup

--- src/main/java/org/spongepowered/common/mixin/core/text/MixinChatComponentPlaceholder.java
+++ src/main/java/org/spongepowered/common/mixin/core/text/MixinChatComponentPlaceholder.java
@@ -26,10 +26,9 @@ package org.spongepowered.common.mixin.core.text;
 
 import org.spongepowered.api.text.TextBuilder;
 import org.spongepowered.api.text.Texts;
-import org.spongepowered.asm.mixin.Shadow;
-
-import org.spongepowered.common.text.ChatComponentPlaceholder;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.common.text.ChatComponentPlaceholder;
 
 @Mixin(value = ChatComponentPlaceholder.class, remap = false)
 public abstract class MixinChatComponentPlaceholder extends MixinChatComponentText {
@@ -38,7 +37,7 @@ public abstract class MixinChatComponentPlaceholder extends MixinChatComponentTe
 
     @Override
     protected TextBuilder createBuilder() {
-        return Texts.placeholderBuilder(placeholderKey).content(getUnformattedText());
+        return Texts.placeholderBuilder(this.placeholderKey).content(getUnformattedText());
     }
 
 }
