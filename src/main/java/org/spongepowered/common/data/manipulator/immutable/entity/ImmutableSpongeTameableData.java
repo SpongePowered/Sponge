313936617c4c5c8c5877a26004a94d024449b8eb
Ryan Leach <rleach@rleach.id.au>
2015-08-22 09:36:15 +0930

    Implement TameableData

---
+++ src/main/java/org/spongepowered/common/data/manipulator/immutable/entity/ImmutableSpongeTameableData.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.data.manipulator.immutable.entity;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ComparisonChain;
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.MemoryDataContainer;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableTameableData;
+import org.spongepowered.api.data.manipulator.mutable.entity.TameableData;
+import org.spongepowered.api.data.value.immutable.ImmutableOptionalValue;
+import org.spongepowered.api.data.value.immutable.ImmutableValue;
+import org.spongepowered.common.data.ImmutableDataCachingUtil;
+import org.spongepowered.common.data.manipulator.immutable.common.AbstractImmutableData;
+import org.spongepowered.common.data.manipulator.mutable.entity.SpongeTameableData;
+import org.spongepowered.common.data.value.immutable.ImmutableSpongeOptionalValue;
+import org.spongepowered.common.util.GetterFunction;
+
+import javax.annotation.Nullable;
+import java.util.UUID;
+
+public class ImmutableSpongeTameableData extends AbstractImmutableData<ImmutableTameableData, TameableData> implements ImmutableTameableData {
+    @Nullable private final UUID owner;
+
+    public ImmutableSpongeTameableData(@Nullable UUID owner) {
+        super(ImmutableTameableData.class);
+        this.owner = owner;
+    }
+
+    @Override
+    public ImmutableOptionalValue<UUID> owner() {
+        return ImmutableDataCachingUtil.getValue(ImmutableSpongeOptionalValue.class, Keys.TAMED_OWNER, Optional.fromNullable(this.owner), Optional.<UUID>absent());
+    }
+
+    @Override
+    public ImmutableTameableData copy() {
+        return this; //It's immutable, just return this, UUID is also immutable.
+    }
+
+    @Override
+    public TameableData asMutable() {
+        return new SpongeTameableData(owner);
+    }
+
+    @Override
+    public int compareTo(ImmutableTameableData o) {
+        return ComparisonChain.start()
+                .compare(owner, o.owner().get().orNull())
+                .result();
+    }
+
+    @Override
+    public DataContainer toContainer() {
+        return new MemoryDataContainer()
+                .set(Keys.TAMED_OWNER.getQuery(), this.owner);
+    }
+
+    @Override
+    protected void registerGetters() {
+        registerFieldGetter(Keys.TAMED_OWNER, new GetterFunction<Object>() {
+            @Override
+            public Object get() {
+                return getOwner();
+            }
+        });
+        registerKeyValue(Keys.TAMED_OWNER, new GetterFunction<ImmutableValue<?>>() {
+            @Override
+            public ImmutableValue<?> get() {
+                return owner();
+            }
+        });
+    }
+
+    public Optional<UUID> getOwner() {
+        return Optional.fromNullable(this.owner);
+    }
+}
---
+++ src/main/java/org/spongepowered/common/data/manipulator/mutable/entity/SpongeTameableData.java
@@ -0,0 +1,117 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.data.manipulator.mutable.entity;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ComparisonChain;
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.MemoryDataContainer;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableTameableData;
+import org.spongepowered.api.data.manipulator.mutable.entity.TameableData;
+import org.spongepowered.api.data.value.mutable.OptionalValue;
+import org.spongepowered.api.data.value.mutable.Value;
+import org.spongepowered.common.data.manipulator.immutable.entity.ImmutableSpongeTameableData;
+import org.spongepowered.common.data.manipulator.mutable.common.AbstractData;
+import org.spongepowered.common.data.value.mutable.SpongeOptionalValue;
+import org.spongepowered.common.util.GetterFunction;
+import org.spongepowered.common.util.SetterFunction;
+
+import javax.annotation.Nullable;
+import java.util.UUID;
+
+public class SpongeTameableData extends AbstractData<TameableData, ImmutableTameableData> implements TameableData  {
+
+    @Nullable private UUID owner;
+
+    public SpongeTameableData(@Nullable UUID owner) {
+        super(TameableData.class);
+        this.owner = owner;
+        registerGettersAndSetters();
+    }
+
+    public SpongeTameableData() {
+        this(null);
+    }
+
+    @Override
+    public OptionalValue<UUID> owner() {
+        return new SpongeOptionalValue<UUID>(Keys.TAMED_OWNER, Optional.fromNullable(owner));
+    }
+
+    @Override
+    public TameableData copy() {
+        return new SpongeTameableData(this.owner);
+    }
+
+    @Override
+    public ImmutableTameableData asImmutable() {
+        return new ImmutableSpongeTameableData(this.owner);
+    }
+
+    @Override
+    public int compareTo(TameableData o) {
+        return ComparisonChain.start()
+                .compare(owner, o.owner().get().orNull())
+                .result();
+    }
+
+    @Override
+    public DataContainer toContainer() {
+        return new MemoryDataContainer()
+                .set(Keys.TAMED_OWNER.getQuery(), this.owner);
+    }
+
+    public Optional<UUID> getOwner(){
+        return Optional.fromNullable(owner);
+    }
+
+    public SpongeTameableData setOwner(@Nullable UUID owner){
+        this.owner = new UUID(owner.getMostSignificantBits(), owner.getLeastSignificantBits());
+        return this;
+    }
+
+    @Override
+    protected void registerGettersAndSetters() {
+        registerFieldGetter(Keys.TAMED_OWNER, new GetterFunction<Object>() {
+            @Override
+            public Object get() {
+                return owner();
+            }
+        });
+        registerFieldSetter(Keys.TAMED_OWNER, new SetterFunction<Object>() {
+            @Override
+            public void set(Object value) {
+                setOwner((UUID) value);
+            }
+        });
+        registerKeyValue(Keys.TAMED_OWNER, new GetterFunction<Value<?>>() {
+            @Override
+            public Value<?> get() {
+                return owner();
+            }
+        });
+    }
+}
