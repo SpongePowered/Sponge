fbc2514d3c41c4b2fa2d9dd32986ee878bd1325a
Aaron Hill <aa1ronham@gmail.com>
2015-09-12 14:35:42 -0400

    Implement TameEntityEvent

--- src/main/java/org/spongepowered/common/mixin/core/entity/living/animal/MixinEntityOcelot.java
+++ src/main/java/org/spongepowered/common/mixin/core/entity/living/animal/MixinEntityOcelot.java
@@ -25,17 +25,47 @@
 package org.spongepowered.common.mixin.core.entity.living.animal;
 
 import net.minecraft.entity.passive.EntityOcelot;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.api.data.manipulator.mutable.entity.OcelotData;
 import org.spongepowered.api.entity.living.animal.Ocelot;
+import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.common.Sponge;
+
+import java.util.Random;
 
 @NonnullByDefault
 @Mixin(EntityOcelot.class)
 public abstract class MixinEntityOcelot extends MixinEntityAnimal implements Ocelot {
 
+    private ItemStack currentItemStack;
+
     @Override
     public OcelotData getOcelotData() {
         return get(OcelotData.class).get();
+    }
+
+    @Inject(method = "interact", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/EntityOcelot;isTamed()Z", shift = At.Shift.BEFORE, ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void afterGetCurrentItem(EntityPlayer player, CallbackInfoReturnable<Boolean> cir, ItemStack currentItemStack) {
+        this.currentItemStack = currentItemStack.copy();
+    }
+
+    @Redirect(method = "interact", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", args = "log=true", ordinal = 0))
+    public int onTame(Random rand, int bound, EntityPlayer player) {
+        int random = rand.nextInt(bound);
+        if (random == 0 && !Sponge.getGame().getEventManager().post(SpongeEventFactory.createTameEntityEvent(Sponge.getGame(), Cause.of(player, this.currentItemStack), this))) {
+            this.currentItemStack = null;
+            return random;
+        }
+        this.currentItemStack = null;
+        return 1;
     }
 }
