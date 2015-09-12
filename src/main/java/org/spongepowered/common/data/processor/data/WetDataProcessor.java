3a0c08765922458b2bf3f5290756315d71520408
sanman00 <sanjaywygold2011@hotmail.co.uk>
2015-09-12 16:28:07 +0100

    Start of implementation
    * Kind of implement fill() and from()
    * Replace null with Optional.absent()
    * Update formatting

--- src/main/java/org/spongepowered/common/data/processor/data/WetDataProcessor.java
+++ src/main/java/org/spongepowered/common/data/processor/data/WetDataProcessor.java
@@ -25,23 +25,19 @@
 package org.spongepowered.common.data.processor.data;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-
 import org.spongepowered.api.data.DataContainer;
 import org.spongepowered.api.data.DataHolder;
 import org.spongepowered.api.data.DataTransactionResult;
 import org.spongepowered.api.data.key.Key;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.data.manipulator.immutable.ImmutableWetData;
-import org.spongepowered.api.data.manipulator.mutable.SkullData;
 import org.spongepowered.api.data.manipulator.mutable.WetData;
 import org.spongepowered.api.data.merge.MergeFunction;
 import org.spongepowered.api.data.value.BaseValue;
+import org.spongepowered.common.data.manipulator.mutable.SpongeWetData;
 import org.spongepowered.common.data.processor.common.AbstractSpongeDataProcessor;
-
 import com.google.common.base.Optional;
-
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -54,45 +50,44 @@ public class WetDataProcessor extends AbstractSpongeDataProcessor<WetData, Immut
 
 	@Override
 	public Optional<WetData> from(DataHolder dataHolder) {
-		// TODO Auto-generated method stub
-		return null;
+		if (dataHolder instanceof ItemStack && ((ItemStack) dataHolder).hasTagCompound()) {
+			SpongeWetData wetData = new SpongeWetData(false);
+		}
+		return Optional.absent();
 	}
 
 	@Override
 	public Optional<WetData> createFrom(DataHolder dataHolder) {
 		// TODO Auto-generated method stub
-		return null;
+		return Optional.absent();
 	}
 
 	@Override
 	public Optional<WetData> fill(DataHolder dataHolder, WetData manipulator, MergeFunction overlap) {
 		if (this.supports(dataHolder)) {
-            
+			WetData merged = overlap.merge(checkNotNull(manipulator.copy()), this.from(dataHolder).get());
+            return Optional.of(manipulator.set(Keys.IS_WET, merged.get(Keys.IS_WET).get()));
         }
         return Optional.absent();
 	}
 
 	@Override
 	public Optional<WetData> fill(DataContainer container, WetData m) {
-		// TODO Auto-generated method stub
-		return null;
+		return Optional.absent();
 	}
 
 	@Override
 	public DataTransactionResult set(DataHolder dataHolder, WetData manipulator, MergeFunction function) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Optional<ImmutableWetData> with(Key<? extends BaseValue<?>> key, Object value, ImmutableWetData immutable) {
-		// TODO Auto-generated method stub
-		return null;
+		return Optional.absent();
 	}
 
 	@Override
 	public DataTransactionResult remove(DataHolder dataHolder) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
