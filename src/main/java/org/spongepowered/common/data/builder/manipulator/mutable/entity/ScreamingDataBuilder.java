a8580ae20f49428902553dc8c0ff807b5a3d1311
Steven Downer <grinch@outlook.com>
2015-09-12 20:16:19 -0500

    Implement ScreamingData
    Signed-off-by: Steven Downer <grinch@outlook.com>

---
+++ src/main/java/org/spongepowered/common/data/builder/manipulator/mutable/entity/ScreamingDataBuilder.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.data.builder.manipulator.mutable.entity;
+
+import com.google.common.base.Optional;
+import org.spongepowered.api.data.DataHolder;
+import org.spongepowered.api.data.DataView;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.DataManipulatorBuilder;
+import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableScreamingData;
+import org.spongepowered.api.data.manipulator.mutable.entity.ScreamingData;
+import org.spongepowered.api.service.persistence.InvalidDataException;
+import org.spongepowered.common.data.manipulator.mutable.entity.SpongeScreamingData;
+
+public class ScreamingDataBuilder implements DataManipulatorBuilder<ScreamingData, ImmutableScreamingData> {
+
+    @Override
+    public Optional<ScreamingData> build(DataView container) throws InvalidDataException {
+        if (container.contains(Keys.IS_SCREAMING.getQuery())) {
+            Optional<Boolean> isScreaming = container.getBoolean(Keys.IS_SCREAMING.getQuery());
+            if (isScreaming.isPresent()) {
+                return Optional.<ScreamingData>of(new SpongeScreamingData(isScreaming.get()));
+            }
+        }
+        return Optional.absent();
+    }
+
+    @Override
+    public ScreamingData create() {
+        return new SpongeScreamingData(false);
+    }
+
+    @Override
+    public Optional<ScreamingData> createFrom(DataHolder dataHolder) {
+        if (dataHolder.supports(Keys.IS_SCREAMING)) {
+            return Optional.<ScreamingData>of(new SpongeScreamingData(dataHolder.get(Keys.IS_SCREAMING).get()));
+        }
+
+        return Optional.absent();
+    }
+}
---
+++ src/main/java/org/spongepowered/common/data/processor/value/entity/ScreamingValueProcessor.java
@@ -0,0 +1,83 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.data.processor.value.entity;
+
+import com.google.common.base.Optional;
+import net.minecraft.entity.monster.EntityEnderman;
+import org.spongepowered.api.data.DataTransactionBuilder;
+import org.spongepowered.api.data.DataTransactionResult;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.value.ValueContainer;
+import org.spongepowered.api.data.value.immutable.ImmutableValue;
+import org.spongepowered.api.data.value.mutable.Value;
+import org.spongepowered.common.data.processor.common.AbstractSpongeValueProcessor;
+import org.spongepowered.common.data.value.immutable.ImmutableSpongeValue;
+import org.spongepowered.common.data.value.mutable.SpongeValue;
+
+public class ScreamingValueProcessor extends AbstractSpongeValueProcessor<Boolean, Value<Boolean>> {
+
+    public ScreamingValueProcessor() {
+        super(Keys.IS_SCREAMING);
+    }
+
+    @Override
+    protected Value<Boolean> constructValue(Boolean defaultValue) {
+        return new SpongeValue<Boolean>(Keys.IS_SCREAMING, false, defaultValue);
+    }
+
+    @Override
+    public Optional<Boolean> getValueFromContainer(ValueContainer<?> container) {
+        if (container.supports(Keys.IS_SCREAMING)) {
+            return Optional.of(((EntityEnderman) container).isScreaming());
+        }
+        return Optional.absent();
+    }
+
+    @Override
+    public boolean supports(ValueContainer<?> container) {
+        return container instanceof EntityEnderman;
+    }
+
+    @Override
+    public DataTransactionResult offerToStore(ValueContainer<?> container, Boolean value) {
+        final ImmutableValue proposedValue = new ImmutableSpongeValue(Keys.IS_SCREAMING, value);
+        final ImmutableValue oldValue = getApiValueFromContainer(container).get().asImmutable();
+
+        if (container instanceof EntityEnderman) {
+            try {
+                ((EntityEnderman) container).setScreaming(value);
+            } catch (Exception e) {
+                return DataTransactionBuilder.errorResult(proposedValue);
+            }
+            return DataTransactionBuilder.builder().success(proposedValue).replace(oldValue).result(DataTransactionResult.Type.SUCCESS).build();
+        }
+        return DataTransactionBuilder.failResult(proposedValue);
+    }
+
+    @Override
+    public DataTransactionResult removeFrom(ValueContainer<?> container) {
+        return DataTransactionBuilder.failNoData();
+    }
+}
