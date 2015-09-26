d38a8de2179869364ae71f829280d94229228a94
sanman00 <sanjaywygold2011@hotmail.co.uk>
2015-09-26 18:52:21 +0100

    Add key

--- src/main/java/org/spongepowered/common/data/key/KeyRegistry.java
+++ src/main/java/org/spongepowered/common/data/key/KeyRegistry.java
@@ -128,6 +128,7 @@ public class KeyRegistry {
         keyMap.put("horse_variant", makeSingleKey(HorseVariant.class, Value.class, of("HorseVariant")));
         keyMap.put("item_lore", makeListKey(Text.class, of("ItemLore")));
         keyMap.put("book_pages", makeListKey(Text.class, of("BookPages")));
+        keyMap.put("is_wet", makeSingleKey(Boolean.class, Value.class, of("IsWet")));
         RegistryHelper.mapFields(Keys.class, keyMap);
     }
 
