0bfba141c033a3401c7c405e64a979a81c5f829b
Rain336 <rain336@live.de>
2015-10-10 22:03:04 +0200

    Fixed someting Strange

--- src/main/java/org/spongepowered/common/registry/SpongeGameRegistry.java
+++ src/main/java/org/spongepowered/common/registry/SpongeGameRegistry.java
@@ -620,6 +620,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Function;
 
 public abstract class SpongeGameRegistry implements GameRegistry {
 
