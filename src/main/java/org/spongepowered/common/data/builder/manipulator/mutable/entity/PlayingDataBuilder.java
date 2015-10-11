c14b232607a316d2e9b8aaa872f8fe320b12c703
BuildTools <clienthax@gmail.com>
2015-10-12 00:28:58 +0100

    Licenses

--- src/main/java/org/spongepowered/common/data/builder/manipulator/mutable/entity/PlayingDataBuilder.java
+++ src/main/java/org/spongepowered/common/data/builder/manipulator/mutable/entity/PlayingDataBuilder.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.spongepowered.common.data.builder.manipulator.mutable.entity;
 
 import static org.spongepowered.common.data.util.DataUtil.getData;
@@ -26,9 +50,8 @@ public class PlayingDataBuilder implements DataManipulatorBuilder<PlayingData, I
 
     @Override
     public Optional<PlayingData> createFrom(DataHolder dataHolder) {
-        final boolean isPlaying;
-        if(supports(dataHolder)) {
-            isPlaying = ((EntityVillager) dataHolder).isPlaying();
+        if (supports(dataHolder)) {
+            final boolean isPlaying = ((EntityVillager) dataHolder).isPlaying();
             return Optional.of(new SpongePlayingData(isPlaying));
         }
         return Optional.empty();
@@ -36,7 +59,7 @@ public class PlayingDataBuilder implements DataManipulatorBuilder<PlayingData, I
 
     @Override
     public Optional<PlayingData> build(DataView container) throws InvalidDataException {
-        if(container.contains(Keys.IS_PLAYING.getQuery())) {
+        if (container.contains(Keys.IS_PLAYING.getQuery())) {
             PlayingData playingData = create();
             playingData.set(Keys.IS_PLAYING, getData(container, Keys.IS_PLAYING));
             return Optional.of(playingData);
