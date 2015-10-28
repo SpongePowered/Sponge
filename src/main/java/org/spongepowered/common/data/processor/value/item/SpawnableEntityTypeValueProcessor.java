6d8d67956297c23e5129fca7c6bb955f6554ffee
Rain336 <rain336@live.de>
2015-10-28 22:52:32 +0100

    Updated License Headers and Stopped offering of Entities that haven't got a SpawnEgg

--- src/main/java/org/spongepowered/common/data/processor/value/item/SpawnableEntityTypeValueProcessor.java
+++ src/main/java/org/spongepowered/common/data/processor/value/item/SpawnableEntityTypeValueProcessor.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.spongepowered.common.data.processor.value.item;
 
 import net.minecraft.entity.EntityList;
@@ -37,8 +61,11 @@ public class SpawnableEntityTypeValueProcessor extends AbstractSpongeValueProces
     public boolean set(ItemStack container, EntityType value) {
         final String name = (String) EntityList.classToStringMapping.get(value.getEntityClass());
         final int id = (int) EntityList.stringToIDMapping.get(name);
-        container.setItemDamage(id);
-        return true;
+        if(EntityList.entityEggs.containsKey(id)) {
+            container.setItemDamage(id);
+            return true;
+        }
+        return false;
     }
 
     @Override
