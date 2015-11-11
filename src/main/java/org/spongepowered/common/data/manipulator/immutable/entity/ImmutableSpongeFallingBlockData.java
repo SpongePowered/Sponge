ca26af3aa4a095304129a469734b9c06b38c7643
BuildTools <clienthax@gmail.com>
2015-11-11 00:25:24 +0000

    Implement FallingBlockData. Closes #257.

---
+++ src/main/java/org/spongepowered/common/data/manipulator/immutable/entity/ImmutableSpongeFallingBlockData.java
@@ -0,0 +1,204 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.data.manipulator.immutable.entity;
+
+import com.google.common.collect.ComparisonChain;
+import org.spongepowered.api.block.BlockState;
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.MemoryDataContainer;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableFallingBlockData;
+import org.spongepowered.api.data.manipulator.mutable.entity.FallingBlockData;
+import org.spongepowered.api.data.value.immutable.ImmutableValue;
+import org.spongepowered.common.data.manipulator.immutable.common.AbstractImmutableData;
+import org.spongepowered.common.data.manipulator.mutable.entity.SpongeFallingBlockData;
+import org.spongepowered.common.data.util.DataConstants;
+import org.spongepowered.common.data.value.immutable.ImmutableSpongeValue;
+
+public class ImmutableSpongeFallingBlockData extends AbstractImmutableData<ImmutableFallingBlockData, FallingBlockData> implements ImmutableFallingBlockData  {
+
+    private final double fallDamagePerBlock;
+    private final double maxFallDamage;
+    private final BlockState blockState;
+    private final boolean canPlaceAsBlock;
+    private final boolean canDropAsItem;
+    private final int fallTime;
+    private boolean canHurtEntities;
+
+    private final ImmutableSpongeValue<Double> fallDamagePerBlockValue;
+    private final ImmutableSpongeValue<Double> maxFallDamageValue;
+    private final ImmutableSpongeValue<BlockState> blockStateValue;
+    private final ImmutableSpongeValue<Boolean> canPlaceAsBlockValue;
+    private final ImmutableSpongeValue<Boolean> canDropAsItemValue;
+    private final ImmutableSpongeValue<Integer> fallTimeValue;
+    private final ImmutableSpongeValue<Boolean> canHurtEntitiesValue;
+
+    public ImmutableSpongeFallingBlockData(double fallDamagePerBlock, double maxFallDamage, BlockState blockState, boolean canPlaceAsBlock,
+            boolean canDropAsItem, int fallTime, boolean canHurtEntities) {
+        super(ImmutableFallingBlockData.class);
+        this.fallDamagePerBlock = fallDamagePerBlock;
+        this.maxFallDamage = maxFallDamage;
+        this.blockState = blockState;
+        this.canPlaceAsBlock = canPlaceAsBlock;
+        this.canDropAsItem = canDropAsItem;
+        this.fallTime = fallTime;
+        this.canHurtEntities = canHurtEntities;
+
+        this.fallDamagePerBlockValue = new ImmutableSpongeValue<>(Keys.FALL_DAMAGE_PER_BLOCK, DataConstants
+                .DEFAULT_FALLING_BLOCK_FALL_DAMAGE_PER_BLOCK, this.fallDamagePerBlock);
+        this.maxFallDamageValue = new ImmutableSpongeValue<>(Keys.MAX_FALL_DAMAGE, DataConstants.DEFAULT_FALLING_BLOCK_MAX_FALL_DAMAGE, this
+                .maxFallDamage);
+        this.blockStateValue = new ImmutableSpongeValue<>(Keys.FALLING_BLOCK_STATE, DataConstants.DEFAULT_FALLING_BLOCK_BLOCKSTATE, this
+                .blockState);
+        this.canPlaceAsBlockValue = new ImmutableSpongeValue<>(Keys.CAN_PLACE_AS_BLOCK, DataConstants
+                .DEFAULT_FALLING_BLOCK_CAN_PLACE_AS_BLOCK, this.canPlaceAsBlock);
+        this.canDropAsItemValue = new ImmutableSpongeValue<>(Keys.CAN_DROP_AS_ITEM, DataConstants.DEFAULT_FALLING_BLOCK_CAN_DROP_AS_ITEM,
+                this.canDropAsItem);
+        this.fallTimeValue = new ImmutableSpongeValue<>(Keys.FALL_TIME, DataConstants.DEFAULT_FALLING_BLOCK_FALL_TIME,
+                this.fallTime);
+        this.canHurtEntitiesValue = new ImmutableSpongeValue<>(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, DataConstants
+                .DEFAULT_FALLING_BLOCK_CAN_HURT_ENTITIES, this.canHurtEntities);
+
+        registerGetters();
+    }
+
+    @Override
+    protected void registerGetters() {
+        registerFieldGetter(Keys.FALL_DAMAGE_PER_BLOCK, ImmutableSpongeFallingBlockData.this::getFallDamagePerBlock);
+        registerKeyValue(Keys.FALL_DAMAGE_PER_BLOCK, ImmutableSpongeFallingBlockData.this::fallDamagePerBlock);
+
+        registerFieldGetter(Keys.MAX_FALL_DAMAGE, ImmutableSpongeFallingBlockData.this::getMaxFallDamage);
+        registerKeyValue(Keys.MAX_FALL_DAMAGE, ImmutableSpongeFallingBlockData.this::maxFallDamage);
+
+        registerFieldGetter(Keys.FALLING_BLOCK_STATE, ImmutableSpongeFallingBlockData.this::getBlockState);
+        registerKeyValue(Keys.FALLING_BLOCK_STATE, ImmutableSpongeFallingBlockData.this::blockState);
+
+        registerFieldGetter(Keys.CAN_PLACE_AS_BLOCK, ImmutableSpongeFallingBlockData.this::getCanPlaceAsBlock);
+        registerKeyValue(Keys.CAN_PLACE_AS_BLOCK, ImmutableSpongeFallingBlockData.this::canPlaceAsBlock);
+
+        registerFieldGetter(Keys.CAN_DROP_AS_ITEM, ImmutableSpongeFallingBlockData.this::getCanDropAsItem);
+        registerKeyValue(Keys.CAN_DROP_AS_ITEM, ImmutableSpongeFallingBlockData.this::canDropAsItem);
+
+        registerFieldGetter(Keys.FALL_TIME, ImmutableSpongeFallingBlockData.this::getFallTime);
+        registerKeyValue(Keys.FALL_TIME, ImmutableSpongeFallingBlockData.this::fallTime);
+
+        registerFieldGetter(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, ImmutableSpongeFallingBlockData.this::getCanHurtEntities);
+        registerKeyValue(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, ImmutableSpongeFallingBlockData.this::canHurtEntities);
+    }
+
+    public double getFallDamagePerBlock() {
+        return this.fallDamagePerBlock;
+    }
+
+    @Override
+    public ImmutableValue<Double> fallDamagePerBlock() {
+        return fallDamagePerBlockValue;
+    }
+
+    public double getMaxFallDamage() {
+        return this.maxFallDamage;
+    }
+
+    @Override
+    public ImmutableValue<Double> maxFallDamage() {
+        return maxFallDamageValue;
+    }
+
+    public BlockState getBlockState() {
+        return this.blockState;
+    }
+
+    @Override
+    public ImmutableValue<BlockState> blockState() {
+        return blockStateValue;
+    }
+
+    public boolean getCanPlaceAsBlock() {
+        return this.canPlaceAsBlock;
+    }
+
+    @Override
+    public ImmutableValue<Boolean> canPlaceAsBlock() {
+        return canPlaceAsBlockValue;
+    }
+
+    public boolean getCanDropAsItem() {
+        return this.canDropAsItem;
+    }
+
+    @Override
+    public ImmutableValue<Boolean> canDropAsItem() {
+        return canDropAsItemValue;
+    }
+
+    public int getFallTime() {
+        return this.fallTime;
+    }
+
+    @Override
+    public ImmutableValue<Integer> fallTime() {
+        return fallTimeValue;
+    }
+
+    public boolean getCanHurtEntities() {
+        return this.canHurtEntities;
+    }
+
+    @Override
+    public ImmutableValue<Boolean> canHurtEntities() {
+        return canHurtEntitiesValue;
+    }
+
+    @Override
+    public FallingBlockData asMutable() {
+        return new SpongeFallingBlockData(this.fallDamagePerBlock, this.maxFallDamage, this.blockState, this.canPlaceAsBlock, this.canDropAsItem,
+                this.fallTime, this.canHurtEntities);
+    }
+
+    @Override
+    public int compareTo(ImmutableFallingBlockData o) {
+        return ComparisonChain.start()
+                .compare(o.fallDamagePerBlock().get().doubleValue(), this.fallDamagePerBlock)
+                .compare(o.maxFallDamage().get().doubleValue(), this.maxFallDamage)
+                .compare(o.blockState().get().getType().getId(), this.blockState.getType().getId())
+                .compare(o.canPlaceAsBlock().get().booleanValue(), this.canPlaceAsBlock)
+                .compare(o.canDropAsItem().get().booleanValue(), this.canDropAsItem)
+                .compare(o.fallTime().get().intValue(), this.fallTime)
+                .compare(o.canHurtEntities().get().booleanValue(), this.canHurtEntities)
+                .result();
+    }
+
+    @Override
+    public DataContainer toContainer() {
+        return new MemoryDataContainer()
+                .set(Keys.FALL_DAMAGE_PER_BLOCK.getQuery(), this.fallDamagePerBlock)
+                .set(Keys.MAX_FALL_DAMAGE.getQuery(), this.maxFallDamage)
+                .set(Keys.FALLING_BLOCK_STATE.getQuery(), this.blockState)
+                .set(Keys.CAN_PLACE_AS_BLOCK.getQuery(), this.canPlaceAsBlock)
+                .set(Keys.CAN_DROP_AS_ITEM.getQuery(), this.canDropAsItem)
+                .set(Keys.FALL_TIME.getQuery(), this.fallTime)
+                .set(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, this.canHurtEntities);
+    }
+}
---
+++ src/main/java/org/spongepowered/common/data/manipulator/mutable/entity/SpongeFallingBlockData.java
@@ -0,0 +1,228 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.data.manipulator.mutable.entity;
+
+import com.google.common.collect.ComparisonChain;
+import org.spongepowered.api.block.BlockState;
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.MemoryDataContainer;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.immutable.entity.ImmutableFallingBlockData;
+import org.spongepowered.api.data.manipulator.mutable.entity.FallingBlockData;
+import org.spongepowered.api.data.value.mutable.Value;
+import org.spongepowered.common.data.manipulator.immutable.entity.ImmutableSpongeFallingBlockData;
+import org.spongepowered.common.data.manipulator.mutable.common.AbstractData;
+import org.spongepowered.common.data.util.DataConstants;
+import org.spongepowered.common.data.value.mutable.SpongeValue;
+
+public class SpongeFallingBlockData extends AbstractData<FallingBlockData, ImmutableFallingBlockData> implements FallingBlockData {
+
+    private double fallDamagePerBlock;
+    private double maxFallDamage;
+    private BlockState blockState;
+    private boolean canPlaceAsBlock;
+    private boolean canDropAsItem;
+    private int fallTime;
+    private boolean canHurtEntities;
+
+    public SpongeFallingBlockData(double fallDamagePerBlock, double maxFallDamage, BlockState blockState, boolean canPlaceAsBlock, boolean
+            canDropAsItem, int fallTime, boolean canHurtEntities) {
+        super(FallingBlockData.class);
+        this.fallDamagePerBlock = fallDamagePerBlock;
+        this.maxFallDamage = maxFallDamage;
+        this.blockState = blockState;
+        this.canPlaceAsBlock = canPlaceAsBlock;
+        this.canDropAsItem = canDropAsItem;
+        this.fallTime = fallTime;
+        this.canHurtEntities = canHurtEntities;
+        registerGettersAndSetters();
+    }
+
+    public SpongeFallingBlockData() {
+        this(DataConstants.DEFAULT_FALLING_BLOCK_FALL_DAMAGE_PER_BLOCK, DataConstants.DEFAULT_FALLING_BLOCK_MAX_FALL_DAMAGE, DataConstants
+                        .DEFAULT_FALLING_BLOCK_BLOCKSTATE, DataConstants.DEFAULT_FALLING_BLOCK_CAN_PLACE_AS_BLOCK, DataConstants
+                        .DEFAULT_FALLING_BLOCK_CAN_DROP_AS_ITEM, DataConstants.DEFAULT_FALLING_BLOCK_FALL_TIME,
+                DataConstants.DEFAULT_FALLING_BLOCK_CAN_HURT_ENTITIES);
+    }
+
+    @Override
+    protected void registerGettersAndSetters() {
+        registerFieldGetter(Keys.FALL_DAMAGE_PER_BLOCK, SpongeFallingBlockData.this::getFallDamagePerBlock);
+        registerFieldSetter(Keys.FALL_DAMAGE_PER_BLOCK, SpongeFallingBlockData.this::setFallDamagePerBlock);
+        registerKeyValue(Keys.FALL_DAMAGE_PER_BLOCK, SpongeFallingBlockData.this::fallDamagePerBlock);
+
+        registerFieldGetter(Keys.MAX_FALL_DAMAGE, SpongeFallingBlockData.this::getMaxFallDamage);
+        registerFieldSetter(Keys.MAX_FALL_DAMAGE, SpongeFallingBlockData.this::setMaxFallDamage);
+        registerKeyValue(Keys.MAX_FALL_DAMAGE, SpongeFallingBlockData.this::maxFallDamage);
+
+        registerFieldGetter(Keys.FALLING_BLOCK_STATE, SpongeFallingBlockData.this::getBlockState);
+        registerFieldSetter(Keys.FALLING_BLOCK_STATE, SpongeFallingBlockData.this::setBlockState);
+        registerKeyValue(Keys.FALLING_BLOCK_STATE, SpongeFallingBlockData.this::blockState);
+
+        registerFieldGetter(Keys.CAN_PLACE_AS_BLOCK, SpongeFallingBlockData.this::getCanPlaceAsBlock);
+        registerFieldSetter(Keys.CAN_PLACE_AS_BLOCK, SpongeFallingBlockData.this::setCanPlaceAsBlock);
+        registerKeyValue(Keys.CAN_PLACE_AS_BLOCK, SpongeFallingBlockData.this::canPlaceAsBlock);
+
+        registerFieldGetter(Keys.CAN_DROP_AS_ITEM, SpongeFallingBlockData.this::getCanDropAsItem);
+        registerFieldSetter(Keys.CAN_DROP_AS_ITEM, SpongeFallingBlockData.this::setCanDropAsItem);
+        registerKeyValue(Keys.CAN_DROP_AS_ITEM, SpongeFallingBlockData.this::canDropAsItem);
+
+        registerFieldGetter(Keys.FALL_TIME, SpongeFallingBlockData.this::getFallTime);
+        registerFieldSetter(Keys.FALL_TIME, SpongeFallingBlockData.this::setFallTime);
+        registerKeyValue(Keys.FALL_TIME, SpongeFallingBlockData.this::fallTime);
+
+        registerFieldGetter(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, SpongeFallingBlockData.this::getCanHurtEntities);
+        registerFieldSetter(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, SpongeFallingBlockData.this::setCanHurtEntities);
+        registerKeyValue(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, SpongeFallingBlockData.this::canHurtEntities);
+    }
+
+    public void setFallDamagePerBlock(double value) {
+        this.fallDamagePerBlock = value;
+    }
+
+    public double getFallDamagePerBlock() {
+        return this.fallDamagePerBlock;
+    }
+
+    @Override
+    public Value<Double> fallDamagePerBlock() {
+        return new SpongeValue<>(Keys.FALL_DAMAGE_PER_BLOCK, DataConstants.DEFAULT_FALLING_BLOCK_FALL_DAMAGE_PER_BLOCK, this.fallDamagePerBlock);
+    }
+
+    public void setMaxFallDamage(double value) {
+        this.maxFallDamage = value;
+    }
+
+    public double getMaxFallDamage() {
+        return this.maxFallDamage;
+    }
+
+    @Override
+    public Value<Double> maxFallDamage() {
+        return new SpongeValue<>(Keys.MAX_FALL_DAMAGE, DataConstants.DEFAULT_FALLING_BLOCK_MAX_FALL_DAMAGE, this.maxFallDamage);
+    }
+
+    public void setBlockState(BlockState value) {
+        this.blockState = value;
+    }
+
+    public BlockState getBlockState() {
+        return this.blockState;
+    }
+
+    @Override
+    public Value<BlockState> blockState() {
+        return new SpongeValue<>(Keys.FALLING_BLOCK_STATE, DataConstants.DEFAULT_FALLING_BLOCK_BLOCKSTATE, this.blockState);
+    }
+
+    public void setCanPlaceAsBlock(boolean value) {
+        this.canPlaceAsBlock = value;
+    }
+
+    public boolean getCanPlaceAsBlock() {
+        return this.canPlaceAsBlock;
+    }
+
+    @Override
+    public Value<Boolean> canPlaceAsBlock() {
+        return new SpongeValue<>(Keys.CAN_PLACE_AS_BLOCK, DataConstants.DEFAULT_FALLING_BLOCK_CAN_PLACE_AS_BLOCK, this.canPlaceAsBlock);
+    }
+
+    public void setCanDropAsItem(boolean value) {
+        this.canDropAsItem = value;
+    }
+
+    public boolean getCanDropAsItem() {
+        return this.canDropAsItem;
+    }
+
+    @Override
+    public Value<Boolean> canDropAsItem() {
+        return new SpongeValue<>(Keys.CAN_DROP_AS_ITEM, DataConstants.DEFAULT_FALLING_BLOCK_CAN_DROP_AS_ITEM, this.canDropAsItem);
+    }
+
+    public void setFallTime(int value) {
+        this.fallTime = value;
+    }
+
+    public int getFallTime() {
+        return this.fallTime;
+    }
+
+    @Override
+    public Value<Integer> fallTime() {
+        return new SpongeValue<>(Keys.FALL_TIME, DataConstants.DEFAULT_FALLING_BLOCK_FALL_TIME, this.fallTime);
+    }
+
+    public void setCanHurtEntities(boolean value) {
+        this.canHurtEntities = value;
+    }
+
+    public boolean getCanHurtEntities() {
+        return this.canHurtEntities;
+    }
+
+    @Override
+    public Value<Boolean> canHurtEntities() {
+        return new SpongeValue<>(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, DataConstants.DEFAULT_FALLING_BLOCK_CAN_HURT_ENTITIES, this.canHurtEntities);
+    }
+
+    @Override
+    public FallingBlockData copy() {
+        return new SpongeFallingBlockData(this.fallDamagePerBlock, this.maxFallDamage, this.blockState, this.canPlaceAsBlock, this.canDropAsItem,
+                this.fallTime, this.canHurtEntities);
+    }
+
+    @Override
+    public ImmutableFallingBlockData asImmutable() {
+        return new ImmutableSpongeFallingBlockData(this.fallDamagePerBlock, this.maxFallDamage, this.blockState, this.canPlaceAsBlock, this
+                .canDropAsItem, this.fallTime, this.canHurtEntities);
+    }
+
+    @Override
+    public int compareTo(FallingBlockData o) {
+        return ComparisonChain.start()
+                .compare(o.fallDamagePerBlock().get().doubleValue(), this.fallDamagePerBlock)
+                .compare(o.maxFallDamage().get().doubleValue(), this.maxFallDamage)
+                .compare(o.blockState().get().getType().getId(), this.blockState.getType().getId())
+                .compare(o.canPlaceAsBlock().get().booleanValue(), this.canPlaceAsBlock)
+                .compare(o.canDropAsItem().get().booleanValue(), this.canDropAsItem)
+                .compare(o.fallTime().get().intValue(), this.fallTime)
+                .compare(o.canHurtEntities().get().booleanValue(), this.canHurtEntities)
+                .result();
+    }
+
+    @Override
+    public DataContainer toContainer() {
+        return new MemoryDataContainer()
+                .set(Keys.FALL_DAMAGE_PER_BLOCK.getQuery(), this.fallDamagePerBlock)
+                .set(Keys.MAX_FALL_DAMAGE.getQuery(), this.maxFallDamage)
+                .set(Keys.FALLING_BLOCK_STATE.getQuery(), this.blockState)
+                .set(Keys.CAN_PLACE_AS_BLOCK.getQuery(), this.canPlaceAsBlock)
+                .set(Keys.CAN_DROP_AS_ITEM.getQuery(), this.canDropAsItem)
+                .set(Keys.FALL_TIME.getQuery(), this.fallTime)
+                .set(Keys.FALLING_BLOCK_CAN_HURT_ENTITIES, this.canHurtEntities);
+    }
+}
