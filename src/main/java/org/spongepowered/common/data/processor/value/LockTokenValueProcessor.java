940ca504bd30565981f8f4e60ba8805ca8a46ff3
JBYoshi <jbyoshi@users.noreply.github.com>
2015-11-29 21:25:47 -0600

    Fix LocableValueProcessor. Closes #296.

--- src/main/java/org/spongepowered/common/data/processor/value/LockTokenValueProcessor.java
+++ src/main/java/org/spongepowered/common/data/processor/value/LockTokenValueProcessor.java
@@ -45,8 +45,8 @@ public final class LockTokenValueProcessor extends AbstractSpongeValueProcessor<
 
     @Override
     public DataTransactionResult removeFrom(ValueContainer<?> container) {
-        if (container instanceof ILockableContainer) {
-            set((ILockableContainer) container, "");
+        if (container instanceof TileEntityLockable) {
+            set((TileEntityLockable) container, "");
             return DataTransactionResult.successNoData();
         }
         return DataTransactionResult.failNoData();
