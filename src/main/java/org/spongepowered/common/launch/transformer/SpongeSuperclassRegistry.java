a78f7955fdabee1ec1781328f71530dfeeff3ce6
Minecrell <dev@minecrell.net>
2015-12-01 16:16:34 +0100

    Move transformer to launch package

--- src/main/java/org/spongepowered/common/launch/transformer/SpongeSuperclassRegistry.java
+++ src/main/java/org/spongepowered/common/launch/transformer/SpongeSuperclassRegistry.java
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.common.transformer;
+package org.spongepowered.common.launch.transformer;
 
 import com.google.common.collect.Maps;
 
--- src/main/java/org/spongepowered/common/launch/transformer/SpongeSuperclassTransformer.java
+++ src/main/java/org/spongepowered/common/launch/transformer/SpongeSuperclassTransformer.java
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.common.transformer;
+package org.spongepowered.common.launch.transformer;
 
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.spongepowered.asm.lib.ClassReader;
