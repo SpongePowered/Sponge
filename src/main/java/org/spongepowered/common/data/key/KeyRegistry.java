f57b604ba550d78c5507128fa47f3aa5c8b773c7
sanman00 <sanjaywygold2011@hotmail.co.uk>
2015-12-04 18:02:10 +0000

    Start implementation of AggressiveData
    Summary of changes:
    * Add keys
    * Implement data manipulator and immutable data manipulator
    * Implement data and value processor
    * Update access transformer
    * Modify mixin to support AggressiveData and modify implemntation of setAngry()
    * Register data and value processor

--- src/main/java/org/spongepowered/common/data/key/KeyRegistry.java
+++ src/main/java/org/spongepowered/common/data/key/KeyRegistry.java
@@ -263,6 +263,7 @@ public class KeyRegistry {
         keyMap.put("expiration_ticks", makeSingleKey(Integer.class, MutableBoundedValue.class, of("ExpirationTicks")));
         keyMap.put("skin", makeSingleKey(UUID.class, Value.class, of("Skin")));
         keyMap.put("moisture", makeSingleKey(Integer.class, MutableBoundedValue.class, of("Moisture")));
+        keyMap.put("angry", makeSingleKey(Boolean.class, Value.class, of("Angry")));
     }
 
     private static Map<String, Key<?>> getKeyMap() {
