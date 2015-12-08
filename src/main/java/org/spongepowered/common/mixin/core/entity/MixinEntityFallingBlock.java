f1ec3d7d1d13d3e4e728b16ac96a1e5c1ef2c295
Chris Sanders <zidane@outlook.com>
2015-12-07 23:33:32 -0600

    Remove useless overwrite in MixinEntityFallingBlock.
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spongepowered/common/mixin/core/entity/MixinEntityFallingBlock.java
+++ src/main/java/org/spongepowered/common/mixin/core/entity/MixinEntityFallingBlock.java
@@ -24,28 +24,19 @@
  */
 package org.spongepowered.common.mixin.core.entity;
 
-import com.google.common.collect.Lists;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockAnvil;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityFallingBlock;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.MathHelper;
 import org.spongepowered.api.entity.FallingBlock;
 import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.common.event.MinecraftFallingBlockDamageSource;
-
-import java.util.ArrayList;
-import java.util.Iterator;
 
 @Mixin(EntityFallingBlock.class)
 public abstract class MixinEntityFallingBlock extends MixinEntity implements FallingBlock {
@@ -81,63 +72,6 @@ public abstract class MixinEntityFallingBlock extends MixinEntity implements Fal
             DamageSource.anvil = this.original;
         } else {
             DamageSource.fallingBlock = this.original;
-        }
-    }
-
-    /**
-     * @author gabizou - November 22, 2015
-     *
-     * Purpose: This will overwrite the damage source to properly use our damage source objects.
-     */
-    @Overwrite
-    public void fall(float distance, float damageMultiplier)
-    {
-        Block block = this.fallTile.getBlock();
-
-        if (this.hurtEntities) {
-            int i = MathHelper.ceiling_float_int(distance - 1.0F);
-
-            if (i > 0) {
-                ArrayList arraylist = Lists.newArrayList(this.worldObj.getEntitiesWithinAABBExcludingEntity((EntityFallingBlock) (Object) this, this.getEntityBoundingBox()));
-                boolean flag = block == Blocks.anvil;
-                // Sponge Start - re-assign the damage sources for equality checks
-                DamageSource original;
-                if (flag) {
-                    original = DamageSource.anvil;
-                    DamageSource.anvil = new MinecraftFallingBlockDamageSource("anvil", (EntityFallingBlock) (Object) this);
-                } else {
-                    original = DamageSource.fallingBlock;
-
-                }
-                // Sponge End
-                DamageSource damagesource = flag ? DamageSource.anvil : DamageSource.fallingBlock;
-                Iterator iterator = arraylist.iterator();
-
-                while (iterator.hasNext()) {
-                    Entity entity = (Entity) iterator.next();
-                    entity
-                        .attackEntityFrom(damagesource, (float) Math.min(MathHelper.floor_float((float) i * this.fallHurtAmount), this.fallHurtMax));
-
-                }
-                // Sponge Start - Re-assign back to the original damage sources so as to not leak entities
-                if (flag) {
-                    DamageSource.anvil = original;
-                } else {
-                    DamageSource.fallingBlock = original;
-                }
-                // Sponge End
-
-                if (flag && (double) this.rand.nextFloat() < 0.05000000074505806D + (double) i * 0.05D) {
-                    int j = ((Integer) this.fallTile.getValue(BlockAnvil.DAMAGE)).intValue();
-                    ++j;
-
-                    if (j > 2) {
-                        this.canSetAsBlock = true;
-                    } else {
-                        this.fallTile = this.fallTile.withProperty(BlockAnvil.DAMAGE, Integer.valueOf(j));
-                    }
-                }
-            }
         }
     }
 
