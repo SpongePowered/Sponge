bf7164e34915edad9c33847c05045025a37ae9ca
Me4502 <mnmiller1@hotmail.com>
2015-12-08 11:48:03 +1000

    Update MoistureData impl.

---
+++ src/main/java/org/spongepowered/common/data/processor/value/block/MoistureValueProcessor.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.data.processor.value.block;
+
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.value.mutable.MutableBoundedValue;
+import org.spongepowered.common.data.processor.common.AbstractBlockOnlyValueProcessor;
+import org.spongepowered.common.data.value.SpongeValueBuilder;
+
+public class MoistureValueProcessor extends AbstractBlockOnlyValueProcessor<Integer, MutableBoundedValue<Integer>> {
+
+    public MoistureValueProcessor() {
+        super(Keys.MOISTURE);
+    }
+
+    @Override
+    protected MutableBoundedValue<Integer> constructValue(Integer defaultValue) {
+        return SpongeValueBuilder.boundedBuilder(Keys.MOISTURE)
+                .defaultValue(0)
+                .minimum(0)
+                .maximum(7)
+                .actualValue(defaultValue)
+                .build();
+    }
+
+}
