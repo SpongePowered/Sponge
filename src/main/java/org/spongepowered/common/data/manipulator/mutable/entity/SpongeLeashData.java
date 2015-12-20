0b5a807ddc7de5152a047fd19a0053b680367ed3
Rain336 <rain336@live.de>
2015-12-20 04:50:10 +0100

    Enhanced compareTo, Changed Value to SpongeEntityValue and Added License

--- src/main/java/org/spongepowered/common/data/manipulator/mutable/entity/SpongeLeashData.java
+++ src/main/java/org/spongepowered/common/data/manipulator/mutable/entity/SpongeLeashData.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.spongepowered.common.data.manipulator.mutable.entity;
 
 import com.google.common.collect.ComparisonChain;
@@ -11,6 +35,7 @@ import org.spongepowered.api.entity.Entity;
 import org.spongepowered.common.data.manipulator.immutable.entity.ImmutableSpongeLeashData;
 import org.spongepowered.common.data.manipulator.mutable.common.AbstractSingleData;
 import org.spongepowered.common.data.value.SpongeValueFactory;
+import org.spongepowered.common.data.value.mutable.common.SpongeEntityValue;
 
 public class SpongeLeashData extends AbstractSingleData<Entity, LeashData, ImmutableLeashData> implements LeashData {
 
@@ -31,13 +56,13 @@ public class SpongeLeashData extends AbstractSingleData<Entity, LeashData, Immut
     @Override
     public int compareTo(LeashData o) {
         return ComparisonChain.start()
-                .compare(getValue().getType().getId(), o.leashHolder().get().getType().getId())
+                .compare(getValue().getUniqueId(), o.leashHolder().get().getUniqueId())
                 .result();
     }
 
     @Override
     public Value<Entity> leashHolder() {
-        return SpongeValueFactory.getInstance().createValue(Keys.LEASH_HOLDER, getValue());
+        return new SpongeEntityValue<>(Keys.LEASH_HOLDER, getValue());
     }
 
     @Override
