2c93993898b1fad25bc34b4f358672196e13cda5
kamcio96 <k.nadworski@icloud.com>
2016-01-08 15:23:41 +0100

    Update TargetedLocationDataProcessor to AbstractEntitySingleDataProcessor

--- src/main/java/org/spongepowered/common/data/processor/data/TargetedLocationDataProcessor.java
+++ src/main/java/org/spongepowered/common/data/processor/data/TargetedLocationDataProcessor.java
@@ -24,57 +24,67 @@
  */
 package org.spongepowered.common.data.processor.data;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.item.EntityEnderEye;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.pathfinding.PathEntity;
 import net.minecraft.pathfinding.PathPoint;
 import net.minecraft.util.BlockPos;
-import org.spongepowered.api.data.DataContainer;
 import org.spongepowered.api.data.DataHolder;
 import org.spongepowered.api.data.DataTransactionResult;
-import org.spongepowered.api.data.key.Key;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.data.manipulator.immutable.ImmutableTargetedLocationData;
 import org.spongepowered.api.data.manipulator.mutable.TargetedLocationData;
-import org.spongepowered.api.data.merge.MergeFunction;
-import org.spongepowered.api.data.value.BaseValue;
+import org.spongepowered.api.data.value.immutable.ImmutableValue;
+import org.spongepowered.api.data.value.mutable.Value;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 import org.spongepowered.common.SpongeImpl;
-import org.spongepowered.common.data.manipulator.immutable.ImmutableSpongeTargetedLocationData;
-import org.spongepowered.common.data.manipulator.mutable.SpongeTargetedLocationData;
-import org.spongepowered.common.data.processor.common.AbstractSpongeDataProcessor;
-import org.spongepowered.common.data.util.DataUtil;
+import org.spongepowered.common.data.processor.common.AbstractEntitySingleDataProcessor;
+import org.spongepowered.common.data.value.immutable.ImmutableSpongeValue;
 import org.spongepowered.common.interfaces.IMixinEntityPlayer;
+import org.spongepowered.common.util.VecHelper;
 
 import java.util.Optional;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+public class TargetedLocationDataProcessor extends AbstractEntitySingleDataProcessor<net.minecraft.entity.Entity, Location<World>, Value<Location<World>>, TargetedLocationData, ImmutableTargetedLocationData> {
 
-public class TargetedLocationDataProcessor extends AbstractSpongeDataProcessor<TargetedLocationData, ImmutableTargetedLocationData> {
-
-    @Override
-    public boolean supports(DataHolder dataHolder) {
-        return dataHolder instanceof EntityEnderEye || dataHolder instanceof EntityLiving;
+    public TargetedLocationDataProcessor() {
+        super(net.minecraft.entity.Entity.class, Keys.TARGETED_LOCATION);
     }
 
     @Override
-    public Optional<TargetedLocationData> from(DataHolder dataHolder) {
+    protected boolean set(Entity dataHolder, Location<World> value) {
+        if(dataHolder instanceof EntityPlayer) {
+            ((IMixinEntityPlayer) dataHolder).setCompassLocation(VecHelper.toBlockPos(value));
+            return true;
+        } else if(dataHolder instanceof EntityLiving) {
+            final EntityLiving entity = (EntityLiving) dataHolder;
+            entity.getNavigator().clearPathEntity();
+            entity.getNavigator().setPath(new PathEntity(new PathPoint[]{new PathPoint(value.getBlockX(), value.getBlockY(), value.getBlockZ())}), entity.getAIMoveSpeed());
+            return true;
+        } else if(dataHolder instanceof EntityEnderEye) {
+            // TODO
+        }
+        return false;
+    }
+
+    @Override
+    protected Optional<Location<World>> getVal(Entity dataHolder) {
         if(dataHolder instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) dataHolder;
             IMixinEntityPlayer mixinPlayer = (IMixinEntityPlayer) dataHolder;
             BlockPos pos = mixinPlayer.getCompassLocation();
             if(pos == null) pos = player.getEntityWorld().getSpawnPoint();
-            final TargetedLocationData data = new SpongeTargetedLocationData(new Location<>((World) player.getEntityWorld(), pos.getX(), pos.getY(), pos.getZ()));
-            return Optional.of(data);
+            return Optional.of(new Location<>((World) player.getEntityWorld(), pos.getX(), pos.getY(), pos.getZ()));
         } else if(dataHolder instanceof EntityLiving) {
             final EntityLiving entity = (EntityLiving) dataHolder;
             final PathPoint path = entity.getNavigator().getPath().getFinalPathPoint();
             if(path == null) {
                 return Optional.empty();
             }
-            final TargetedLocationData data = new SpongeTargetedLocationData(new Location<>((World) entity.getEntityWorld(), path.xCoord, path.yCoord, path.zCoord));
-            return Optional.of(data);
+            return Optional.of(new Location<>((World) entity.getEntityWorld(), path.xCoord, path.yCoord, path.zCoord));
         } else if(dataHolder instanceof EntityEnderEye) {
             // TODO
         }
@@ -82,50 +92,19 @@ public class TargetedLocationDataProcessor extends AbstractSpongeDataProcessor<T
     }
 
     @Override
-    public Optional<TargetedLocationData> createFrom(DataHolder dataHolder) {
-        if (dataHolder instanceof EntityPlayer) {
-            return from(dataHolder);
-        } else if (dataHolder instanceof EntityLiving) {
-            if (((EntityLiving) dataHolder).getNavigator().getPath() != null) {
-                return from(dataHolder);
-            } else {
-                return Optional.empty();
-            }
-        } else if (dataHolder instanceof EntityEnderEye) {
-            return from(dataHolder);
-        }
-        return Optional.empty();
-        // just from method?
+    protected ImmutableValue<Location<World>> constructImmutableValue(Location<World> value) {
+        return new ImmutableSpongeValue<>(Keys.TARGETED_LOCATION, value);
     }
 
     @Override
-    public Optional<TargetedLocationData> fill(DataHolder dataHolder, TargetedLocationData manipulator, MergeFunction overlap) {
-        if (supports(dataHolder)) {
-            final TargetedLocationData data = from(dataHolder).orElse(null);
-            final TargetedLocationData newData = checkNotNull(overlap.merge(checkNotNull(manipulator), data));
-            final Location location = newData.target().get();
-            return Optional.of(manipulator.set(Keys.TARGETED_LOCATION, location));
-        }
-        return Optional.empty();
+    public boolean supports(DataHolder dataHolder) {
+        return dataHolder instanceof EntityEnderEye || dataHolder instanceof EntityLiving;
     }
 
     @Override
-    public Optional<TargetedLocationData> fill(DataContainer container, TargetedLocationData targetedLocationData) {
-        final Location location = DataUtil.getData(container, Keys.TARGETED_LOCATION, Location.class);
-        return Optional.of(targetedLocationData.set(Keys.TARGETED_LOCATION, location));
-    }
-
-    @Override
-    public DataTransactionResult set(DataHolder dataHolder, TargetedLocationData manipulator, MergeFunction function) {
-        return DataTransactionResult.failResult(manipulator.getValues());
-    }
-
-    @Override
-    public Optional<ImmutableTargetedLocationData> with(Key<? extends BaseValue<?>> key, Object value, ImmutableTargetedLocationData immutable) {
-        if (key == Keys.TARGETED_LOCATION) {
-            return Optional.<ImmutableTargetedLocationData>of(new ImmutableSpongeTargetedLocationData(immutable.target().get()));
-        }
-        return Optional.empty();
+    protected TargetedLocationData createManipulator() {
+        //return new SpongeTargetedLocationData(new Location<World>()); what there?
+        return null;
     }
 
     @Override
