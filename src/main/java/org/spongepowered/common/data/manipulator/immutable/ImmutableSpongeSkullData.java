d6f5cb1fb783bd5d6756b7953a026497cc2696ae
Aaron Hill <aa1ronham@gmail.com>
2016-02-22 19:46:38 -0500

    Fix default value for ImmutableSpongeSkullData
    Closes #530

--- src/main/java/org/spongepowered/common/data/manipulator/immutable/ImmutableSpongeSkullData.java
+++ src/main/java/org/spongepowered/common/data/manipulator/immutable/ImmutableSpongeSkullData.java
@@ -35,6 +35,6 @@ import org.spongepowered.common.data.manipulator.mutable.SpongeSkullData;
 public class ImmutableSpongeSkullData extends AbstractImmutableSingleCatalogData<SkullType, ImmutableSkullData, SkullData> implements ImmutableSkullData {
 
     public ImmutableSpongeSkullData(SkullType value) {
-        super(ImmutableSkullData.class, value, SkullTypes.ZOMBIE, Keys.SKULL_TYPE, SpongeSkullData.class);
+        super(ImmutableSkullData.class, value, SkullTypes.SKELETON, Keys.SKULL_TYPE, SpongeSkullData.class);
     }
 }
