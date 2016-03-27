3a460678fda8972f560645f8647038237679ed0a
Aaron Hill <aa1ronham@gmail.com>
2016-03-27 13:11:01 -0400

    Allow getting ItemTypes.NONE by id

--- src/main/java/org/spongepowered/common/registry/type/ItemTypeRegistryModule.java
+++ src/main/java/org/spongepowered/common/registry/type/ItemTypeRegistryModule.java
@@ -67,7 +67,7 @@ public final class ItemTypeRegistryModule implements SpongeAdditionalCatalogRegi
     @Override
     public Optional<ItemType> getById(String id) {
         checkNotNull(id);
-        if (!id.contains(":")) {
+        if (!id.contains(":") && !id.equals("none")) {
             id = "minecraft:" + id; // assume vanilla
         }
         return Optional.ofNullable(this.itemTypeMappings.get(id));
