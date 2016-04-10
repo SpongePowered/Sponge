830ed25ca70334e4df3eee63997f832b9a12c0ba
Nick Conde <nick@budgiehouse.com>
2016-04-10 11:55:04 -0700

    Change SubtypeFilterDelegate to a loop, fixes #643

--- src/main/java/org/spongepowered/common/event/filter/delegate/SubtypeFilterDelegate.java
+++ src/main/java/org/spongepowered/common/event/filter/delegate/SubtypeFilterDelegate.java
@@ -27,7 +27,11 @@ package org.spongepowered.common.event.filter.delegate;
 import static org.objectweb.asm.Opcodes.ACONST_NULL;
 import static org.objectweb.asm.Opcodes.ALOAD;
 import static org.objectweb.asm.Opcodes.ARETURN;
+import static org.objectweb.asm.Opcodes.ASTORE;
+import static org.objectweb.asm.Opcodes.CHECKCAST;
 import static org.objectweb.asm.Opcodes.GETFIELD;
+import static org.objectweb.asm.Opcodes.GOTO;
+import static org.objectweb.asm.Opcodes.IFNE;
 import static org.objectweb.asm.Opcodes.INVOKEINTERFACE;
 import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
 
@@ -47,18 +51,30 @@ public abstract class SubtypeFilterDelegate implements FilterDelegate {
 
     @Override
     public int write(String name, ClassWriter cw, MethodVisitor mv, Method method, int locals) {
-
+        // TODO could do an optimization here to inline a single instanceof if
+        // the set would contain only a single member
         mv.visitVarInsn(ALOAD, 0);
         mv.visitFieldInsn(GETFIELD, name, "classes", "Ljava/util/Set;");
+        // Loop through the classes set's members
+        mv.visitMethodInsn(INVOKEINTERFACE, "java/util/Set", "iterator", "()Ljava/util/Iterator;", true);
+        mv.visitVarInsn(ASTORE, locals);
+        Label continueLabel = new Label();
+        mv.visitJumpInsn(GOTO, continueLabel);
+        Label loopStart = new Label();
+        mv.visitLabel(loopStart);
+        mv.visitVarInsn(ALOAD, locals);
+        mv.visitMethodInsn(INVOKEINTERFACE, "java/util/Iterator", "next", "()Ljava/lang/Object;", true);
+        mv.visitTypeInsn(CHECKCAST, "java/lang/Class");
         mv.visitVarInsn(ALOAD, 1);
-        mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Object", "getClass", "()Ljava/lang/Class;", false);
-        mv.visitMethodInsn(INVOKEINTERFACE, "java/util/Set", "contains", "(Ljava/lang/Object;)Z", true);
-        Label success = new Label();
-        mv.visitJumpInsn(getJumpOp(), success);
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Class", "isInstance", "(Ljava/lang/Object;)Z", false);
+        mv.visitJumpInsn(getJumpOp(), continueLabel);
         mv.visitInsn(ACONST_NULL);
         mv.visitInsn(ARETURN);
-        mv.visitLabel(success);
-        return locals;
+        mv.visitLabel(continueLabel);
+        mv.visitVarInsn(ALOAD, locals);
+        mv.visitMethodInsn(INVOKEINTERFACE, "java/util/Iterator", "hasNext", "()Z", true);
+        mv.visitJumpInsn(IFNE, loopStart);
+        return locals + 1;
     }
 
     public abstract void writeCtor(String name, ClassWriter cw, MethodVisitor mv);
