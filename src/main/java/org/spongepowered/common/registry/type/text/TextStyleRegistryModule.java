2352257a11d673e8ec3b122c2105ff2d363b2f1a
simon816 <simonbarnes1@googlemail.com>
2016-04-14 02:48:21 +0100

    Fix mapping warning on TextStyles.NONE
    Remove whitespace on common_at.cfg

--- src/main/java/org/spongepowered/common/registry/type/text/TextStyleRegistryModule.java
+++ src/main/java/org/spongepowered/common/registry/type/text/TextStyleRegistryModule.java
@@ -39,7 +39,7 @@ import java.util.Optional;
 
 public final class TextStyleRegistryModule implements CatalogRegistryModule<TextStyle.Base> {
 
-    @RegisterCatalog(TextStyles.class)
+    @RegisterCatalog(value = TextStyles.class, ignoredFields = "NONE")
     public static final ImmutableMap<String, TextStyle.Base> textStyleMappings = ImmutableMap.<String, TextStyle.Base>builder()
         .put("bold", SpongeTextStyle.of(EnumChatFormatting.BOLD))
         .put("italic", SpongeTextStyle.of(EnumChatFormatting.ITALIC))
