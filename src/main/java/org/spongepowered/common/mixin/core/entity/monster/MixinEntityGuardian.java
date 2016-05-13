b013130839bb5911fb519e6cbfe2442b7e57afa6
Aaron Hill <aa1ronham@gmail.com>
2016-05-12 21:40:58 -0400

    Implement guardian beam target

--- src/main/java/org/spongepowered/common/mixin/core/entity/monster/MixinEntityGuardian.java
+++ src/main/java/org/spongepowered/common/mixin/core/entity/monster/MixinEntityGuardian.java
@@ -24,11 +24,13 @@
  */
 package org.spongepowered.common.mixin.core.entity.monster;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityGuardian;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.data.manipulator.DataManipulator;
 import org.spongepowered.api.data.manipulator.mutable.entity.ElderData;
 import org.spongepowered.api.data.value.mutable.Value;
+import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.living.monster.Guardian;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -36,11 +38,13 @@ import org.spongepowered.common.data.manipulator.mutable.entity.SpongeElderData;
 import org.spongepowered.common.data.value.mutable.SpongeValue;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mixin(EntityGuardian.class)
 public abstract class MixinEntityGuardian extends MixinEntityMob implements Guardian {
 
     @Shadow public abstract boolean isElder();
+    @Shadow public abstract void setTargetedEntity(int entityId);
 
     @Override
     public ElderData getElderData() {
@@ -56,5 +60,19 @@ public abstract class MixinEntityGuardian extends MixinEntityMob implements Guar
     public void supplyVanillaManipulators(List<DataManipulator<?, ?>> manipulators) {
         super.supplyVanillaManipulators(manipulators);
         manipulators.add(getElderData());
+    }
+
+    @Override
+    public Optional<Living> getBeamTarget() {
+        return Optional.ofNullable((Living) this.worldObj.getEntityByID(this.dataWatcher.getWatchableObjectInt(17)));
+    }
+
+    @Override
+    public void setBeamTarget(Living entity) {
+        if (entity == null) {
+            this.setTargetedEntity(0);
+        } else {
+            this.setTargetedEntity(((EntityLivingBase) entity).getEntityId());
+        }
     }
 }
