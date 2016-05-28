6f65180839dd997ea4b8b60d4c5894eda9118e35
bloodshot <jdroque@gmail.com>
2016-05-28 00:17:25 -0400

    Fix entity activation.

--- src/main/java/org/spongepowered/common/mixin/entityactivation/MixinWorldServer_Activation.java
+++ src/main/java/org/spongepowered/common/mixin/entityactivation/MixinWorldServer_Activation.java
@@ -29,12 +29,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.common.interfaces.world.IMixinWorld;
 import org.spongepowered.common.mixin.plugin.entityactivation.ActivationRange;
 
 @NonnullByDefault
-@Mixin(value = net.minecraft.world.World.class, priority = 1005)
-public abstract class MixinWorld_Activation implements IMixinWorld {
+@Mixin(value = net.minecraft.world.WorldServer.class, priority = 1005)
+public abstract class MixinWorldServer_Activation {
 
     @Inject(method = "updateEntities()V", at = @At(value = "INVOKE_STRING",
             target = "Lnet/minecraft/profiler/Profiler;endStartSection(Ljava/lang/String;)V", args = {"ldc=regular"}))
