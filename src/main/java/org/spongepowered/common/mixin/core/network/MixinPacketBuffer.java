5695907b7796afcfa94357052f43c46f9ff3f5de
Minecrell <dev@minecrell.net>
2016-05-28 15:25:51 +0200

    Fix ChannelBuf#slice not returning the result

--- src/main/java/org/spongepowered/common/mixin/core/network/MixinPacketBuffer.java
+++ src/main/java/org/spongepowered/common/mixin/core/network/MixinPacketBuffer.java
@@ -41,6 +41,7 @@ import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.common.data.persistence.NbtTranslator;
+import org.spongepowered.common.network.SpongeNetworkManager;
 
 import java.io.IOException;
 import java.nio.ByteOrder;
@@ -137,13 +138,11 @@ public abstract class MixinPacketBuffer extends ByteBuf {
     }
 
     public ChannelBuf cbuf$slice() {
-        this.slice();
-        return (ChannelBuf) this;
+        return SpongeNetworkManager.toChannelBuf(this.slice());
     }
 
     public ChannelBuf cbuf$slice(int index, int length) {
-        this.slice(index, length);
-        return (ChannelBuf) this;
+        return SpongeNetworkManager.toChannelBuf(this.slice(index, length));
     }
 
     @Intrinsic
