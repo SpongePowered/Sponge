ac4906455c4e6b6ccfdb745e8fa9db170a7f38ea
Minecrell <dev@minecrell.net>
2016-06-04 12:11:48 +0200

    Mixin backwards compatibility methods to API interface
    Fixes SpongePowered/SpongeForge#660

--- src/main/java/org/spongepowered/common/mixin/api/entity/MixinEntityBackwardsCompatible.java
+++ src/main/java/org/spongepowered/common/mixin/api/entity/MixinEntityBackwardsCompatible.java
@@ -22,13 +22,16 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.common.interfaces.entity;
+package org.spongepowered.common.mixin.api.entity;
 
 import com.flowpowered.math.vector.Vector3d;
+import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.Transform;
 import org.spongepowered.api.util.RelativePositions;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.EnumSet;
 
@@ -36,17 +39,31 @@ import java.util.EnumSet;
  * THIS IS EXPLICITLY PROVIDED ONLY FOR A BACKWARDS COMPATIBILTY SUPPORT FOR
  * THE REMAINDER OF API 4.X, SINCE A SMALL CHANGE IN THE METHOD SIGNATURES
  * WERE A BREAKING CHANGE NECESSARY FOR A PORTAL/TELEPORTATION API IMPLEMENTATION,
- * THESE METHODS ARE PREFIX IMPLEMENTED ONLY FOR OLDER PLUGINS. THESE METHODS
- * AND INTERFACE SHOULD NEVER BE USED IN THE IMPLEMENTATION OTHERWISE.
+ * THESE METHODS ARE IMPLEMENTED ONLY FOR OLDER PLUGINS. THESE METHODS AND
+ * INTERFACE SHOULD NEVER BE USED IN THE IMPLEMENTATION OTHERWISE.
  */
-public interface IMixinEntityBackwardsCompatible {
+@Mixin(Entity.class)
+public interface MixinEntityBackwardsCompatible {
 
-    void setLocation(Location<World> location);
+    @Shadow boolean shadow$setLocation(Location<World> location);
+    @Shadow boolean shadow$setLocationAndRotation(Location<World> location, Vector3d rotation);
+    @Shadow boolean shadow$setLocationAndRotation(Location<World> location, Vector3d rotation, EnumSet<RelativePositions> relativePositions);
+    @Shadow boolean shadow$setTransform(Transform<World> transform);
 
-    void setLocationAndRotation(Location<World> location, Vector3d rotation);
+    default void setLocation(Location<World> location) {
+        shadow$setLocation(location);
+    }
 
-    void setLocationAndRotation(Location<World> location, Vector3d rotation, EnumSet<RelativePositions> relativePositions);
+    default void setLocationAndRotation(Location<World> location, Vector3d rotation) {
+        shadow$setLocationAndRotation(location, rotation);
+    }
 
-    void setTransform(Transform<World> transform);
+    default void setLocationAndRotation(Location<World> location, Vector3d rotation, EnumSet<RelativePositions> relativePositions) {
+        shadow$setLocationAndRotation(location, rotation, relativePositions);
+    }
+
+    default void setTransform(Transform<World> transform) {
+        shadow$setTransform(transform);
+    }
 
 }
