47f20888b80a72bc63c2ec454f113fb7bde491bc
Aaron Hill <aa1ronham@gmail.com>
2016-06-11 15:26:09 -0400

    Only log a message when parsing an invalid ClickEvent URL

--- src/main/java/org/spongepowered/common/mixin/core/text/MixinClickEvent.java
+++ src/main/java/org/spongepowered/common/mixin/core/text/MixinClickEvent.java
@@ -33,6 +33,7 @@ import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.common.SpongeImpl;
 import org.spongepowered.common.interfaces.text.IMixinClickEvent;
 import org.spongepowered.common.text.action.SpongeCallbackHolder;
 
@@ -60,7 +61,7 @@ public abstract class MixinClickEvent implements IMixinClickEvent {
                         try {
                             setHandle(TextActions.openUrl(new URL(this.value)));
                         } catch (MalformedURLException e) {
-                            throw new IllegalArgumentException("Invalid URL: " + this.value, e);
+                            SpongeImpl.getLogger().debug("Tried to parse invalid URL \"{}\": {}", this.value, e.getMessage());
                         }
                         break;
                     case RUN_COMMAND:
