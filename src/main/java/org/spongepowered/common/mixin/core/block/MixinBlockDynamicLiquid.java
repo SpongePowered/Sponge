feccdf2bbc545ecf457278d3c894f5c21bed636e
bloodshot <jdroque@gmail.com>
2016-07-06 15:54:37 -0400

    Call 'ChangeBlockEvent.Pre' before liquids flow into a block area.

---
+++ src/main/java/org/spongepowered/common/mixin/core/block/MixinBlockDynamicLiquid.java
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.mixin.core.block;
+
+import net.minecraft.block.BlockDynamicLiquid;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockPos;
+import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.common.SpongeImpl;
+import org.spongepowered.common.event.CauseTracker;
+import org.spongepowered.common.interfaces.world.IMixinWorld;
+
+@Mixin(BlockDynamicLiquid.class)
+public abstract class MixinBlockDynamicLiquid {
+
+    @Inject(method = "canFlowInto", at = @At("HEAD"), cancellable = true)
+    public void onCanFlowInto(net.minecraft.world.World worldIn, BlockPos pos, IBlockState state, CallbackInfoReturnable<Boolean> cir) {
+        if (worldIn.isRemote) {
+            return;
+        }
+
+        final CauseTracker causeTracker = ((IMixinWorld) worldIn).getCauseTracker();
+        if (!causeTracker.hasTickingBlock()) {
+            // safety measure
+            return;
+        }
+
+        Location<World> location = new Location<>((World) worldIn, pos.getX(), pos.getY(), pos.getZ());
+        ChangeBlockEvent.Pre event = SpongeEventFactory.createChangeBlockEventPre(Cause.source(causeTracker.getCurrentTickBlock().get()).build(), location, (World) worldIn);
+        SpongeImpl.postEvent(event);
+        if (event.isCancelled()) {
+            cir.setReturnValue(false);
+        }
+    }
+}
