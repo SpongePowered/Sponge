8de28d0caab6418349e940e444a1e7d18fb04254
Minecrell <dev@minecrell.net>
2016-08-01 17:24:02 +0200

    Move chunk GC unloading mixin to common and convert to redirect

--- src/main/java/org/spongepowered/common/mixin/core/server/management/MixinPlayerChunkMap.java
+++ src/main/java/org/spongepowered/common/mixin/core/server/management/MixinPlayerChunkMap.java
@@ -26,12 +26,15 @@ package org.spongepowered.common.mixin.core.server.management;
 
 import net.minecraft.server.management.PlayerChunkMap;
 import net.minecraft.server.management.PlayerChunkMapEntry;
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.ChunkProviderServer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.common.interfaces.IMixinChunk;
 import org.spongepowered.common.interfaces.server.management.IMixinPlayerChunkMap;
 import org.spongepowered.common.interfaces.world.IMixinWorldServer;
 
@@ -40,6 +43,8 @@ import javax.annotation.Nullable;
 @Mixin(PlayerChunkMap.class)
 public abstract class MixinPlayerChunkMap implements IMixinPlayerChunkMap {
 
+    @Shadow @Final private WorldServer theWorldServer;
+
     @Shadow @Nullable public abstract PlayerChunkMapEntry getEntry(int chunkX, int chunkZ);
 
     @Override
@@ -47,4 +52,23 @@ public abstract class MixinPlayerChunkMap implements IMixinPlayerChunkMap {
         PlayerChunkMapEntry playerInstance = this.getEntry(x, z);
         return playerInstance != null && playerInstance.players.size() > 0;
     }
+
+    @Redirect(method = "removeEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/ChunkProviderServer;"
+            + "unload(Lnet/minecraft/world/chunk/Chunk;)V"))
+    private void onUnloadChunk(ChunkProviderServer chunkProvider, Chunk chunk) {
+        // We remove the ability for a PlayerChunkMap to queue chunks for unload to prevent chunk thrashing
+        // where the same chunks repeatedly unload and load. This is caused by a player moving in and out of the same chunks.
+        // Instead, the Chunk GC will now be responsible for going through loaded chunks and queuing any chunk where no player
+        // is within view distance or a spawn chunk is force loaded. However, if the Chunk GC is disabled then we will fall back to vanilla
+        // and queue the chunk to be unloaded.
+        // -- blood
+
+        if (((IMixinWorldServer) this.theWorldServer).getChunkGCTickInterval() <= 0
+                || ((IMixinWorldServer) this.theWorldServer).getChunkUnloadDelay() <= 0) {
+            chunkProvider.unload(chunk);
+        } else {
+            ((IMixinChunk) chunk).setScheduledForUnload(System.currentTimeMillis());
+        }
+    }
+
 }
