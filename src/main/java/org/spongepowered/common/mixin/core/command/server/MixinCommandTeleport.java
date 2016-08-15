ee1c37507f2e7c1882fca7e6a9ed3ba3cfe08f81
Aaron Hill <aa1ronham@gmail.com>
2016-08-15 16:48:11 -0400

    Update command mixins for new '/teleport' command

--- src/main/java/org/spongepowered/common/mixin/core/command/server/MixinCommandTeleport.java
+++ src/main/java/org/spongepowered/common/mixin/core/command/server/MixinCommandTeleport.java
@@ -27,17 +27,26 @@ package org.spongepowered.common.mixin.core.command.server;
 import com.flowpowered.math.vector.Vector3d;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.command.server.CommandTeleport;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.SPacketPlayerPosLook;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.spongepowered.common.event.SpongeCommonEventFactory;
 
 import java.util.EnumSet;
@@ -46,178 +55,130 @@ import java.util.Set;
 @Mixin(CommandTeleport.class)
 public abstract class MixinCommandTeleport extends CommandBase {
 
+    // This boolean is added in order to make minimal changes to 'execute'.
+    // It is set to true if the events fired in 'func_189863_a' are not cancelled.
+    // This allows us to prevent calling 'notifyCommandListener' if the event is cancelled.
+    private static boolean shouldNotifyCommandListener = false;
+
     /**
-     * @author blood - May 31st, 2016
-     * @author gabizou - May 31st, 2016 - Update to 1.9.4
-     * @reason to fix LVT errors with SpongeForge
-     *
-     * @param sender The command source
-     * @param args The command arguments
+     * @author Aaron1011 - August 15, 2016 - Prevent 'notifyCommandListener' from being called the event is cancelled
      */
-    @Override
     @Overwrite
+    @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
-        if (args.length < 1)
+        if (args.length < 4)
         {
-            throw new WrongUsageException("commands.tp.usage");
+            throw new WrongUsageException("commands.teleport.usage", new Object[0]);
         }
         else
         {
-            int i = 0;
-            Entity entity;
+            Entity entity = getEntity(server, sender, args[0]);
 
-            if (args.length != 2 && args.length != 4 && args.length != 6)
+            if (entity.worldObj != null)
             {
-                entity = getCommandSenderAsPlayer(sender);
-            }
-            else
-            {
-                entity = getEntity(server, sender, args[0]);
-                i = 1;
-            }
+                int i = 4096;
+                Vec3d vec3d = sender.getPositionVector();
+                int j = 1;
+                CommandBase.CoordinateArg commandbase$coordinatearg = parseCoordinate(vec3d.xCoord, args[j++], true);
+                CommandBase.CoordinateArg commandbase$coordinatearg1 = parseCoordinate(vec3d.yCoord, args[j++], -4096, 4096, false);
+                CommandBase.CoordinateArg commandbase$coordinatearg2 = parseCoordinate(vec3d.zCoord, args[j++], true);
+                Entity entity1 = sender.getCommandSenderEntity() == null ? entity : sender.getCommandSenderEntity();
+                CommandBase.CoordinateArg commandbase$coordinatearg3 = parseCoordinate(args.length > j ? (double)entity1.rotationYaw : (double)entity.rotationYaw, args.length > j ? args[j] : "~", false);
+                ++j;
+                CommandBase.CoordinateArg commandbase$coordinatearg4 = parseCoordinate(args.length > j ? (double)entity1.rotationPitch : (double)entity.rotationPitch, args.length > j ? args[j] : "~", false);
+                // Sponge start - check shouldNotifyCommandListener before calling 'notifyCommandListener'
 
-            if (args.length != 1 && args.length != 2)
-            {
-                if (args.length < i + 3)
-                {
-                    throw new WrongUsageException("commands.tp.usage");
+                // Guard against any possible re-entrance
+                boolean shouldNotify = shouldNotifyCommandListener;
+
+                func_189862_a(entity, commandbase$coordinatearg, commandbase$coordinatearg1, commandbase$coordinatearg2, commandbase$coordinatearg3, commandbase$coordinatearg4);
+                if (shouldNotifyCommandListener) {
+                    notifyCommandListener(sender, this, "commands.tp.success.coordinates", new Object[] {entity.getName(), Double.valueOf(commandbase$coordinatearg.getResult()), Double.valueOf(commandbase$coordinatearg1.getResult()), Double.valueOf(commandbase$coordinatearg2.getResult())});
                 }
-                else if (entity.worldObj != null)
-                {
-                    int lvt_5_2_ = i + 1;
-                    CommandBase.CoordinateArg commandbase$coordinatearg = parseCoordinate(entity.posX, args[i], true);
-                    CommandBase.CoordinateArg commandbase$coordinatearg1 = parseCoordinate(entity.posY, args[lvt_5_2_++], 0, 0, false);
-                    CommandBase.CoordinateArg commandbase$coordinatearg2 = parseCoordinate(entity.posZ, args[lvt_5_2_++], true);
-                    CommandBase.CoordinateArg commandbase$coordinatearg3 = parseCoordinate((double)entity.rotationYaw, args.length > lvt_5_2_ ? args[lvt_5_2_++] : "~", false);
-                    CommandBase.CoordinateArg commandbase$coordinatearg4 = parseCoordinate((double)entity.rotationPitch, args.length > lvt_5_2_ ? args[lvt_5_2_] : "~", false);
-
-                    if (entity instanceof EntityPlayerMP)
-                    {
-                        Set<SPacketPlayerPosLook.EnumFlags> set = EnumSet.noneOf(SPacketPlayerPosLook.EnumFlags.class);
-
-                        if (commandbase$coordinatearg.isRelative())
-                        {
-                            set.add(SPacketPlayerPosLook.EnumFlags.X);
-                        }
-
-                        if (commandbase$coordinatearg1.isRelative())
-                        {
-                            set.add(SPacketPlayerPosLook.EnumFlags.Y);
-                        }
-
-                        if (commandbase$coordinatearg2.isRelative())
-                        {
-                            set.add(SPacketPlayerPosLook.EnumFlags.Z);
-                        }
-
-                        if (commandbase$coordinatearg4.isRelative())
-                        {
-                            set.add(SPacketPlayerPosLook.EnumFlags.X_ROT);
-                        }
-
-                        if (commandbase$coordinatearg3.isRelative())
-                        {
-                            set.add(SPacketPlayerPosLook.EnumFlags.Y_ROT);
-                        }
-
-                        float f = (float)commandbase$coordinatearg3.getAmount();
-
-                        if (!commandbase$coordinatearg3.isRelative())
-                        {
-                            f = MathHelper.wrapDegrees(f);
-                        }
-
-                        float f1 = (float)commandbase$coordinatearg4.getAmount();
-
-                        if (!commandbase$coordinatearg4.isRelative())
-                        {
-                            f1 = MathHelper.wrapDegrees(f1);
-                        }
-
-
-                        // Sponge start
-                        EntityPlayerMP player = (EntityPlayerMP) entity;
-                        double x = commandbase$coordinatearg.getAmount();
-                        double y = commandbase$coordinatearg1.getAmount();
-                        double z = commandbase$coordinatearg2.getAmount();
-                        MoveEntityEvent.Teleport event = SpongeCommonEventFactory.handleDisplaceEntityTeleportEvent(player, x, y, z, f, f1);
-                        if (event.isCancelled()) {
-                            return;
-                        }
-
-                        entity.dismountRidingEntity();
-                        Vector3d position = event.getToTransform().getPosition();
-                        ((EntityPlayerMP)entity).connection.setPlayerLocation(position.getX(), position.getY(), position.getZ(), (float) event.getToTransform().getYaw(), (float) event.getToTransform().getPitch(), set);
-                        entity.setRotationYawHead((float) event.getToTransform().getYaw());
-                        // Sponge end
-                    }
-                    else
-                    {
-                        float f2 = (float)MathHelper.wrapDegrees(commandbase$coordinatearg3.getResult());
-                        float f3 = (float)MathHelper.wrapDegrees(commandbase$coordinatearg4.getResult());
-
-                        f3 = MathHelper.clamp_float(f3, -90.0F, 90.0F);
-
-                        // Sponge start
-                        double x = commandbase$coordinatearg.getResult();
-                        double y = commandbase$coordinatearg1.getResult();
-                        double z = commandbase$coordinatearg2.getResult();
-                        MoveEntityEvent.Teleport event = SpongeCommonEventFactory.handleDisplaceEntityTeleportEvent(entity, x, y, z, f2, f3);
-                        if (event.isCancelled()) {
-                            return;
-                        }
-
-                        Vector3d position = event.getToTransform().getPosition();
-                        entity.setLocationAndAngles(position.getX(), position.getY(), position.getZ(), (float) event.getToTransform().getYaw(), (float) event.getToTransform().getPitch());
-                        entity.setRotationYawHead((float) event.getToTransform().getYaw());
-                        // Sponge end
-                    }
-
-                    notifyCommandListener(sender, this, "commands.tp.success.coordinates", entity.getName(), commandbase$coordinatearg.getResult(), commandbase$coordinatearg1.getResult(), commandbase$coordinatearg2.getResult());
-                }
-            }
-            else
-            {
-                Entity entity1 = getEntity(server, sender, args[args.length - 1]);
-
-                if (entity1.worldObj != entity.worldObj)
-                {
-                    throw new CommandException("commands.tp.notSameDimension");
-                }
-                else
-                {
-                    entity.dismountRidingEntity();
-
-                    if (entity instanceof EntityPlayerMP)
-                    {
-                        // Sponge start
-                        EntityPlayerMP player = (EntityPlayerMP) entity;
-                        MoveEntityEvent.Teleport event = SpongeCommonEventFactory.handleDisplaceEntityTeleportEvent(entity, entity1.posX, entity1.posY, entity1.posZ, entity1.rotationYaw, entity1.rotationPitch);
-                        if (event.isCancelled()) {
-                            return;
-                        }
-
-                        Vector3d position = event.getToTransform().getPosition();
-                        player.connection.setPlayerLocation(position.getX(), position.getY(), position.getZ(), (float) event.getToTransform().getYaw(), (float) event.getToTransform().getPitch());
-                        // Sponge end
-                    }
-                    else
-                    {
-                        // Sponge Start - Events
-                        MoveEntityEvent.Teleport event = SpongeCommonEventFactory.handleDisplaceEntityTeleportEvent(entity, entity1.posX, entity1.posY, entity1.posZ, entity1.rotationYaw, entity1.rotationPitch);
-                        if (event.isCancelled()) {
-                            return;
-                        }
-
-                        Vector3d position = event.getToTransform().getPosition();
-                        entity.setLocationAndAngles(position.getX(), position.getY(), position.getZ(), (float) event.getToTransform().getYaw(), (float) event.getToTransform().getPitch());
-                        // Sponge End
-                    }
-
-                    notifyCommandListener(sender, this, "commands.tp.success", entity.getName(), entity1.getName());
-                }
+                shouldNotifyCommandListener = shouldNotify;
+                // Sponge end
             }
         }
     }
+
+    /**
+     * @author Aaron1011 - August 15, 2016 - Muliple modification points are needed, so an overwrite is easier
+     */
+    @Overwrite
+    private static void func_189862_a(Entity p_189862_0_, CommandBase.CoordinateArg p_189862_1_, CommandBase.CoordinateArg p_189862_2_, CommandBase.CoordinateArg p_189862_3_, CommandBase.CoordinateArg p_189862_4_, CommandBase.CoordinateArg p_189862_5_)
+    {
+        if (p_189862_0_ instanceof EntityPlayerMP)
+        {
+            Set<SPacketPlayerPosLook.EnumFlags> set = EnumSet.<SPacketPlayerPosLook.EnumFlags>noneOf(SPacketPlayerPosLook.EnumFlags.class);
+            float f = (float)p_189862_4_.getAmount();
+
+            if (p_189862_4_.isRelative())
+            {
+                set.add(SPacketPlayerPosLook.EnumFlags.Y_ROT);
+            }
+            else
+            {
+                f = MathHelper.wrapDegrees(f);
+            }
+
+            float f1 = (float)p_189862_5_.getAmount();
+
+            if (p_189862_5_.isRelative())
+            {
+                set.add(SPacketPlayerPosLook.EnumFlags.X_ROT);
+            }
+            else
+            {
+                f1 = MathHelper.wrapDegrees(f1);
+            }
+
+            // Sponge start
+            EntityPlayerMP player = (EntityPlayerMP) p_189862_0_;
+            double x = p_189862_1_.getAmount();
+            double y = p_189862_2_.getAmount();
+            double z = p_189862_3_.getAmount();
+            MoveEntityEvent.Teleport event = SpongeCommonEventFactory.handleDisplaceEntityTeleportEvent(player, x, y, z, f, f1);
+            if (event.isCancelled()) {
+                return;
+            }
+
+            p_189862_0_.dismountRidingEntity();
+            Vector3d position = event.getToTransform().getPosition();
+            ((EntityPlayerMP)p_189862_0_).connection.setPlayerLocation(position.getX(), position.getY(), position.getZ(), (float) event.getToTransform().getYaw(), (float) event.getToTransform().getPitch(), set);
+            p_189862_0_.setRotationYawHead((float) event.getToTransform().getYaw());
+            // Sponge end
+        }
+        else
+        {
+            float f2 = (float)MathHelper.wrapDegrees(p_189862_4_.getResult());
+            float f3 = (float)MathHelper.wrapDegrees(p_189862_5_.getResult());
+            f3 = MathHelper.clamp_float(f3, -90.0F, 90.0F);
+
+            // Sponge start
+            double x = p_189862_1_.getResult();
+            double y = p_189862_2_.getResult();
+            double z = p_189862_3_.getResult();
+            MoveEntityEvent.Teleport event = SpongeCommonEventFactory.handleDisplaceEntityTeleportEvent(p_189862_0_, x, y, z, f2, f3);
+            if (event.isCancelled()) {
+                return;
+            }
+
+            Vector3d position = event.getToTransform().getPosition();
+            p_189862_0_.setLocationAndAngles(position.getX(), position.getY(), position.getZ(), (float) event.getToTransform().getYaw(), (float) event.getToTransform().getPitch());
+            p_189862_0_.setRotationYawHead((float) event.getToTransform().getYaw());
+            // Sponge end
+        }
+
+        if (!(p_189862_0_ instanceof EntityLivingBase) || !((EntityLivingBase)p_189862_0_).isElytraFlying())
+        {
+            p_189862_0_.motionY = 0.0D;
+            p_189862_0_.onGround = true;
+        }
+
+        // Sponge start - set 'shouldNotifyCommandListener' to 'true' if we make it to the end of the method (the event wasn't cancelled)
+        shouldNotifyCommandListener = true;
+        // Sponge end
+    }
+
 }
