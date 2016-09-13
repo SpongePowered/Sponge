7fff65cdd554eb2e4398cc1c867a2e0d24e1d09f
Kenzie Togami <ket1999@gmail.com>
2016-09-12 20:39:20 -0700

    Some general patches for selector resolving
    Still debating if we just want to delegate to Mojang for this -- their
    implementation is less efficient but will be present every update...

--- src/main/java/org/spongepowered/common/text/selector/SpongeSelectorFactory.java
+++ src/main/java/org/spongepowered/common/text/selector/SpongeSelectorFactory.java
@@ -25,6 +25,7 @@
 package org.spongepowered.common.text.selector;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
@@ -222,6 +223,8 @@ public class SpongeSelectorFactory implements SelectorFactory {
     public <T> SpongeArgumentType.Invertible<T> createInvertibleArgumentType(
             String key, Class<T> type, String converterKey) {
         if (!this.argumentLookupMap.containsKey(key)) {
+            checkNotNull(converterKey, "converter key cannot be null");
+            checkNotNull(converterKey, "converter key cannot be null");
             this.argumentLookupMap.put(key,
                                        new SpongeArgumentType.Invertible<>(key, type,
                                                                            converterKey));
