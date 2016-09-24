7f41b4b5742d92981145b6f9c3431b6f719f470b
Minecrell <dev@minecrell.net>
2016-09-24 14:19:14 +0200

    Update SpongeAPI

--- src/main/java/org/spongepowered/common/SpongeImpl.java
+++ src/main/java/org/spongepowered/common/SpongeImpl.java
@@ -67,7 +67,6 @@ public final class SpongeImpl {
     public static final String GAME_ID = "minecraft";
     public static final String GAME_NAME = "Minecraft";
 
-    public static final String API_ID = "spongeapi";
     public static final String API_NAME = firstNonNull(getPackage().getSpecificationTitle(), "SpongeAPI");
     public static final Optional<String> API_VERSION = Optional.ofNullable(getPackage().getSpecificationVersion());
 
