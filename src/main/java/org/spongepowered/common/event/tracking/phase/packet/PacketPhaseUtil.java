d213fdc81b631125d65053c954e72ad2c120851b
Aaron Hill <aa1ronham@gmail.com>
2016-10-23 17:18:27 -0400

    Fire 'fake' transaction with proper ItemStack, rather than always NONe
    Lack of changes doesn't always imply that the underlying slot is empty.
    For example, if the pickup event prior to a double-click event is
    cancelled, then the second click will actually be on a full slot
    (assuming the restore packet reaches the client fast enough)

--- src/main/java/org/spongepowered/common/event/tracking/phase/packet/PacketPhaseUtil.java
+++ src/main/java/org/spongepowered/common/event/tracking/phase/packet/PacketPhaseUtil.java
@@ -86,13 +86,14 @@ public final class PacketPhaseUtil {
     }
 
     public static void validateCapturedTransactions(int slotId, Container openContainer, List<SlotTransaction> capturedTransactions) {
-        /*if (capturedTransactions.size() == 0 && slotId >= 0) {
+        if (capturedTransactions.size() == 0 && slotId >= 0) {
             final Slot slot = openContainer.getSlot(slotId);
             if (slot != null) {
-                final SlotTransaction slotTransaction = new SlotTransaction(new SlotAdapter(slot), ItemStackSnapshot.NONE, ItemStackSnapshot.NONE);
+                ItemStackSnapshot snapshot = slot.getHasStack() ? ((org.spongepowered.api.item.inventory.ItemStack) slot.getStack()).createSnapshot() : ItemStackSnapshot.NONE;
+                final SlotTransaction slotTransaction = new SlotTransaction(new SlotAdapter(slot), snapshot, snapshot);
                 capturedTransactions.add(slotTransaction);
             }
-        }*/
+        }
     }
 
     public static void handlePlayerSlotRestore(EntityPlayerMP player, ItemStack itemStack, EnumHand hand) {
