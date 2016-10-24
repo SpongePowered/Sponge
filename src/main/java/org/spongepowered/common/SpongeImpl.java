71b61bf51f4c2fbaae66974a562147508b6288bc
Nick Conde <nick@budgiehouse.com>
2016-10-24 11:30:00 -0700

    Update to 16w42a

--- src/main/java/org/spongepowered/common/SpongeImpl.java
+++ src/main/java/org/spongepowered/common/SpongeImpl.java
@@ -77,7 +77,7 @@ public final class SpongeImpl {
     public static final Optional<String> IMPLEMENTATION_VERSION =  Optional.ofNullable(getPackage().getImplementationVersion());
 
     // TODO: Keep up to date
-    public static final SpongeMinecraftVersion MINECRAFT_VERSION = new SpongeMinecraftVersion("1.10.2", 210);
+    public static final SpongeMinecraftVersion MINECRAFT_VERSION = new SpongeMinecraftVersion("16w42a", 210);
 
     private static final Logger logger = LogManager.getLogger(ECOSYSTEM_NAME);
     private static final org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger(ECOSYSTEM_NAME);
