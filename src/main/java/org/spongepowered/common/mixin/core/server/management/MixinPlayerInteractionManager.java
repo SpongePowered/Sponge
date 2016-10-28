ef52c0d93821ba006327111bc47e1123cfb89fa0
bloodshot <jdroque@gmail.com>
2016-10-28 10:51:46 -0400

    Remove duplicate InteractItemEvent call.
    This event is handled in firePreEvents in PacketUtil.

--- src/main/java/org/spongepowered/common/mixin/core/server/management/MixinPlayerInteractionManager.java
+++ src/main/java/org/spongepowered/common/mixin/core/server/management/MixinPlayerInteractionManager.java
@@ -59,9 +59,6 @@ import org.spongepowered.api.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.common.event.SpongeCommonEventFactory;
 import org.spongepowered.common.interfaces.server.management.IMixinPlayerInteractionManager;
 import org.spongepowered.common.registry.provider.DirectionFacingProvider;
@@ -78,16 +75,6 @@ public abstract class MixinPlayerInteractionManager implements IMixinPlayerInter
     @Shadow private GameType gameType;
 
     @Shadow public abstract boolean isCreative();
-
-    @Inject(method = "processRightClick", at = @At("HEAD"), cancellable = true)
-    private void onProcessRightClick(EntityPlayer player, net.minecraft.world.World worldIn, ItemStack stack, EnumHand hand,
-            CallbackInfoReturnable<EnumActionResult> cir) {
-
-        // We fire an event here when a player right-click's the AIR with an item
-        if (SpongeCommonEventFactory.callInteractItemEventSecondary(player, stack, hand).isCancelled()) {
-            cir.setReturnValue(EnumActionResult.FAIL);
-        }
-    }
 
     /**
      * @author Aaron1011
