06d87286e23274571d5499e84f1bc362c853b476
Max Qian <public@maxqia.com>
2016-11-26 15:05:57 -0800

    Actually post the DropItemEvent.Pre on mob drops

--- src/main/java/org/spongepowered/common/entity/EntityUtil.java
+++ src/main/java/org/spongepowered/common/entity/EntityUtil.java
@@ -870,6 +870,7 @@ public final class EntityUtil {
         original.add(snapshot);
         final DropItemEvent.Pre dropEvent = SpongeEventFactory.createDropItemEventPre(Cause.of(NamedCause.source(entity)),
                 ImmutableList.of(snapshot), original);
+        SpongeImpl.postEvent(dropEvent);
         if (dropEvent.isCancelled()) {
             return null;
         }
@@ -944,6 +945,7 @@ public final class EntityUtil {
         original.add(snapshot);
         final DropItemEvent.Pre dropEvent = SpongeEventFactory.createDropItemEventPre(Cause.of(NamedCause.source(player)),
                 ImmutableList.of(snapshot), original);
+        SpongeImpl.postEvent(dropEvent);
         if (dropEvent.isCancelled()) {
             return null;
         }
