93e90326585ea74ef7e41a380cf315eab6046f4c
Aaron Hill <aa1ronham@gmail.com>
2017-01-02 16:54:25 -0500

    Make changes needed for allowing mod command duplicates

--- src/main/java/org/spongepowered/common/command/MinecraftCommandWrapper.java
+++ src/main/java/org/spongepowered/common/command/MinecraftCommandWrapper.java
@@ -240,4 +240,8 @@ public class MinecraftCommandWrapper implements CommandCallable {
             commandErrors.get().push(error);
         }
     }
+
+    public boolean suppressDuplicateAlias(String alias) {
+        return false;
+    }
 }
