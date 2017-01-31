7641b71329d3d067d218588b5f2df13efadf2eb5
Aaron Hill <aa1ronham@gmail.com>
2017-01-30 23:18:13 -0500

    Register banner patterns under short "hashname"

--- src/main/java/org/spongepowered/common/registry/type/BannerPatternShapeRegistryModule.java
+++ src/main/java/org/spongepowered/common/registry/type/BannerPatternShapeRegistryModule.java
@@ -60,6 +60,7 @@ public final class BannerPatternShapeRegistryModule implements CatalogRegistryMo
     public void registerDefaults() {
         for (BannerPattern pattern : BannerPattern.values()) {
             this.bannerPatternShapeMappings.put(pattern.name().toLowerCase(Locale.ENGLISH), (BannerPatternShape) (Object) pattern);
+            this.bannerPatternShapeMappings.put(pattern.getHashname().toLowerCase(Locale.ENGLISH), (BannerPatternShape) (Object) pattern);
         }
     }
 
@@ -68,6 +69,7 @@ public final class BannerPatternShapeRegistryModule implements CatalogRegistryMo
         for (BannerPattern pattern : BannerPattern.values()) {
             if (!this.bannerPatternShapeMappings.containsKey(pattern.name())) {
                 this.bannerPatternShapeMappings.put(pattern.name().toLowerCase(Locale.ENGLISH), (BannerPatternShape) (Object) pattern);
+                this.bannerPatternShapeMappings.put(pattern.getHashname().toLowerCase(Locale.ENGLISH), (BannerPatternShape) (Object) pattern);
             }
         }
     }
