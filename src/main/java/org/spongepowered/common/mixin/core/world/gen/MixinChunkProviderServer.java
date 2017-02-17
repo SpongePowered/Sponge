073d5bd112a73b5827203d174550fc7d3f543122
Aaron Hill <aa1ronham@gmail.com>
2017-02-17 16:26:29 -0500

    Update MixinChunkProviderServer for bleeding changes

--- src/main/java/org/spongepowered/common/mixin/core/world/gen/MixinChunkProviderServer.java
+++ src/main/java/org/spongepowered/common/mixin/core/world/gen/MixinChunkProviderServer.java
@@ -195,7 +195,7 @@ public abstract class MixinChunkProviderServer implements WorldStorage, IMixinCh
     @Inject(method = "provideChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/crash/CrashReport;makeCrashReport(Ljava/lang/Throwable;Ljava/lang/String;)Lnet/minecraft/crash/CrashReport;"))
     public void onError(CallbackInfoReturnable<Chunk> ci) {
         if (CauseTracker.ENABLED) {
-            ((IMixinWorldServer) this.worldObj).getCauseTracker().completePhase();
+            ((IMixinWorldServer) this.world).getCauseTracker().completePhase();
         }
     }
 
