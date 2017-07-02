ed204c955fb51e53ab67862d80ebb58851c70421
Gabriel Harris-Rouquette <gabizou@me.com>
2017-07-01 20:02:32 -0700

    Clean up the ScaledHealthTest plugin to use commands instead of a listener. This fully tests scaling nicely.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- testplugins/src/main/java/org/spongepowered/test/ScaledHealthTest.java
+++ testplugins/src/main/java/org/spongepowered/test/ScaledHealthTest.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of testplugins, licensed under the MIT License (MIT).
+ * This file is part of Sponge, licensed under the MIT License (MIT).
  *
  * Copyright (c) SpongePowered <https://www.spongepowered.org>
  * Copyright (c) contributors
@@ -24,21 +24,110 @@
  */
 package org.spongepowered.test;
 
+import static org.spongepowered.api.command.args.GenericArguments.doubleNum;
+import static org.spongepowered.api.command.args.GenericArguments.firstParsing;
+import static org.spongepowered.api.command.args.GenericArguments.onlyOne;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandCallable;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.args.ChildCommandElementExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.filter.Getter;
-import org.spongepowered.api.event.network.ClientConnectionEvent;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.LiteralText;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.format.TextStyles;
 
 @Plugin(id = "scaledhealthtest", name = "ScaledHealthTest", description = "A plugin to test scaled health")
 public class ScaledHealthTest {
 
     @Listener
-    public void onPlayerJoin(ClientConnectionEvent.Join event, @Getter("getTargetEntity") Player player) {
-        player.offer(Keys.HEALTH_SCALE, 10D);
-        player.offer(Keys.MAX_HEALTH, 1000D);
-        player.offer(Keys.HEALTH, 500D);
+    public void onInit(GameInitializationEvent event) {
+        Sponge.getCommandManager().register(this, getHealthCommand(), "health");
     }
+
+    private static CommandCallable getHealthCommand() {
+        final ChildCommandElementExecutor flagChildren = new ChildCommandElementExecutor(null);
+        final ChildCommandElementExecutor nonFlagChildren = new ChildCommandElementExecutor(flagChildren);
+        nonFlagChildren.register(getShowHealth(), "show");
+        nonFlagChildren.register(getSetHealthScale(), "setScale");
+        nonFlagChildren.register(getSetHealth(), "setHealth");
+        nonFlagChildren.register(getSetMaxHealth(), "setMax");
+        return CommandSpec.builder()
+            .description(Text.of("ScaledHealth command"))
+            .extendedDescription(Text.of("commands:\n", "set a trail to you as a player"))
+            .arguments(firstParsing(nonFlagChildren))
+            .executor(nonFlagChildren)
+            .build();
+    }
+
+    private static CommandCallable getSetMaxHealth() {
+        final LiteralText health = Text.of("health");
+        return CommandSpec.builder()
+            .arguments(onlyOne(doubleNum(health)))
+            .description(Text.of(TextColors.AQUA, "Sets your maximum health"))
+            .executor((src, args) -> {
+                if (!(src instanceof Player)) {
+                    return CommandResult.empty();
+                }
+                final double newHealth = args.<Double>getOne(health).get();
+                ((Player) src).offer(Keys.MAX_HEALTH, newHealth);
+                return CommandResult.success();
+            })
+            .build();
+    }
+
+    private static CommandCallable getSetHealth() {
+        final LiteralText health = Text.of("health");
+        return CommandSpec.builder()
+            .arguments(onlyOne(doubleNum(health)))
+            .description(Text.of(TextColors.AQUA, "Sets your health"))
+            .executor((src, args) -> {
+                if (!(src instanceof Player)) {
+                    return CommandResult.empty();
+                }
+                final double newHealth = args.<Double>getOne(health).get();
+                ((Player) src).offer(Keys.HEALTH, newHealth);
+                return CommandResult.success();
+            })
+            .build();
+    }
+
+    private static CommandCallable getSetHealthScale() {
+        final LiteralText healthText = Text.of("health");
+        return CommandSpec.builder()
+            .arguments(onlyOne(doubleNum(healthText)))
+            .description(Text.of(TextColors.AQUA, "Sets your health scale"))
+            .executor(((src, args) -> {
+                if (!(src instanceof Player)) {
+                    return CommandResult.empty();
+                }
+                final double health = args.<Double>getOne(Text.of(healthText)).get();
+                ((Player) src).offer(Keys.HEALTH_SCALE, health);
+
+                return CommandResult.success();
+            }))
+            .build();
+    }
+
+    private static CommandCallable getShowHealth() {
+        return CommandSpec.builder()
+            .description(Text.of(TextColors.AQUA, "Shows your health"))
+            .executor(((src, args) -> {
+                if (!(src instanceof Player)) {
+                    return CommandResult.empty();
+                }
+                src.sendMessage(Text.of(TextColors.DARK_GREEN, TextStyles.BOLD, "Health: ", TextColors.RED, TextStyles.NONE, ((Player) src).get(
+                    Keys.HEALTH).orElse(0D)));
+                return CommandResult.success();
+            }))
+            .build();
+    }
+
 
 }
