67503ef2cf0ba570192284e352b9c28e04f362de
bloodshot <jdroque@gmail.com>
2017-08-08 20:57:16 -0400

    Re-add container overwrite to handle listeners that already exist.
    Fixes SpongePowered/SpongeForge#1703

--- src/main/java/org/spongepowered/common/mixin/core/item/inventory/MixinContainer.java
+++ src/main/java/org/spongepowered/common/mixin/core/item/inventory/MixinContainer.java
@@ -118,6 +118,28 @@ public abstract class MixinContainer implements org.spongepowered.api.item.inven
 
     /**
      * @author bloodmc
+     * @reason If listener already exists, avoid firing an exception
+     * and simply send the inventory changes to client.
+     */
+    @SuppressWarnings("unchecked")
+    @Overwrite
+    public void addListener(IContainerListener listener) {
+        Container container = (Container) (Object) this;
+        if (this.listeners.contains(listener)) {
+            // Sponge start
+            // throw new IllegalArgumentException("Listener already listening");
+            listener.updateCraftingInventory(container, this.getInventory());
+            container.detectAndSendChanges();
+            // Sponge end
+        } else {
+            this.listeners.add(listener);
+            listener.updateCraftingInventory(container, this.getInventory());
+            container.detectAndSendChanges();
+        }
+    }
+
+    /**
+     * @author bloodmc
      * @reason All player fabric changes that need to be synced to
      * client flow through this method. Overwrite is used as no mod
      * should be touching this method.
