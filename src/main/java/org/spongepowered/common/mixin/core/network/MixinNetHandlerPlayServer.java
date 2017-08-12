ccbdd30d4c8ac4465315dee6a1a6bf6b28b12f07
Gabriel Harris-Rouquette <gabizou@me.com>
2017-08-12 15:59:49 -0700

    Have PlayerTickPhase surrounded with a try catch to avoid exceptions causing additional stacks. Partially fixes SpongePowered/SpongeForge#1713 and SpongePowered/SpongeForge#1477.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/main/java/org/spongepowered/common/mixin/core/network/MixinNetHandlerPlayServer.java
+++ src/main/java/org/spongepowered/common/mixin/core/network/MixinNetHandlerPlayServer.java
@@ -198,13 +198,15 @@ public abstract class MixinNetHandlerPlayServer implements PlayerConnection, IMi
             return;
         }
         final CauseTracker causeTracker = CauseTracker.getInstance();
-        causeTracker.switchToPhase(TickPhase.Tick.PLAYER, PhaseContext.start()
-                .add(NamedCause.source(player))
-                .addCaptures()
-                .addEntityDropCaptures()
-                .complete());
-        player.onUpdateEntity();
-        causeTracker.completePhase(TickPhase.Tick.PLAYER);
+        final PhaseContext complete = PhaseContext.start()
+            .add(NamedCause.source(player))
+            .addCaptures()
+            .addEntityDropCaptures()
+            .complete();
+        causeTracker.switchToPhase(TickPhase.Tick.PLAYER, complete, () -> {
+            player.onUpdateEntity();
+            return null;
+        });
     }
 
     @Override
