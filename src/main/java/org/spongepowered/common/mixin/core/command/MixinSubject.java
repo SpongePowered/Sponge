radfast/SpongeCommon

Finish implementing cause refactor

src/main/java/org/spongepowered/common/mixin/core/command/MixinSubject.java
@@ -29,7 +29,7 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.rcon.RConConsoleSource;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntityCommandBlock;
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.PermissionService;
 import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.service.permission.SubjectCollection;
@@ -122,7 +122,7 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public boolean hasPermission(Set<ServiceContext> contexts, String permission) {
+    public boolean hasPermission(Set<Context> contexts, String permission) {
         Subject subj = internalSubject();
         if (subj == null) {
             return this.permDefault(permission).asBoolean();
@@ -143,7 +143,7 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public Tristate getPermissionValue(Set<ServiceContext> contexts, String permission) {
+    public Tristate getPermissionValue(Set<Context> contexts, String permission) {
         Subject subj = internalSubject();
         return subj == null ? this.permDefault(permission) : subj.getPermissionValue(contexts, permission);
     }
@@ -155,7 +155,7 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public boolean isChildOf(Set<ServiceContext> contexts, Subject parent) {
+    public boolean isChildOf(Set<Context> contexts, Subject parent) {
         Subject subj = internalSubject();
         return subj != null && subj.isChildOf(contexts, parent);
     }
@@ -167,19 +167,19 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public List<Subject> getParents(Set<ServiceContext> contexts) {
+    public List<Subject> getParents(Set<Context> contexts) {
         Subject subj = internalSubject();
         return subj == null ? Collections.<Subject>emptyList() : subj.getParents(contexts);
     }
 
     @Override
-    public Set<ServiceContext> getActiveContexts() {
+    public Set<Context> getActiveContexts() {
         Subject subj = internalSubject();
-        return subj == null ? Collections.<ServiceContext>emptySet() : subj.getActiveContexts();
+        return subj == null ? Collections.<Context>emptySet() : subj.getActiveContexts();
     }
 
     @Override
-    public Optional<String> getOption(Set<ServiceContext> contexts, String key) {
+    public Optional<String> getOption(Set<Context> contexts, String key) {
         Subject subj = internalSubject();
         return subj == null ? Optional.empty() : subj.getOption(contexts, key);
     }

src/main/java/org/spongepowered/common/service/permission/base/FixedParentMemorySubjectData.java
@@ -26,7 +26,7 @@ package org.spongepowered.common.service.permission.base;
 
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.PermissionService;
 import org.spongepowered.api.service.permission.Subject;
 
@@ -50,12 +50,12 @@ public class FixedParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public List<Subject> getParents(Set<ServiceContext> contexts) {
+    public List<Subject> getParents(Set<Context> contexts) {
         return ImmutableList.<Subject>builder().add(this.forcedParent).addAll(super.getParents(contexts)).build();
     }
 
     @Override
-    public boolean addParent(Set<ServiceContext> contexts, Subject parent) {
+    public boolean addParent(Set<Context> contexts, Subject parent) {
         if (Objects.equal(this.forcedParent, parent) && GLOBAL_CONTEXT.equals(contexts)) {
             return true;
         }
@@ -63,7 +63,7 @@ public class FixedParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public boolean removeParent(Set<ServiceContext> contexts, Subject parent) {
+    public boolean removeParent(Set<Context> contexts, Subject parent) {
         if (Objects.equal(this.forcedParent, parent)) {
             return false;
         }

src/main/java/org/spongepowered/common/service/permission/base/SingleParentMemorySubjectData.java
@@ -24,7 +24,7 @@
  */
 package org.spongepowered.common.service.permission.base;
 
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.PermissionService;
 import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.common.service.permission.OpLevelCollection;
@@ -47,13 +47,13 @@ public class SingleParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public List<Subject> getParents(Set<ServiceContext> contexts) {
+    public List<Subject> getParents(Set<Context> contexts) {
         final Subject parent = getParent();
         return GLOBAL_CONTEXT.equals(contexts) && parent != null ? Collections.singletonList(parent) : Collections.<Subject>emptyList();
     }
 
     @Override
-    public boolean addParent(Set<ServiceContext> contexts, Subject parent) {
+    public boolean addParent(Set<Context> contexts, Subject parent) {
         if (!(parent instanceof OpLevelCollection.OpLevelSubject)) {
             return false;
         }
@@ -65,7 +65,7 @@ public class SingleParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public boolean removeParent(Set<ServiceContext> contexts, Subject parent) {
+    public boolean removeParent(Set<Context> contexts, Subject parent) {
         if (parent == this.parent) {
             return setParent(null);
         }
@@ -78,7 +78,7 @@ public class SingleParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public boolean clearParents(Set<ServiceContext> contexts) {
+    public boolean clearParents(Set<Context> contexts) {
         return GLOBAL_CONTEXT.equals(contexts) && clearParents();
     }
 

src/main/java/org/spongepowered/common/service/permission/base/SpongeSubjectCollection.java
@@ -24,7 +24,7 @@
  */
 package org.spongepowered.common.service.permission.base;
 
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.service.permission.SubjectCollection;
 import org.spongepowered.api.util.Tristate;
@@ -67,7 +67,7 @@ public abstract class SpongeSubjectCollection implements SubjectCollection {
     }
 
     @Override
-    public Map<Subject, Boolean> getAllWithPermission(Set<ServiceContext> contexts, String permission) {
+    public Map<Subject, Boolean> getAllWithPermission(Set<Context> contexts, String permission) {
         final Map<Subject, Boolean> ret = new HashMap<>();
         for (Subject subj : getAllSubjects()) {
             Tristate state = subj.getPermissionValue(contexts, permission);

SpongePowered/Sponge

src/main/java/org/spongepowered/common/mixin/core/command/MixinSubject.java
@@ -29,7 +29,7 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.rcon.RConConsoleSource;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntityCommandBlock;
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.PermissionService;
 import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.service.permission.SubjectCollection;
@@ -166,7 +166,7 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public boolean hasPermission(Set<ServiceContext> contexts, String permission) {
+    public boolean hasPermission(Set<Context> contexts, String permission) {
         Subject subj = resolveNullable();
         if (subj == null) {
             return permDefault(permission).asBoolean();
@@ -191,7 +191,7 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public boolean isChildOf(Set<ServiceContext> contexts, SubjectReference parent) {
+    public boolean isChildOf(Set<Context> contexts, SubjectReference parent) {
         return resolve().isChildOf(contexts, parent);
     }
 
@@ -201,7 +201,7 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public List<SubjectReference> getParents(Set<ServiceContext> contexts) {
+    public List<SubjectReference> getParents(Set<Context> contexts) {
         return resolve().getParents(contexts);
     }
 
@@ -211,7 +211,7 @@ public abstract class MixinSubject implements Subject, IMixinCommandSource, IMix
     }
 
     @Override
-    public Optional<String> getOption(Set<ServiceContext> contexts, String key) {
+    public Optional<String> getOption(Set<Context> contexts, String key) {
         return resolve().getOption(contexts, key);
     }
 

src/main/java/org/spongepowered/common/service/permission/base/FixedParentMemorySubjectData.java
@@ -26,7 +26,7 @@ package org.spongepowered.common.service.permission.base;
 
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.PermissionService;
 import org.spongepowered.api.service.permission.SubjectReference;
 
@@ -51,12 +51,12 @@ public class FixedParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public List<SubjectReference> getParents(Set<ServiceContext> contexts) {
+    public List<SubjectReference> getParents(Set<Context> contexts) {
         return ImmutableList.<SubjectReference>builder().add(this.forcedParent).addAll(super.getParents(contexts)).build();
     }
 
     @Override
-    public CompletableFuture<Boolean> addParent(Set<ServiceContext> contexts, SubjectReference parent) {
+    public CompletableFuture<Boolean> addParent(Set<Context> contexts, SubjectReference parent) {
         if (Objects.equal(this.forcedParent, parent) && contexts.isEmpty()) {
             return CompletableFuture.completedFuture(true);
         }
@@ -64,7 +64,7 @@ public class FixedParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public CompletableFuture<Boolean> removeParent(Set<ServiceContext> contexts, SubjectReference parent) {
+    public CompletableFuture<Boolean> removeParent(Set<Context> contexts, SubjectReference parent) {
         if (Objects.equal(this.forcedParent, parent)) {
             return CompletableFuture.completedFuture(false);
         }

src/main/java/org/spongepowered/common/service/permission/base/SingleParentMemorySubjectData.java
@@ -24,7 +24,7 @@
  */
 package org.spongepowered.common.service.permission.base;
 
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.PermissionService;
 import org.spongepowered.api.service.permission.SubjectReference;
 import org.spongepowered.common.service.permission.OpLevelCollection;
@@ -50,13 +50,13 @@ public class SingleParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public List<SubjectReference> getParents(Set<ServiceContext> contexts) {
+    public List<SubjectReference> getParents(Set<Context> contexts) {
         final SubjectReference parent = getParent();
         return contexts.isEmpty() && parent != null ? Collections.singletonList(parent) : Collections.emptyList();
     }
 
     @Override
-    public CompletableFuture<Boolean> addParent(Set<ServiceContext> contexts, SubjectReference parent) {
+    public CompletableFuture<Boolean> addParent(Set<Context> contexts, SubjectReference parent) {
         if (!(parent instanceof OpLevelCollection.OpLevelSubject)) {
             return CompletableFuture.completedFuture(false);
         }
@@ -69,7 +69,7 @@ public class SingleParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public CompletableFuture<Boolean> removeParent(Set<ServiceContext> contexts, SubjectReference parent) {
+    public CompletableFuture<Boolean> removeParent(Set<Context> contexts, SubjectReference parent) {
         if (parent == this.parent) {
             setParent(null);
             return CompletableFuture.completedFuture(true);
@@ -84,7 +84,7 @@ public class SingleParentMemorySubjectData extends GlobalMemorySubjectData {
     }
 
     @Override
-    public CompletableFuture<Boolean> clearParents(Set<ServiceContext> contexts) {
+    public CompletableFuture<Boolean> clearParents(Set<Context> contexts) {
         if (!contexts.isEmpty()) {
             return CompletableFuture.completedFuture(false);
         }

src/main/java/org/spongepowered/common/service/permission/base/SpongeSubjectCollection.java
@@ -26,7 +26,7 @@ package org.spongepowered.common.service.permission.base;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import org.spongepowered.api.service.context.ServiceContext;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.service.permission.SubjectCollection;
 import org.spongepowered.api.service.permission.SubjectReference;
@@ -107,7 +107,7 @@ public abstract class SpongeSubjectCollection implements SubjectCollection {
     }
 
     @Override
-    public Map<Subject, Boolean> getLoadedWithPermission(Set<ServiceContext> contexts, String permission) {
+    public Map<Subject, Boolean> getLoadedWithPermission(Set<Context> contexts, String permission) {
         final Map<Subject, Boolean> ret = new HashMap<>();
         for (Subject subj : getLoadedSubjects()) {
             Tristate state = subj.getPermissionValue(contexts, permission);