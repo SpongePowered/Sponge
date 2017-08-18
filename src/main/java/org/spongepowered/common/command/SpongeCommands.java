dc1b068e13c1b1f53e8608d3a0b6f57be87dba8a
Ryan Leach <rleach@rleach.id.au>
2017-08-19 00:24:54 +0930

    Merge SpongeHelpCommand back into SpongeCommands, add JavaDocs.

--- src/main/java/org/spongepowered/common/command/SpongeCommands.java
+++ src/main/java/org/spongepowered/common/command/SpongeCommands.java
@@ -39,6 +39,8 @@ import static org.spongepowered.api.command.args.GenericArguments.world;
 
 import co.aikar.timings.SpongeTimingsFactory;
 import co.aikar.timings.Timings;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
@@ -54,8 +56,12 @@ import org.spongepowered.api.command.CommandManager;
 import org.spongepowered.api.command.CommandMapping;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.ArgumentParseException;
 import org.spongepowered.api.command.args.ChildCommandElementExecutor;
+import org.spongepowered.api.command.args.CommandArgs;
 import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.CommandElement;
+import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.entity.living.player.Player;
@@ -63,11 +69,16 @@ import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.event.SpongeEventFactory;
 import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.service.pagination.PaginationList;
+import org.spongepowered.api.service.pagination.PaginationService;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.action.ClickAction;
 import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.format.TextStyles;
+import org.spongepowered.api.text.translation.Translation;
+import org.spongepowered.api.util.GuavaCollectors;
+import org.spongepowered.api.util.SpongeApiTranslationHelper;
+import org.spongepowered.api.util.StartsWithPredicate;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.api.world.DimensionType;
 import org.spongepowered.api.world.World;
@@ -102,7 +113,11 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.TreeSet;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
 
 @NonnullByDefault
 public class SpongeCommands {
@@ -117,6 +132,31 @@ public class SpongeCommands {
     static final Text UNKNOWN = Text.of("UNKNOWN");
 
     private static final DecimalFormat THREE_DECIMAL_DIGITS_FORMATTER = new DecimalFormat("########0.000");
+    private static final Comparator<CommandMapping> COMMAND_COMPARATOR = Comparator.comparing(CommandMapping::getPrimaryAlias);
+    private static final Text PAGE_KEY = Text.of("page");
+    private static final Text COMMAND_KEY = Text.of("command");
+    private static final CommandElement COMMAND_ARGUMENT = new CommandElement(COMMAND_KEY) {
+
+        @Nullable
+        @Override protected Object parseValue(CommandSource source, CommandArgs args) throws ArgumentParseException {
+            String input = args.next();
+            Optional<? extends CommandMapping> cmd = SpongeImpl.getGame().getCommandManager().get(input, source);
+
+            if (!cmd.isPresent()) {
+                throw args.createError(SpongeApiTranslationHelper.t("No such command: ", input));
+            }
+            return cmd.orElse(null);
+        }
+
+        @Override public List<String> complete(CommandSource src, CommandArgs args, CommandContext context) {
+            final String prefix = args.nextIfPresent().orElse("");
+            return commandsStr(src).stream()
+                .filter(new StartsWithPredicate(prefix))
+                .collect(GuavaCollectors.toImmutableList());
+        }
+    };
+    private static final Text NOT_FOUND = Text.of("notFound");
+
     /**
      * Create a new instance of the Sponge command structure.
      *
@@ -162,6 +202,101 @@ public class SpongeCommands {
                         .buildWith(flagChildren)))
                 .executor(nonFlagChildren)
                 .build();
+    }
+
+    /**
+     * Creates a new instance of the Sponge help command.
+     * @return The newly created command
+     */
+    public static CommandSpec createHelpCommand() {
+        return CommandSpec
+            .builder()
+            .permission("sponge.command.help")
+            .arguments(
+                optional(
+                    firstParsing(
+                        GenericArguments.integer(PAGE_KEY),
+                        COMMAND_ARGUMENT,
+                        string(NOT_FOUND)
+                    )
+                )
+            )
+            .description(Text.of("View a list of all commands."))
+            .extendedDescription(
+                Text.of("View a list of all commands. Hover over\n" + " a command to view its description. Click\n"
+                         + " a command to insert it into your chat bar."))
+            .executor((src, args) -> {
+
+                if(args.getOne(NOT_FOUND).isPresent()){
+                    throw new CommandException(Text.of("No such command: ", args.getOne(NOT_FOUND).get()));
+                }
+
+                Optional<CommandMapping> command = args.getOne(COMMAND_KEY);
+                Optional<Integer> page = args.getOne(PAGE_KEY);
+
+                if (command.isPresent()) {
+                    CommandCallable callable = command.get().getCallable();
+                    Optional<? extends Text> desc = callable.getHelp(src);
+                    if (desc.isPresent()) {
+                        src.sendMessage(desc.get());
+                    } else {
+                        src.sendMessage(Text.of("Usage: /", command.get(), callable.getUsage(src)));
+                    }
+                    return CommandResult.success();
+                }
+
+                Translation helpTip = Sponge.getRegistry().getTranslationById("commands.help.footer").get();
+                PaginationList.Builder builder = SpongeImpl.getGame().getServiceManager().provide(PaginationService.class).get().builder();
+                builder.title(Text.builder("Showing Help (/page <page>):").color(TextColors.DARK_GREEN).build());
+
+                ImmutableList<Text> contents = ImmutableList.<Text>builder()
+                        .add(Text.of(helpTip))
+                        .addAll(Collections2.transform(commands(src), input -> createDescription(src, input))).build();
+                builder.contents(contents);
+                builder.build().sendTo(src, page.orElse(1));
+                return CommandResult.success();
+            }).build();
+    }
+
+    /**
+     * get a collection of primary aliases of commands that the source has access to.
+     * @param src the command source to permission check.
+     * @return a collection of primary aliases.
+     */
+    private static Collection<String> commandsStr(CommandSource src) {
+        return commands(src).stream().map(CommandMapping::getPrimaryAlias).collect(Collectors.toList());
+    }
+
+    /**
+     *
+     * @param src the CommandSource to test permissions against.
+     * @return a set of commandMapping, sorted by primary alias
+     */
+    private static TreeSet<CommandMapping> commands(CommandSource src) {
+        TreeSet<CommandMapping> commands = new TreeSet<>(COMMAND_COMPARATOR);
+        commands.addAll(Collections2.filter(SpongeImpl.getGame().getCommandManager().getAll().values(), input -> input.getCallable()
+                .testPermission(src)));
+        return commands;
+    }
+
+    /**
+     * Creates a short description for display on the help index.
+     * @param source the source the description will be shown to for translation purposes.
+     * @param mapping the command mapping to generate a description from
+     * @return a Text representing the command mapping, formatted for display on the help index.
+     */
+    private static Text createDescription(CommandSource source, CommandMapping mapping) {
+        @SuppressWarnings("unchecked")
+        final Optional<Text> description = mapping.getCallable().getShortDescription(source);
+        Text.Builder text = Text.builder("/" + mapping.getPrimaryAlias());
+        text.color(TextColors.GREEN);
+        //End with a space, so tab completion works immediately.
+        text.onClick(TextActions.suggestCommand("/" + mapping.getPrimaryAlias() + " "));
+        Optional<? extends Text> longDescription = mapping.getCallable().getHelp(source);
+        if (longDescription.isPresent()) {
+            text.onHover(TextActions.showText(longDescription.get()));
+        }
+        return Text.of(text, " ", description.orElse(mapping.getCallable().getUsage(source)));
     }
 
 
@@ -577,7 +712,6 @@ public class SpongeCommands {
             src.sendMessage(builder.build());
         }
     }
-
 
     private static CommandCallable createSpongeTimingsCommand() {
         return CommandSpec.builder()
