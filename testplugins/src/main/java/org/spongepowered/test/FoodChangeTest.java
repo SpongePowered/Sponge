d89c01fe56d52323fa81617380f400f4e9a0f157
Aaron Hill <aa1ronham@gmail.com>
2017-11-18 22:40:09 -0500

    Clean up FoodChangeTest

--- testplugins/src/main/java/org/spongepowered/test/FoodChangeTest.java
+++ testplugins/src/main/java/org/spongepowered/test/FoodChangeTest.java
@@ -9,6 +9,7 @@ import org.spongepowered.api.data.value.immutable.ImmutableValue;
 import org.spongepowered.api.data.value.mutable.MutableBoundedValue;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.data.ChangeDataHolderEvent;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.filter.data.GetKey;
@@ -16,30 +17,25 @@ import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.channel.MessageChannel;
 
+import java.util.Optional;
+
 @Plugin(id = "food-change-test", authors = "Aaron1011")
 public class FoodChangeTest {
 
-    @Listener
-    public void onChange(ChangeDataHolderEvent.ValueChange event) {
-        int oldFood = 0;
-        int newFood = 0;
-        for (ImmutableValue<?> val: event.getChanges().getReplacedData()) {
-            if (val.getKey().equals(Keys.FOOD_LEVEL)) {
-                oldFood = (Integer) val.get();
-            }
+    @Listener(order = Order.EARLY)
+    public void simpleListener(ChangeDataHolderEvent.ValueChange event) {
+        Optional<ImmutableValue<?>> oldFood = event.getChanges().get(DataTransactionResult.DataCategory.REPLACED, Keys.FOOD_LEVEL);
+        Optional<ImmutableValue<?>> newFood = event.getChanges().get(DataTransactionResult.DataCategory.SUCCESSFUL, Keys.FOOD_LEVEL);
+
+        if (!oldFood.isPresent() || !newFood.isPresent()) {
+            return;
         }
 
-        for (ImmutableValue<?> val: event.getChanges().getSuccessfulData()) {
-            if (val.getKey().equals(Keys.FOOD_LEVEL)) {
-                newFood = (Integer) val.get();
-            }
-        }
-
-        MessageChannel.TO_ALL.send(Text.of(String.format("Simple listener: %s %s", oldFood, newFood)));
+        MessageChannel.TO_ALL.send(Text.of(String.format("Simple listener: %s %s", oldFood.get().get(), newFood.get().get())));
     }
 
     @Listener
-    public void onChange(ChangeDataHolderEvent.ValueChange event,
+    public void getKeyListener(ChangeDataHolderEvent.ValueChange event,
             @GetKey(value = "FOOD_LEVEL", from = REPLACED) int oldFood,
             @GetKey(value = "FOOD_LEVEL", from = SUCCESSFUL) MutableBoundedValue<Integer> newFood,
             @First(tag = "a") Player player,
