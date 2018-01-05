8bd04d41f27a9b615e4c41844db9ce9362424cb2
Matthew Miller <mnmiller1@me.com>
2018-01-05 11:15:33 +1000

    Added a command to enable/disable dispenser fire test.

--- testplugins/src/main/java/org/spongepowered/test/DispenserFireTest.java
+++ testplugins/src/main/java/org/spongepowered/test/DispenserFireTest.java
@@ -24,10 +24,14 @@
  */
 package org.spongepowered.test;
 
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.effect.sound.SoundTypes;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.item.inventory.DropItemEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.world.LocatableBlock;
@@ -35,12 +39,32 @@ import org.spongepowered.api.world.LocatableBlock;
 @Plugin(id = "dispenserfiretest", name = "Dispenser Fire Test", description = "A plugin to test the item dispense event fires for dispensers.")
 public class DispenserFireTest {
 
-    @Listener
-    public void onDispense(DropItemEvent.Dispense event, @First LocatableBlock dispenser) {
-        if (dispenser.getBlockState().getType() != BlockTypes.DISPENSER) {
-            return;
-        }
+    private final DispenserFireListener listener = new DispenserFireListener();
+    private boolean registered = false;
 
-        dispenser.getWorld().playSound(SoundTypes.ENTITY_GENERIC_EXPLODE, dispenser.getPosition().toDouble(), 2.0);
+    @Listener
+    public void onInit(GameInitializationEvent event) {
+        Sponge.getCommandManager().register(this,
+                CommandSpec.builder().executor((source, context) -> {
+                    if (this.registered) {
+                        this.registered = false;
+                        Sponge.getEventManager().unregisterListeners(this.listener);
+                    } else {
+                        this.registered = true;
+                        Sponge.getEventManager().registerListeners(this, this.listener);
+                    }
+                    return CommandResult.success();
+                }).build(), "dispensersound");
+    }
+
+    public static class DispenserFireListener {
+        @Listener
+        public void onDispense(DropItemEvent.Dispense event, @First LocatableBlock dispenser) {
+            if (dispenser.getBlockState().getType() != BlockTypes.DISPENSER) {
+                return;
+            }
+
+            dispenser.getWorld().playSound(SoundTypes.ENTITY_GENERIC_EXPLODE, dispenser.getPosition().toDouble(), 2.0);
+        }
     }
 }
