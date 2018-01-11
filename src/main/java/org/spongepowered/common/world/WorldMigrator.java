c12727dbe6f0abf5e5ec920ded92593057dc2a38
Chris Sanders <zidane@spongepowered.org>
2018-01-10 18:42:05 -0600

    Have migrator "fix" save data existing in wrong folder on client.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/spongepowered/common/world/WorldMigrator.java
+++ src/main/java/org/spongepowered/common/world/WorldMigrator.java
@@ -26,20 +26,27 @@ package org.spongepowered.common.world;
 
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.yaml.YAMLConfigurationLoader;
+import org.apache.commons.io.FileUtils;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.util.file.CopyFileVisitor;
 import org.spongepowered.common.SpongeImpl;
 
 import java.io.IOException;
+import java.nio.file.CopyOption;
 import java.nio.file.DirectoryStream;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Used to migrate Worlds from Bukkit -> Sponge
+ * Used to migrate Worlds from Bukkit -> Sponge and fix core world errors
  */
 public class WorldMigrator {
 
@@ -83,39 +90,108 @@ public class WorldMigrator {
         final Path oldWorldContainer = getOldWorldContainer();
         final List<Path> migrated = new ArrayList<>();
 
-        if (Files.notExists(oldWorldContainer)) {
-            return;
+        if (Files.exists(oldWorldContainer)) {
+
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(oldWorldContainer,
+                    entry -> !entry.getFileName().equals(worldContainer.getFileName()) && Files.exists(entry.resolve("level.dat")) && !Files
+                            .exists(entry.resolve("level_sponge.dat")))) {
+                for (Path oldWorldPath : stream) {
+                    Path worldPath = worldContainer.resolve(getVanillaNetherOrEndName(oldWorldPath));
+
+                    // Only copy over the old world files if we don't have it already.
+                    if (Files.notExists(worldPath)) {
+                        SpongeImpl.getLogger().info("Migrating [{}] from [{}].", oldWorldPath.getFileName(), oldWorldContainer);
+                        try {
+                            worldPath = renameToVanillaNetherOrEnd(worldContainer, oldWorldPath, worldPath);
+                            Files.walkFileTree(oldWorldPath, new CopyFileVisitor(worldPath));
+                            fixInnerNetherOrEndRegionData(worldPath);
+                            removeInnerNameFolder(worldPath);
+                            migrated.add(worldPath);
+                        } catch (IOException ioe) {
+                            SpongeImpl.getLogger().warn("Failed to migrate [{}] from [{}] to [{}]", oldWorldPath.getFileName(), oldWorldContainer,
+                                    worldPath, ioe);
+                        }
+
+                        SpongeImpl.getLogger()
+                                .info("Migrated world [{}] from [{}] to [{}]", oldWorldPath.getFileName(), oldWorldContainer, worldPath);
+                    }
+                }
+            } catch (Exception ignore) {
+            }
+
+            if (!migrated.isEmpty()) {
+                SpongeImpl.getLogger().info("[{}] worlds have been migrated back to Vanilla's format.", migrated.size());
+            } else {
+                SpongeImpl.getLogger().info("No worlds were found in need of migration.");
+            }
         }
 
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(oldWorldContainer,
-                entry -> !entry.getFileName().equals(worldContainer.getFileName()) && Files.exists(entry.resolve("level.dat")) && !Files.exists(entry
-                        .resolve("level_sponge.dat")))) {
-            for (Path oldWorldPath : stream) {
-                Path worldPath = worldContainer.resolve(getVanillaNetherOrEndName(oldWorldPath));
+        // Previous to 1/10/2018, Sponge accidentally used the wrong save folder for client saves (put them on the root instead of saves/my_save).
+        // This fixes this issue by moving over the folders and deleting them on the root
+        if (Sponge.getPlatform().getType().isClient()) {
+            try {
+                Files.newDirectoryStream(worldContainer.getParent()).forEach((saveFolder) -> {
+                    final boolean isWorld = Files.exists(saveFolder.resolve("level.dat"));
+                    if (isWorld) {
+                        final Path invalidSaveDataFolder = saveFolder.getParent().getParent().resolve(saveFolder.getFileName().toString());
+                        try {
+                            if (Files.isDirectory(invalidSaveDataFolder)) {
+                                Files.walkFileTree(invalidSaveDataFolder, new SimpleFileVisitor<Path>() {
 
-                // Only copy over the old world files if we don't have it already.
-                if (Files.notExists(worldPath)) {
-                    SpongeImpl.getLogger().info("Migrating [{}] from [{}].", oldWorldPath.getFileName(), oldWorldContainer);
-                    try {
-                        worldPath = renameToVanillaNetherOrEnd(worldContainer, oldWorldPath, worldPath);
-                        Files.walkFileTree(oldWorldPath, new CopyFileVisitor(worldPath));
-                        fixInnerNetherOrEndRegionData(worldPath);
-                        removeInnerNameFolder(worldPath);
-                        migrated.add(worldPath);
-                    } catch (IOException ioe) {
-                        SpongeImpl.getLogger().warn("Failed to migrate [{}] from [{}] to [{}]", oldWorldPath.getFileName(), oldWorldContainer,
-                                worldPath, ioe);
+                                    @Override
+                                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                                        if (invalidSaveDataFolder != dir) {
+                                            Path validFile = null;
+
+                                            for (int i = 2; i < dir.getNameCount(); i++) {
+                                                if (validFile == null) {
+                                                    validFile = saveFolder.resolve(dir.getName(i));
+                                                } else {
+                                                    validFile = validFile.resolve(dir.getName(i));
+                                                }
+                                            }
+
+                                            if (validFile != null && Files.notExists(validFile)) {
+                                                Files.createDirectories(validFile);
+                                            }
+                                        }
+
+                                        return super.preVisitDirectory(dir, attrs);
+                                    }
+
+                                    @Override
+                                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                                        // Directories are made above
+                                        if (!Files.isDirectory(file)) {
+                                            Path validFile = null;
+
+                                            for (int i = 2; i < file.getNameCount(); i++) {
+                                                if (validFile == null) {
+                                                    validFile = saveFolder.resolve(file.getName(i));
+                                                } else {
+                                                    validFile = validFile.resolve(file.getName(i));
+                                                }
+                                            }
+
+                                            // Only move non-existing files in the proper directory
+                                            if (validFile != null && Files.notExists(validFile)) {
+                                                com.google.common.io.Files.move(file.toFile(), validFile.toFile());
+                                            }
+                                        }
+                                        return super.visitFile(file, attrs);
+                                    }
+                                });
+
+                                FileUtils.deleteDirectory(invalidSaveDataFolder.toFile());
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-
-                    SpongeImpl.getLogger().info("Migrated world [{}] from [{}] to [{}]", oldWorldPath.getFileName(), oldWorldContainer, worldPath);
-                }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (Exception ignore) {}
-
-        if (migrated.size() > 0) {
-            SpongeImpl.getLogger().info("[{}] worlds have been migrated back to Vanilla's format.", migrated.size());
-        } else {
-            SpongeImpl.getLogger().info("No worlds were found in need of migration.");
         }
     }
 
@@ -204,11 +280,11 @@ public class WorldMigrator {
     private static void removeInnerNameFolder(Path worldPath) {
         // We successfully copied the region folder elsewhere so now delete the DIM-1/DIM1 inside
         try {
-            Files.delete(worldPath.resolve("DIM-1"));
+            Files.deleteIfExists(worldPath.resolve("DIM-1"));
         } catch (IOException ignore) {}
 
         try {
-            Files.delete(worldPath.resolve("DIM1"));
+            Files.deleteIfExists(worldPath.resolve("DIM1"));
         } catch (IOException ignore) {}
     }
 }
