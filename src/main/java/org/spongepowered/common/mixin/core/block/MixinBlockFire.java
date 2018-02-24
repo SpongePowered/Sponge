85db5f180fc6c51f6bde1dfb0c4a76c6019db23b
bloodshot <jdroque@gmail.com>
2018-02-24 10:43:08 -0500

    Add missing fire spread event context.

--- src/main/java/org/spongepowered/common/mixin/core/block/MixinBlockFire.java
+++ src/main/java/org/spongepowered/common/mixin/core/block/MixinBlockFire.java
@@ -28,6 +28,8 @@ import net.minecraft.block.BlockFire;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -43,9 +45,11 @@ public abstract class MixinBlockFire extends MixinBlock {
 
     @Redirect(method = "updateTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;I)Z", ordinal = 1))
     private boolean onFireSpread(World world, BlockPos pos, IBlockState state, int updateFlag) {
-        // TODO FIRE_SPREAD flag
-        if (!world.isRemote && SpongeCommonEventFactory.callChangeBlockEventPre((IMixinWorldServer) world, pos).isCancelled()) {
-            return false;
+        if (!world.isRemote) {
+            Sponge.getCauseStackManager().addContext(EventContextKeys.FIRE_SPREAD, (org.spongepowered.api.world.World) world);
+            if (SpongeCommonEventFactory.callChangeBlockEventPre((IMixinWorldServer) world, pos).isCancelled()) {
+                return false;
+            }
         }
         return world.setBlockState(pos, state, updateFlag);
     }
