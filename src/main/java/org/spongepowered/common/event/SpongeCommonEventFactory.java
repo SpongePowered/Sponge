91bbe6c9839d66a7e553b5f6d5cc4aeb70bec906
Anselm Brehme <development.anselm@breh.me>
2018-04-07 13:26:59 +0200

    fix empty Transactions in ChangeInventoryEvent.Transfer.Post fixes #1861

--- src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
+++ src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
@@ -1132,6 +1132,9 @@ public class SpongeCommonEventFactory {
             return;
         }
         Inventory ordered = inv.query(OrderedInventory.class);
+        if (!(ordered instanceof OrderedInventory)) {
+            ordered = ordered.iterator().next();
+        }
         if (ordered instanceof OrderedInventory) {
             Optional<org.spongepowered.api.item.inventory.Slot> slot = ((OrderedInventory) ordered).getSlot(SlotIndex.of(index));
             if (slot.isPresent()) {
@@ -1141,6 +1144,7 @@ public class SpongeCommonEventFactory {
                 captureIn.getCapturedTransactions().add(trans);
             }
         }
+        // else inventory was missing the slot for some reason
     }
 
     /**
@@ -1158,6 +1162,9 @@ public class SpongeCommonEventFactory {
             return transaction.get();
         }
         Inventory ordered = inv.query(OrderedInventory.class);
+        if (!(ordered instanceof OrderedInventory)) {
+            ordered = ordered.iterator().next();
+        }
         if (ordered instanceof OrderedInventory) {
             Optional<org.spongepowered.api.item.inventory.Slot> slot = ((OrderedInventory) ordered).getSlot(SlotIndex.of(index));
             if (slot.isPresent()) {
