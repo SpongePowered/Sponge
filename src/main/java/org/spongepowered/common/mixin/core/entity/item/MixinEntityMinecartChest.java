514bb63adc729f56d79cdbafac892452e06c32ad
Anselm Brehme <development.anselm@breh.me>
2018-06-05 19:54:17 +0200

    move lens creation up to EntityMinecartContainer with override in EntityMinecartChest this fixes mods extending EntityMinecartContainer
    (cherry picked from commit 13668d9)

--- src/main/java/org/spongepowered/common/mixin/core/entity/item/MixinEntityMinecartChest.java
+++ src/main/java/org/spongepowered/common/mixin/core/entity/item/MixinEntityMinecartChest.java
@@ -25,54 +25,22 @@
 package org.spongepowered.common.mixin.core.entity.item;
 
 import net.minecraft.entity.item.EntityMinecartChest;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.api.entity.vehicle.minecart.ChestMinecart;
-import org.spongepowered.api.item.inventory.type.CarriedInventory;
 import org.spongepowered.asm.mixin.Implements;
 import org.spongepowered.asm.mixin.Interface;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.common.item.inventory.adapter.impl.MinecraftInventoryAdapter;
-import org.spongepowered.common.item.inventory.lens.Fabric;
-import org.spongepowered.common.item.inventory.lens.Lens;
-import org.spongepowered.common.item.inventory.lens.SlotProvider;
-import org.spongepowered.common.item.inventory.lens.impl.collections.SlotCollection;
 import org.spongepowered.common.item.inventory.lens.impl.comp.GridInventoryLensImpl;
-import org.spongepowered.common.item.inventory.lens.impl.fabric.IInventoryFabric;
 
 @Mixin(EntityMinecartChest.class)
-@Implements({@Interface(iface = MinecraftInventoryAdapter.class, prefix = "inventory$"), @Interface(iface = ChestMinecart.class, prefix =
-        "minecart$")})
+@Implements(@Interface(iface = ChestMinecart.class, prefix = "minecart$"))
 public abstract class MixinEntityMinecartChest extends MixinEntityMinecartContainer {
-
-    private Fabric<IInventory> fabric;
-    private SlotCollection slots;
-    private Lens<IInventory, ItemStack> lens;
 
     @Inject(method = "<init>*", at = @At("RETURN"))
     public void onConstructed(CallbackInfo ci) {
-        this.fabric = new IInventoryFabric(this);
-        this.slots = new SlotCollection.Builder().add(27).build();
+        // Override default Lens with GridLens
         this.lens = new GridInventoryLensImpl(0, 9, 3, 9, this.slots);
-    }
-
-    @SuppressWarnings("unchecked")
-    public CarriedInventory<ChestMinecart> minecart$getInventory() {
-        return (CarriedInventory<ChestMinecart>) this;
-    }
-
-    public SlotProvider<IInventory, ItemStack> inventory$getSlotProvider() {
-        return this.slots;
-    }
-
-    public Lens<IInventory, ItemStack> inventory$getRootLens() {
-        return this.lens;
-    }
-
-    public Fabric<IInventory> inventory$getFabric() {
-        return this.fabric;
     }
 }
--- src/main/java/org/spongepowered/common/mixin/core/entity/item/MixinEntityMinecartHopper.java
+++ src/main/java/org/spongepowered/common/mixin/core/entity/item/MixinEntityMinecartHopper.java
@@ -25,54 +25,13 @@
 package org.spongepowered.common.mixin.core.entity.item;
 
 import net.minecraft.entity.item.EntityMinecartHopper;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.api.entity.vehicle.minecart.HopperMinecart;
-import org.spongepowered.api.item.inventory.type.CarriedInventory;
 import org.spongepowered.asm.mixin.Implements;
 import org.spongepowered.asm.mixin.Interface;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.common.item.inventory.adapter.impl.MinecraftInventoryAdapter;
-import org.spongepowered.common.item.inventory.lens.Fabric;
-import org.spongepowered.common.item.inventory.lens.Lens;
-import org.spongepowered.common.item.inventory.lens.SlotProvider;
-import org.spongepowered.common.item.inventory.lens.impl.collections.SlotCollection;
-import org.spongepowered.common.item.inventory.lens.impl.comp.OrderedInventoryLensImpl;
-import org.spongepowered.common.item.inventory.lens.impl.fabric.IInventoryFabric;
 
 @Mixin(EntityMinecartHopper.class)
-@Implements({@Interface(iface = MinecraftInventoryAdapter.class, prefix = "inventory$"), @Interface(iface = HopperMinecart.class, prefix =
-        "minecart$")})
+@Implements(@Interface(iface = HopperMinecart.class, prefix = "minecart$"))
 public abstract class MixinEntityMinecartHopper extends MixinEntityMinecartContainer {
 
-    private Fabric<IInventory> fabric;
-    private SlotCollection slots;
-    private Lens<IInventory, ItemStack> lens;
-
-    @Inject(method = "<init>*", at = @At("RETURN"))
-    public void onConstructed(CallbackInfo ci) {
-        this.fabric = new IInventoryFabric(this);
-        this.slots = new SlotCollection.Builder().add(5).build();
-        this.lens = new OrderedInventoryLensImpl(0, 5, 1, this.slots);
-    }
-
-    @SuppressWarnings("unchecked")
-    public CarriedInventory<HopperMinecart> minecart$getInventory() {
-        return (CarriedInventory<HopperMinecart>) this;
-    }
-
-    public SlotProvider<IInventory, ItemStack> inventory$getSlotProvider() {
-        return this.slots;
-    }
-
-    public Lens<IInventory, ItemStack> inventory$getRootLens() {
-        return this.lens;
-    }
-
-    public Fabric<IInventory> inventory$getFabric() {
-        return this.fabric;
-    }
 }
