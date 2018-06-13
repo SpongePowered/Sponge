97cd346e3480d51c9bf6e4b9addddb54e5059723
bloodshot <jdroque@gmail.com>
2018-06-13 01:58:24 -0400

    Revert "Refactor default activation state for TE activation."
    This reverts commit 4e47fea5a3dae7a654d49fb8ce57b155a153e4ec.
    
    I realized default-activation-state can just be controlled by block
    range. A proper fix will come shortly.

--- src/main/java/org/spongepowered/common/config/category/TileEntityActivationModCategory.java
+++ src/main/java/org/spongepowered/common/config/category/TileEntityActivationModCategory.java
@@ -39,14 +39,10 @@ public class TileEntityActivationModCategory extends ConfigCategory {
     private Integer defaultBlockRange;
     @Setting(value = "default-tick-rate")
     private Integer defaultTickRate;
-    @Setting(value = "default-activation-state")
-    private Boolean defaultActivationState;
     @Setting(value = "block-range")
     private Map<String, Integer> tileEntityRangeList = new HashMap<>();
     @Setting(value = "tick-rate")
     private Map<String, Integer> tileEntityTickRateList = new HashMap<>();
-    @Setting(value = "activation-state")
-    private Map<String, Boolean> tileEntityActivationStateList = new HashMap<>();
 
     public TileEntityActivationModCategory() {
     }
@@ -72,10 +68,6 @@ public class TileEntityActivationModCategory extends ConfigCategory {
         return this.defaultBlockRange;
     }
 
-    public Boolean getDefaultActivationState() {
-        return this.defaultActivationState;
-    }
-
     public Integer getDefaultTickRate() {
         return this.defaultTickRate;
     }
@@ -86,9 +78,5 @@ public class TileEntityActivationModCategory extends ConfigCategory {
 
     public Map<String, Integer> getTileEntityTickRateList() {
         return this.tileEntityTickRateList;
-    }
-
-    public Map<String, Boolean> getTileEntityActivationStateList() {
-        return this.tileEntityActivationStateList;
     }
 }
