c1c6aeffb8fe818a54d4630278542ee6f5f321ba
Xakep_SDK <salimhon@gmail.com>
2018-06-28 04:41:42 +0600

    fix 1964

--- src/main/java/org/spongepowered/common/data/DataRegistrar.java
+++ src/main/java/org/spongepowered/common/data/DataRegistrar.java
@@ -43,6 +43,7 @@ import org.spongepowered.api.data.manipulator.mutable.item.*;
 import org.spongepowered.api.data.manipulator.mutable.tileentity.*;
 import org.spongepowered.api.data.property.IntProperty;
 import org.spongepowered.api.effect.potion.PotionEffect;
+import org.spongepowered.api.entity.EntityArchetype;
 import org.spongepowered.api.extra.fluid.FluidStackSnapshot;
 import org.spongepowered.api.item.enchantment.Enchantment;
 import org.spongepowered.api.data.meta.PatternLayer;
@@ -89,6 +90,7 @@ import org.spongepowered.common.data.builder.manipulator.immutable.block.Immutab
 import org.spongepowered.common.data.builder.manipulator.immutable.item.ImmutableItemEnchantmentDataBuilder;
 import org.spongepowered.common.data.property.store.common.InventoryPropertyStore;
 import org.spongepowered.common.effect.potion.PotionEffectContentUpdater;
+import org.spongepowered.common.entity.SpongeEntityArchetypeBuilder;
 import org.spongepowered.common.extra.fluid.SpongeFluidStackSnapshotBuilder;
 import org.spongepowered.common.item.enchantment.SpongeEnchantmentBuilder;
 import org.spongepowered.common.data.builder.meta.SpongePatternLayerBuilder;
@@ -183,6 +185,7 @@ public class DataRegistrar {
 
         // Entity stuff
         dataManager.registerBuilder(EntitySnapshot.class, new SpongeEntitySnapshotBuilder());
+        dataManager.registerBuilder(EntityArchetype.class, new SpongeEntityArchetypeBuilder());
 
         // ItemStack stuff
         dataManager.registerBuilder(ItemStack.class, new SpongeItemStackBuilder());
