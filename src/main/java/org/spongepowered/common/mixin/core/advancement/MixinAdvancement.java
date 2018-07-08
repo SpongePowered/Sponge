f459d0602a23d557008b431bcf3ce6605d30fbed
Aaron Hill <aa1ronham@gmail.com>
2018-07-08 13:07:30 -0400

    Fix issues with advancement registration
    * Allow advancement registration to take place off the server thread
    during intial module reigstration. This ensures that advancement
    registration is able to properly take place on the dedicated client.
    * Make AdvancementRegistryModule dependent on CriterionRegistryModule to
    prevent crash on startup.
    
    Closes SpongePowered/SpongeForge#2283
    
    Conflicts:
    	src/main/java/org/spongepowered/common/mixin/core/advancement/MixinAdvancement.java

--- src/main/java/org/spongepowered/common/mixin/core/advancement/MixinAdvancement.java
+++ src/main/java/org/spongepowered/common/mixin/core/advancement/MixinAdvancement.java
@@ -105,7 +105,7 @@ public class MixinAdvancement implements org.spongepowered.api.advancement.Advan
     private void onInit(ResourceLocation id, @Nullable Advancement parentIn, @Nullable DisplayInfo displayIn,
             AdvancementRewards rewardsIn, Map<String, Criterion> criteriaIn, String[][] requirementsIn, CallbackInfo ci) {
         // Don't do anything on the client, unless we're performing registry initialization
-        if (!this.isMainThread()) {
+        if (!this.isMainThread() && !AdvancementRegistryModule.getInstance().isInInitialRegistration()) {
             return;
         }
         if (displayIn != null) {
