67cb290caefd7c39c32a86b43e334e2286d0644f
Aaron Hill <aa1ronham@gmail.com>
2018-07-09 16:13:52 -0400

    Make getHandlerCache public

--- src/main/java/org/spongepowered/common/event/SpongeEventManager.java
+++ src/main/java/org/spongepowered/common/event/SpongeEventManager.java
@@ -376,7 +376,7 @@ public class SpongeEventManager implements EventManager {
     }
 
     @SuppressWarnings({"ConstantConditions", "unchecked", "rawtypes"})
-    protected RegisteredListener.Cache getHandlerCache(Event event) {
+    public RegisteredListener.Cache getHandlerCache(Event event) {
         checkNotNull(event, "event");
         final Class<? extends Event> eventClass = event.getClass();
         final EventType<? extends Event> eventType;
