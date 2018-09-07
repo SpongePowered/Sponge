fb71c137859e481f8bc03e8f03d8c5aa232941c9
RedNesto <RedNestoMC@gmail.com>
2018-09-07 21:10:25 +0200

    Add test plugin for shulkers

---
+++ testplugins/src/main/java/org/spongepowered/test/ShulkerTest.java
@@ -0,0 +1,122 @@
+package org.spongepowered.test;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.type.DyeColor;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.entity.ShulkerBullet;
+import org.spongepowered.api.entity.living.golem.Shulker;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.block.CollideBlockEvent;
+import org.spongepowered.api.event.entity.CollideEntityEvent;
+import org.spongepowered.api.event.entity.SpawnEntityEvent;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.World;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+@Plugin(id = "shulkertest", version = "0.0.0", name = "ShulkerTest", description = "Play with Shulkers!")
+public class ShulkerTest {
+
+    private Random random = new Random();
+
+    private boolean cancelImpacts;
+    private boolean colorShulker;
+
+    @Listener
+    public void onInit(GameInitializationEvent event) {
+        Sponge.getCommandManager().register(this, CommandSpec.builder().executor((src, args) -> {
+            if (!(src instanceof Player)) {
+                throw new CommandException(Text.of("Only players can execute this command"));
+            }
+
+            ((Player) src).getNearbyEntities(20).forEach(entity -> {
+                if (entity instanceof ShulkerBullet) {
+                    entity.remove(Keys.TARGETED_ENTITY);
+                }
+            });
+
+            return CommandResult.success();
+        }).build(), "untarget");
+
+        Sponge.getCommandManager().register(this, CommandSpec.builder().executor((src, args) -> {
+            this.cancelImpacts = !this.cancelImpacts;
+
+            return CommandResult.success();
+        }).build(), "toggle_impacts");
+
+        Sponge.getCommandManager().register(this, CommandSpec.builder().executor((src, args) -> {
+            this.colorShulker = !this.colorShulker;
+
+            return CommandResult.success();
+        }).build(), "color_shulkers");
+
+        Sponge.getCommandManager().register(this, CommandSpec.builder().executor((src, args) -> {
+            if (!(src instanceof Player)) {
+                throw new CommandException(Text.of("Only players can execute this command"));
+            }
+
+            Iterator<Entity> iterator = ((Player) src).getNearbyEntities(entity -> entity instanceof Shulker).iterator();
+            if (iterator.hasNext()) {
+                Shulker shulker = (Shulker) iterator.next();
+                World world = shulker.getWorld();
+                Random random = new Random();
+                // Creates the spider at a random location within a 10x5x10 zone around the bullet
+                Entity spider = world.createEntity(EntityTypes.SPIDER, shulker.getLocation().add(
+                        random.nextInt(20) - 10,
+                        random.nextInt(5),
+                        random.nextInt(20) - 10).getPosition());
+                world.spawnEntity(spider);
+
+                shulker.launchWithTarget(ShulkerBullet.class, spider);
+            }
+
+            return CommandResult.success();
+        }).build(), "launch");
+
+        Sponge.getCommandManager().register(this, CommandSpec.builder().executor((src, args) -> {
+            if (!(src instanceof Player)) {
+                throw new CommandException(Text.of("Only players can execute this command"));
+            }
+
+            ((Player) src).getNearbyEntities(entity -> entity instanceof ShulkerBullet)
+                    .forEach(bullet -> System.out.println(bullet.get(Keys.TARGETED_ENTITY).orElse(null)));
+            return CommandResult.success();
+        }).build(), "target");
+    }
+
+    @Listener
+    public void onEntitySpawn(SpawnEntityEvent event) {
+        if (!this.colorShulker)
+            return;
+
+        event.getEntities().forEach(entity -> {
+            if (entity instanceof Shulker) {
+                Collection<DyeColor> dyeColors = Sponge.getRegistry().getAllOf(DyeColor.class);
+                DyeColor dyeColor = dyeColors.toArray(new DyeColor[]{})[this.random.nextInt(dyeColors.size())];
+                entity.offer(Keys.DYE_COLOR, dyeColor);
+            }
+        });
+    }
+
+    @Listener
+    public void onBlockImpact(CollideBlockEvent.Impact event) {
+        if (this.cancelImpacts)
+            event.setCancelled(true);
+    }
+
+    @Listener
+    public void onEntityImpact(CollideEntityEvent.Impact event) {
+        if (this.cancelImpacts)
+            event.setCancelled(true);
+    }
+}
