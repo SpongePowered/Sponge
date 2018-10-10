18e3de3b6672332ab1b204bb8370db38886779fc
Ryan Leach <rleach@rleach.id.au>
2018-10-10 20:25:50 +1030

    PlayerSimulator: tweak test plugin to nuke an area

--- testplugins/src/main/java/org/spongepowered/test/PlayerSimulatedTest.java
+++ testplugins/src/main/java/org/spongepowered/test/PlayerSimulatedTest.java
@@ -24,15 +24,13 @@
  */
 package org.spongepowered.test;
 
-import com.google.common.collect.ImmutableList;
+import com.flowpowered.math.vector.Vector3i;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.block.BlockState;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.spec.CommandSpec;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.property.block.InstrumentProperty;
-import org.spongepowered.api.data.type.InstrumentType;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.InteractBlockEvent;
@@ -41,11 +39,10 @@ import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.item.inventory.query.QueryOperation;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.world.World;
 
 import java.util.Optional;
 
@@ -64,9 +61,9 @@ public class PlayerSimulatedTest {
                             enabled = !enabled;
 
                             if (enabled) {
-                                src.sendMessage(Text.of(TextColors.DARK_GREEN,
+                                src.sendMessage(Text.of(TextColors.DARK_RED,
                                         "You have enabled fake-player digging test.\n" +
-                                        "give yourself a diamond pickaxe, with or without silktouch, and right click" +
+                                        "give yourself a diamond pickaxe, with or without silktouch, and right-click " +
                                         "with a different item in your main hand. The pick should take damage, and " +
                                         "the blocks should break instantly, like the pick broke it, with appropriate" +
                                         "causes."));
@@ -89,8 +86,34 @@ public class PlayerSimulatedTest {
 
         Inventory firstSlot = player.getInventory().query(ItemTypes.DIAMOND_PICKAXE).first();
         Optional<ItemStack> first = firstSlot.peek();
-        if(!first.isPresent()) return;
-        player.getWorld().digBlockWith(snapshot.getPosition(), first.get(), player.getProfile());
+
+        if (!first.isPresent()) return;
+
+        if (snapshot.getState().getType().equals(BlockTypes.AIR)) return;
+
+        final World world = player.getWorld();
+        world.digBlockWith(snapshot.getPosition(), first.get(), player.getProfile());
+        world.getExtentView(minNukeBound(snapshot), maxNukeBound(snapshot))
+                .getBlockWorker().iterate(
+                    (volume, x, y, z) -> {
+                        final BlockState block = volume.getBlock(x, y, z);
+
+                        if(!BlockTypes.AIR.equals(block.getType()))
+                            volume.digBlockWith(x,y,z, first.get(), player.getProfile());
+                    }
+                );
+
+
         firstSlot.set(first.get());
+    }
+
+
+
+    private Vector3i maxNukeBound(final BlockSnapshot snapshot) {
+        return snapshot.getPosition().add(2,2,2);
+    }
+
+    private Vector3i minNukeBound(final BlockSnapshot snapshot) {
+        return snapshot.getPosition().sub(2,2,2);
     }
 }
