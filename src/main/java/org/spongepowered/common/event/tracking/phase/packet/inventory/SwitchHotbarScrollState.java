34c30691f38b141181f56b8913307c7bbc440a42
Anselm Brehme <development.anselm@breh.me>
2018-10-20 12:39:58 +0200

    fix SwitchHotbarScrollState not turning off capture after unwind #2112

--- src/main/java/org/spongepowered/common/event/tracking/phase/packet/inventory/SwitchHotbarScrollState.java
+++ src/main/java/org/spongepowered/common/event/tracking/phase/packet/inventory/SwitchHotbarScrollState.java
@@ -44,6 +44,7 @@ import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.item.inventory.transaction.SlotTransaction;
 import org.spongepowered.common.SpongeImpl;
 import org.spongepowered.common.event.tracking.phase.packet.PacketPhaseUtil;
+import org.spongepowered.common.interfaces.IMixinContainer;
 import org.spongepowered.common.item.inventory.util.ContainerUtil;
 import org.spongepowered.common.item.inventory.util.ItemStackUtil;
 
@@ -101,6 +102,7 @@ public final class SwitchHotbarScrollState extends BasicInventoryPacketState {
                 inventory.currentItem = itemChange.getSlotId();
                 player.markPlayerActive();
             }
+            ((IMixinContainer) openContainer).setCaptureInventory(false);
         }
     }
 }
