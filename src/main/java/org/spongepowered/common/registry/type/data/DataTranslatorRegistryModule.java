2b0f8ba097b3106643989d325e5a3b4384f4314d
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2019-01-08 14:15:47 +0100

    Fix GameRegistryEvent.Register<DataTranslator<?>> not triggering.
    Fixes #2169
    
    + Added a test plugin for custom data translators.

--- src/main/java/org/spongepowered/common/registry/type/data/DataTranslatorRegistryModule.java
+++ src/main/java/org/spongepowered/common/registry/type/data/DataTranslatorRegistryModule.java
@@ -27,58 +27,78 @@ package org.spongepowered.common.registry.type.data;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.reflect.TypeToken;
+import ninja.leaping.configurate.objectmapping.serialize.TypeSerializers;
 import org.spongepowered.api.CatalogKey;
 import org.spongepowered.api.data.persistence.DataTranslator;
-import org.spongepowered.api.data.persistence.DataTranslators;
+import org.spongepowered.api.registry.AdditionalCatalogRegistryModule;
 import org.spongepowered.api.registry.AlternateCatalogRegistryModule;
-import org.spongepowered.api.registry.util.RegisterCatalog;
-import org.spongepowered.common.SpongeImpl;
 import org.spongepowered.common.data.persistence.ConfigurateTranslator;
 import org.spongepowered.common.data.persistence.DataSerializers;
+import org.spongepowered.common.data.persistence.DataTranslatorTypeSerializer;
 import org.spongepowered.common.data.persistence.LegacySchematicTranslator;
 import org.spongepowered.common.data.persistence.SchematicTranslator;
 import org.spongepowered.common.registry.AbstractCatalogRegistryModule;
-import org.spongepowered.common.registry.SpongeAdditionalCatalogRegistryModule;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 
-@SuppressWarnings("rawtypes")
-@RegisterCatalog(DataTranslators.class)
-public class DataTranslatorRegistryModule extends AbstractCatalogRegistryModule<DataTranslator>
-    implements AlternateCatalogRegistryModule<DataTranslator>, SpongeAdditionalCatalogRegistryModule<DataTranslator> {
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class DataTranslatorRegistryModule extends AbstractCatalogRegistryModule<DataTranslator> implements
+        AlternateCatalogRegistryModule<DataTranslator>, AdditionalCatalogRegistryModule<DataTranslator> {
 
     public static DataTranslatorRegistryModule getInstance() {
         return Holder.INSTANCE;
     }
 
+    private final Map<Class<?>, DataTranslator> byClass = new HashMap<>();
 
     @Override
-    public void registerAdditionalCatalog(DataTranslator extraCatalog) {
-        checkNotNull(extraCatalog, "CatalogType cannot be null");
-        checkArgument(!extraCatalog.getKey().getValue().isEmpty(), "Id cannot be empty");
-        checkArgument(!this.map.containsKey(extraCatalog.getKey().toString()), "Duplicate Id");
-        this.map.put(extraCatalog.getKey(), extraCatalog);
+    public Map<String, DataTranslator> provideCatalogMap() {
+        final Map<String, DataTranslator> modifierMap = new HashMap<>();
+        for (Map.Entry<CatalogKey, DataTranslator> entry : this.map.entrySet()) {
+            if (entry.getKey().getNamespace().equals("sponge")) {
+                modifierMap.put(entry.getKey().getValue(), entry.getValue());
+            }
+        }
+        return modifierMap;
+    }
+
+    public <T> Optional<DataTranslator<T>> getByClass(Class<T> type) {
+        return Optional.ofNullable(this.byClass.get(checkNotNull(type, "type")));
+    }
+
+    @Override
+    public void registerAdditionalCatalog(DataTranslator dataTranslator) {
+        checkNotNull(dataTranslator, "DataTranslator cannot be null");
+        checkArgument(!this.map.containsKey(dataTranslator.getKey()), "Duplicate Id");
+        final TypeToken<?> typeToken = dataTranslator.getToken();
+        checkNotNull(typeToken, "DataTranslator type token cannot be null");
+        final Class<?> type = typeToken.getRawType();
+        if (this.byClass.containsKey(type)) {
+            throw new IllegalStateException("Already registered the DataTranslator for " + type.getCanonicalName());
+        }
+        this.map.put(dataTranslator.getKey(), dataTranslator);
+        this.byClass.put(type, dataTranslator);
+        if (TypeSerializers.getDefaultSerializers().get(typeToken) == null) {
+            TypeSerializers.getDefaultSerializers().registerType(typeToken, DataTranslatorTypeSerializer.from(dataTranslator));
+        }
     }
 
     @Override
     public void registerDefaults() {
-        DataSerializers.registerSerializers(SpongeImpl.getGame().getDataManager());
-        registerAdditionalCatalog(LegacySchematicTranslator.get());
+        DataSerializers.registerSerializers(this);
+
         registerAdditionalCatalog(SchematicTranslator.get());
         registerAdditionalCatalog(ConfigurateTranslator.instance());
+
+        // Just provide a mapping for the following translator, Schematic is already in use
+        final LegacySchematicTranslator legacySchematicTranslator = LegacySchematicTranslator.get();
+        this.map.put(legacySchematicTranslator.getKey(), legacySchematicTranslator);
     }
 
     DataTranslatorRegistryModule() {
-    }
-
-    @Override
-    public boolean allowsApiRegistration() {
-        return false;
     }
 
     private static final class Holder {
