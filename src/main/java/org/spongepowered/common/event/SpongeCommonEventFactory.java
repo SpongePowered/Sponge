ustc-zzzz/SpongeCommon

Implement ChangeEntityEquipmentEvents

src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
@@ -67,6 +67,7 @@ import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.entity.Transform;
 import org.spongepowered.api.entity.living.Agent;
+import org.spongepowered.api.entity.living.Human;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
@@ -82,6 +83,7 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.EventContext;
 import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
+import org.spongepowered.api.event.entity.ChangeEntityEquipmentEvent;
 import org.spongepowered.api.event.entity.CollideEntityEvent;
 import org.spongepowered.api.event.entity.DestructEntityEvent;
 import org.spongepowered.api.event.entity.InteractEntityEvent;
@@ -133,6 +135,7 @@ import org.spongepowered.common.interfaces.entity.player.IMixinEntityPlayerMP;
 import org.spongepowered.common.interfaces.entity.player.IMixinInventoryPlayer;
 import org.spongepowered.common.interfaces.world.IMixinWorldServer;
 import org.spongepowered.common.item.inventory.adapter.InventoryAdapter;
+import org.spongepowered.common.item.inventory.adapter.impl.slots.SlotAdapter;
 import org.spongepowered.common.item.inventory.custom.CustomInventory;
 import org.spongepowered.common.item.inventory.util.ContainerUtil;
 import org.spongepowered.common.item.inventory.util.InventoryUtil;
@@ -1429,5 +1432,23 @@ public class SpongeCommonEventFactory {
     public static void callPostPlayerRespawnEvent(EntityPlayerMP playerMP, boolean conqueredEnd) {
         // We overwrite this method in SpongeForge, in order to fire
         // Forge's PlayerRespawnEvent
+    }
+
+    public static ChangeEntityEquipmentEvent callChangeEntityEquipmentEvent(EntityLivingBase entity, ItemStackSnapshot before, ItemStackSnapshot after, SlotAdapter slot) {
+        ChangeEntityEquipmentEvent event;
+        Cause currentCause = Sponge.getCauseStackManager().getCurrentCause();
+        Transaction<ItemStackSnapshot> transaction = new Transaction<>(before, after);
+        if (entity instanceof EntityPlayerMP) {
+            Player player = EntityUtil.toPlayer((EntityPlayerMP) entity);
+            event = SpongeEventFactory.createChangeEntityEquipmentEventTargetPlayer(currentCause, player, slot, transaction);
+        } else if (entity instanceof Human) {
+            Human humanoid = (Human) entity;
+            event = SpongeEventFactory.createChangeEntityEquipmentEventTargetHumanoid(currentCause, humanoid, slot, transaction);
+        } else {
+            Living living = EntityUtil.fromNativeToLiving(entity);
+            event = SpongeEventFactory.createChangeEntityEquipmentEventTargetLiving(currentCause, living, slot, transaction);
+        }
+        SpongeImpl.postEvent(event);
+        return event;
     }
 }

src/main/java/org/spongepowered/common/mixin/core/entity/MixinEntityLivingBase.java
@@ -36,8 +36,10 @@ import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.Potion;
@@ -69,6 +71,7 @@ import org.spongepowered.api.event.SpongeEventFactory;
 import org.spongepowered.api.event.cause.entity.damage.DamageFunction;
 import org.spongepowered.api.event.cause.entity.damage.DamageModifier;
 import org.spongepowered.api.event.cause.entity.damage.source.FallingBlockDamageSource;
+import org.spongepowered.api.event.entity.ChangeEntityEquipmentEvent;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.event.item.inventory.UseItemStackEvent;
@@ -105,11 +108,20 @@ import org.spongepowered.common.event.tracking.phase.entity.EntityPhase;
 import org.spongepowered.common.interfaces.entity.IMixinEntityLivingBase;
 import org.spongepowered.common.interfaces.entity.player.IMixinEntityPlayerMP;
 import org.spongepowered.common.interfaces.world.IMixinWorld;
+import org.spongepowered.common.interfaces.entity.player.IMixinInventoryPlayer;
+import org.spongepowered.common.item.inventory.adapter.InventoryAdapter;
+import org.spongepowered.common.item.inventory.adapter.impl.slots.SlotAdapter;
+import org.spongepowered.common.item.inventory.lens.comp.HotbarLens;
+import org.spongepowered.common.item.inventory.lens.impl.fabric.EquipmentSlotFabric;
+import org.spongepowered.common.item.inventory.lens.impl.minecraft.PlayerInventoryLens;
+import org.spongepowered.common.item.inventory.lens.impl.slots.SlotLensImpl;
+import org.spongepowered.common.item.inventory.lens.slots.SlotLens;
 import org.spongepowered.common.item.inventory.util.ItemStackUtil;
 import org.spongepowered.common.registry.type.event.DamageSourceRegistryModule;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -895,6 +907,59 @@ public abstract class MixinEntityLivingBase extends MixinEntity implements Livin
         }
     }
 
+    private EquipmentSlotFabric inventory = new EquipmentSlotFabric(this);
+    private EnumMap<EntityEquipmentSlot, SlotLens> slotLens = new EnumMap<>(EntityEquipmentSlot.class);
+
+    @Inject(method = "onUpdate", locals = LocalCapture.CAPTURE_FAILHARD,
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;getItemStackFromSlot(Lnet/minecraft/inventory/EntityEquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
+    private void onGetItemStackFromSlot(CallbackInfo ci, int i, EntityEquipmentSlot[] slots, int j, int k,
+                                        EntityEquipmentSlot entityEquipmentSlot, ItemStack before) {
+        ItemStack after = this.getItemStackFromSlot(entityEquipmentSlot);
+        EntityLivingBase entity = EntityUtil.toNative((IMixinEntityLivingBase) this);
+        if (!ItemStack.areItemStacksEqual(after, before)) {
+            InventoryAdapter slotAdapter;
+            if (entity instanceof EntityPlayerMP) {
+                SlotLens slotLens;
+                IMixinInventoryPlayer inventory = (IMixinInventoryPlayer) ((EntityPlayerMP) entity).inventory;
+                PlayerInventoryLens inventoryLens = (PlayerInventoryLens) inventory.getRootLens();
+                switch (entityEquipmentSlot) {
+                case OFFHAND:
+                    slotLens = inventoryLens.getOffhandLens();
+                    break;
+                case MAINHAND:
+                    HotbarLens hotbarLens = inventoryLens.getMainLens().getHotbar();
+                    slotLens = hotbarLens.getSlot(hotbarLens.getSelectedSlotIndex(inventory.getFabric()));
+                    break;
+                default:
+                    slotLens = inventoryLens.getEquipmentLens().getSlot(entityEquipmentSlot.getIndex());
+                }
+                slotAdapter = slotLens.getAdapter(inventory.getFabric(), inventory);
+            } else {
+                if (this.slotLens.isEmpty()) {
+                    for (EntityEquipmentSlot slot : EntityEquipmentSlot.values()) {
+                        this.slotLens.put(slot, new SlotLensImpl(slot.getSlotIndex()));
+                    }
+                }
+                slotAdapter = this.slotLens.get(entityEquipmentSlot).getAdapter(this.inventory, null);
+            }
+            ChangeEntityEquipmentEvent event = SpongeCommonEventFactory.callChangeEntityEquipmentEvent(entity,
+                    ItemStackUtil.snapshotOf(before), ItemStackUtil.snapshotOf(after), (SlotAdapter) slotAdapter);
+            if (event.isCancelled()) {
+                this.setItemStackToSlot(entityEquipmentSlot, before);
+                return;
+            }
+            Transaction<ItemStackSnapshot> transaction = event.getTransaction();
+            if (!transaction.isValid()) {
+                this.setItemStackToSlot(entityEquipmentSlot, before);
+                return;
+            }
+            Optional<ItemStackSnapshot> optional = transaction.getCustom();
+            if (optional.isPresent()) {
+                ItemStack custom = ItemStackUtil.fromSnapshotToNative(optional.get());
+                this.setItemStackToSlot(entityEquipmentSlot, custom);
+            }
+        }
+    }
 
     @Override
     public void onSpongeDeathUpdate() {

SpongePowered/Sponge

src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
@@ -62,15 +62,13 @@ import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.block.BlockState;
 import org.spongepowered.api.block.tileentity.TileEntity;
-import org.spongepowered.api.data.DataContainer;
-import org.spongepowered.api.data.DataQuery;
-import org.spongepowered.api.data.Queries;
 import org.spongepowered.api.data.Transaction;
 import org.spongepowered.api.data.type.HandTypes;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.entity.Transform;
 import org.spongepowered.api.entity.living.Agent;
+import org.spongepowered.api.entity.living.Human;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
@@ -86,6 +84,7 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.EventContext;
 import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
+import org.spongepowered.api.event.entity.ChangeEntityEquipmentEvent;
 import org.spongepowered.api.event.entity.CollideEntityEvent;
 import org.spongepowered.api.event.entity.DestructEntityEvent;
 import org.spongepowered.api.event.entity.InteractEntityEvent;
@@ -137,6 +136,7 @@ import org.spongepowered.common.interfaces.entity.player.IMixinEntityPlayerMP;
 import org.spongepowered.common.interfaces.entity.player.IMixinInventoryPlayer;
 import org.spongepowered.common.interfaces.world.IMixinWorldServer;
 import org.spongepowered.common.item.inventory.adapter.InventoryAdapter;
+import org.spongepowered.common.item.inventory.adapter.impl.slots.SlotAdapter;
 import org.spongepowered.common.item.inventory.custom.CustomInventory;
 import org.spongepowered.common.item.inventory.util.ContainerUtil;
 import org.spongepowered.common.item.inventory.util.InventoryUtil;
@@ -1419,5 +1419,26 @@ public class SpongeCommonEventFactory {
                 new Transaction<>(costs, costs), name, ItemStackUtil.snapshotOf(slot1), transaction, ItemStackUtil.snapshotOf(slot2), (Inventory)anvil);
         SpongeImpl.postEvent(event);
         return event;
+    }
+
+    public static ChangeEntityEquipmentEvent callChangeEntityEquipmentEvent(EntityLivingBase entity, ItemStackSnapshot before, ItemStackSnapshot after, SlotAdapter slot) {
+        ChangeEntityEquipmentEvent event;
+        try (CauseStackManager.StackFrame frame = Sponge.getCauseStackManager().pushCauseFrame()) {
+            frame.pushCause(entity);
+            Cause cause = frame.getCurrentCause();
+            Transaction<ItemStackSnapshot> transaction = new Transaction<>(before, after);
+            if (entity instanceof EntityPlayerMP) {
+                Player player = EntityUtil.toPlayer((EntityPlayerMP) entity);
+                event = SpongeEventFactory.createChangeEntityEquipmentEventTargetPlayer(cause, player, slot, transaction);
+            } else if (entity instanceof Human) {
+                Human humanoid = (Human) entity;
+                event = SpongeEventFactory.createChangeEntityEquipmentEventTargetHumanoid(cause, humanoid, slot, transaction);
+            } else {
+                Living living = EntityUtil.fromNativeToLiving(entity);
+                event = SpongeEventFactory.createChangeEntityEquipmentEventTargetLiving(cause, living, slot, transaction);
+            }
+            SpongeImpl.postEvent(event);
+            return event;
+        }
     }
 }

src/main/java/org/spongepowered/common/mixin/core/entity/MixinEntityLivingBase.java
@@ -36,6 +36,7 @@ import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
@@ -71,6 +72,7 @@ import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.cause.entity.damage.DamageFunction;
 import org.spongepowered.api.event.cause.entity.damage.DamageModifier;
 import org.spongepowered.api.event.cause.entity.damage.source.FallingBlockDamageSource;
+import org.spongepowered.api.event.entity.ChangeEntityEquipmentEvent;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.event.item.inventory.UseItemStackEvent;
@@ -106,12 +108,21 @@ import org.spongepowered.common.event.tracking.phase.entity.EntityDeathContext;
 import org.spongepowered.common.event.tracking.phase.entity.EntityPhase;
 import org.spongepowered.common.interfaces.entity.IMixinEntityLivingBase;
 import org.spongepowered.common.interfaces.entity.player.IMixinEntityPlayerMP;
+import org.spongepowered.common.interfaces.entity.player.IMixinInventoryPlayer;
 import org.spongepowered.common.interfaces.world.IMixinWorld;
+import org.spongepowered.common.item.inventory.adapter.InventoryAdapter;
+import org.spongepowered.common.item.inventory.adapter.impl.slots.SlotAdapter;
+import org.spongepowered.common.item.inventory.lens.comp.HotbarLens;
+import org.spongepowered.common.item.inventory.lens.impl.fabric.EquipmentSlotsFabric;
+import org.spongepowered.common.item.inventory.lens.impl.minecraft.PlayerInventoryLens;
+import org.spongepowered.common.item.inventory.lens.impl.slots.SlotLensImpl;
+import org.spongepowered.common.item.inventory.lens.slots.SlotLens;
 import org.spongepowered.common.item.inventory.util.ItemStackUtil;
 import org.spongepowered.common.registry.type.event.DamageSourceRegistryModule;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -897,6 +908,62 @@ public abstract class MixinEntityLivingBase extends MixinEntity implements Livin
         }
     }
 
+    private EquipmentSlotsFabric inventory = new EquipmentSlotsFabric(this);
+    private EnumMap<EntityEquipmentSlot, SlotLens> slotLens = new EnumMap<>(EntityEquipmentSlot.class);
+
+    @Inject(method = "onUpdate", locals = LocalCapture.CAPTURE_FAILHARD,
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;getItemStackFromSlot(Lnet/minecraft/inventory/EntityEquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
+    private void onGetItemStackFromSlot(CallbackInfo ci, int i, EntityEquipmentSlot[] slots, int j, int k,
+                                        EntityEquipmentSlot entityEquipmentSlot, ItemStack before) {
+        if (this.ticksExisted == 1 && (Object)this instanceof EntityPlayer) {
+            return; // Ignore Equipment on player spawn/respawn
+        }
+        ItemStack after = this.getItemStackFromSlot(entityEquipmentSlot);
+        EntityLivingBase entity = EntityUtil.toNative((IMixinEntityLivingBase) this);
+        if (!ItemStack.areItemStacksEqual(after, before)) {
+            InventoryAdapter slotAdapter;
+            if (entity instanceof EntityPlayerMP) {
+                SlotLens slotLens;
+                IMixinInventoryPlayer inventory = (IMixinInventoryPlayer) ((EntityPlayerMP) entity).inventory;
+                PlayerInventoryLens inventoryLens = (PlayerInventoryLens) inventory.getRootLens();
+                switch (entityEquipmentSlot) {
+                    case OFFHAND:
+                        slotLens = inventoryLens.getOffhandLens();
+                        break;
+                    case MAINHAND:
+                        HotbarLens hotbarLens = inventoryLens.getMainLens().getHotbar();
+                        slotLens = hotbarLens.getSlot(hotbarLens.getSelectedSlotIndex(inventory.getFabric()));
+                        break;
+                    default:
+                        slotLens = inventoryLens.getEquipmentLens().getSlot(entityEquipmentSlot.getIndex());
+                }
+                slotAdapter = slotLens.getAdapter(inventory.getFabric(), inventory);
+            } else {
+                if (this.slotLens.isEmpty()) {
+                    for (EntityEquipmentSlot slot : EntityEquipmentSlot.values()) {
+                        this.slotLens.put(slot, new SlotLensImpl(slot.getSlotIndex()));
+                    }
+                }
+                slotAdapter = this.slotLens.get(entityEquipmentSlot).getAdapter(this.inventory, null);
+            }
+            ChangeEntityEquipmentEvent event = SpongeCommonEventFactory.callChangeEntityEquipmentEvent(entity,
+                    ItemStackUtil.snapshotOf(before), ItemStackUtil.snapshotOf(after), (SlotAdapter) slotAdapter);
+            if (event.isCancelled()) {
+                this.setItemStackToSlot(entityEquipmentSlot, before);
+                return;
+            }
+            Transaction<ItemStackSnapshot> transaction = event.getTransaction();
+            if (!transaction.isValid()) {
+                this.setItemStackToSlot(entityEquipmentSlot, before);
+                return;
+            }
+            Optional<ItemStackSnapshot> optional = transaction.getCustom();
+            if (optional.isPresent()) {
+                ItemStack custom = ItemStackUtil.fromSnapshotToNative(optional.get());
+                this.setItemStackToSlot(entityEquipmentSlot, custom);
+            }
+        }
+    }
 
     @Override
     public void onSpongeDeathUpdate() {