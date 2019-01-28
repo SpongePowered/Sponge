2af105cb971117f8633f57f0aede31c332714c9b
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2019-01-28 12:23:39 +0100

    Log a meaningful error when a mod tries to register a invalid smelting recipe.

--- src/main/java/org/spongepowered/common/mixin/core/item/recipe/smelting/MixinFurnaceRecipes.java
+++ src/main/java/org/spongepowered/common/mixin/core/item/recipe/smelting/MixinFurnaceRecipes.java
@@ -46,6 +46,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.common.SpongeImpl;
 import org.spongepowered.common.SpongeImplHooks;
 import org.spongepowered.common.item.inventory.util.ItemStackUtil;
 import org.spongepowered.common.item.recipe.smelting.MatchSmeltingVanillaItemStack;
@@ -128,10 +129,24 @@ public abstract class MixinFurnaceRecipes implements SmeltingRecipeRegistry, Spo
     private void onAddSmeltingRecipe(ResourceLocation p_i48715_1_, String p_i48715_2_, Ingredient p_i48715_3_, ItemStack output, float p_i48715_5_, int p_i48715_6_, CallbackInfo ci) {
         final ItemStackSnapshot result = ItemStackUtil.snapshotOf(output);
         final ItemStackSnapshot ingredient = ItemStackUtil.snapshotOf(input);
-        final Predicate<ItemStackSnapshot> ingredientPredicate = new MatchSmeltingVanillaItemStack(ingredient);
 
         final PluginContainer activeModContainer = SpongeImplHooks.getActiveModContainer();
         final String namespace = activeModContainer == null ? CatalogKey.MINECRAFT_NAMESPACE : activeModContainer.getId();
+
+        // Some mods are doing things wrongly which causes their recipes to be broken,
+        // just log the problem and don't register them. Crashing the server is not an
+        // option either.
+        if (result.isEmpty() || ingredient.isEmpty()) {
+            SpongeImpl.getLogger().error("Invalid smelting recipe registration!\n" +
+                            "The mod \"" + namespace + "\" is registering a smelting recipe wrongly which causes the resulting recipe to be " +
+                            "invalid.\nA common cause which makes smelting recipes invalid occurs when registering those recipes before " +
+                            "the used blocks are\nregistered, it's recommend to register them when the items are finished registering.\n",
+                    new IllegalStateException("Invalid smelting recipe registered by the mod \"" + namespace + "\"!"));
+            return;
+        }
+
+        final Predicate<ItemStackSnapshot> ingredientPredicate = new MatchSmeltingVanillaItemStack(ingredient);
+
 
         final CatalogKey key = CatalogKey.of(namespace,
                 ingredient.getType().getKey().getValue() + "_to_" + result.getType().getKey().getValue());
