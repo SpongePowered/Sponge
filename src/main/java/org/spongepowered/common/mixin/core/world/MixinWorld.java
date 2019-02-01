91ea031bd459053dc0c5557ae681f7a5e344ceff
Jonathan Browne <12983479+JBYoshi@users.noreply.github.com>
2019-02-01 12:07:26 -0600

    Don't convert back and forth between Explosion objects and their unpacked data. (#1368)
    Fixes SpongePowered#1367

--- src/main/java/org/spongepowered/common/mixin/core/world/MixinWorld.java
+++ src/main/java/org/spongepowered/common/mixin/core/world/MixinWorld.java
@@ -236,8 +236,12 @@ public abstract class MixinWorld implements World, IMixinWorld {
     @Shadow public abstract Biome getBiome(BlockPos pos);
     @Shadow public abstract boolean isBlockPowered(BlockPos pos);
     @Shadow public abstract net.minecraft.world.chunk.Chunk getChunk(int chunkX, int chunkZ);
-    @Shadow public abstract net.minecraft.world.Explosion newExplosion(@Nullable net.minecraft.entity.Entity entityIn, double x, double y, double z, float strength,
-            boolean isFlaming, boolean isSmoking);
+    @Shadow public abstract List<net.minecraft.entity.Entity> getEntities(Class<net.minecraft.entity.Entity> entityType,
+            com.google.common.base.Predicate<net.minecraft.entity.Entity> filter);
+    @Shadow public abstract <T extends net.minecraft.entity.Entity> List<T> getEntitiesWithinAABB(Class <? extends T > clazz, AxisAlignedBB aabb,
+            com.google.common.base.Predicate<? super T > filter);
+    @Shadow public abstract List<net.minecraft.entity.Entity> getEntitiesWithinAABBExcludingEntity(net.minecraft.entity.Entity entityIn, AxisAlignedBB bb);
+    @Shadow public abstract MinecraftServer getMinecraftServer();
     // Methods needed for MixinWorldServer & Tracking
     @Shadow public abstract boolean spawnEntity(net.minecraft.entity.Entity entity); // This is overridden in MixinWorldServer
     @Shadow public abstract void updateAllPlayersSleepingFlag();
@@ -680,12 +684,8 @@ public abstract class MixinWorld implements World, IMixinWorld {
     @Override
     public void triggerExplosion(Explosion explosion) {
         checkNotNull(explosion, "explosion");
-        Location origin = explosion.getLocation();
-        checkNotNull(origin, "location");
-        newExplosion(EntityUtil.toNullableNative(explosion.getSourceExplosive().orElse(null)), origin.getX(),
-                origin.getY(), origin.getZ(), explosion.getRadius(), explosion.canCauseFire(),
-                explosion.shouldBreakBlocks()
-        );
+        ((net.minecraft.world.Explosion) explosion).doExplosionA();
+        ((net.minecraft.world.Explosion) explosion).doExplosionB(true);
     }
 
     @Override
