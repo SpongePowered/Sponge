7bc7468f9d263ab8d5725982d0e1fd07d0527094
Anselm Brehme <development.anselm@breh.me>
2019-03-15 17:26:03 +0100

    Fix lens generation for modded inventories that do not use the entire source inventory in containers.

--- src/main/java/org/spongepowered/common/item/inventory/lens/impl/DelegatingLens.java
+++ src/main/java/org/spongepowered/common/item/inventory/lens/impl/DelegatingLens.java
@@ -24,12 +24,17 @@
  */
 package org.spongepowered.common.item.inventory.lens.impl;
 
+import net.minecraft.inventory.Slot;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.common.item.inventory.adapter.InventoryAdapter;
 import org.spongepowered.common.item.inventory.adapter.impl.VanillaAdapter;
 import org.spongepowered.common.item.inventory.lens.Fabric;
 import org.spongepowered.common.item.inventory.lens.Lens;
 import org.spongepowered.common.item.inventory.lens.SlotProvider;
+import org.spongepowered.common.item.inventory.lens.slots.SlotLens;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A delegating Lens used in Containers. Provides ordered inventory access.
@@ -50,9 +55,36 @@ public class DelegatingLens extends AbstractLens {
         this.addChild(delegate);
     }
 
+    public DelegatingLens(int base, List<Slot> containerSlots, Lens lens, SlotProvider slots) {
+        super(base, containerSlots.size(), VanillaAdapter.class);
+        this.delegate = lens;
+        CustomSlotProvider slotProvider = new CustomSlotProvider();
+        for (Slot slot : containerSlots) {
+            // Get slots from original slot provider and add them to custom slot provider in order of actual containerSlots.
+            slotProvider.add(slots.getSlotLens(this.base + slot.slotIndex));
+        }
+        // Provide indexed access over the Container to the slots in the base inventory
+        this.addSpanningChild(new DefaultIndexedLens(this.base, containerSlots.size(), slotProvider));
+        this.addChild(delegate);
+    }
+
     @SuppressWarnings("unchecked")
     @Override
     public InventoryAdapter getAdapter(Fabric inv, Inventory parent) {
         return new VanillaAdapter(inv, this, parent);
+    }
+
+    public static class CustomSlotProvider implements SlotProvider {
+
+        private List<SlotLens> lenses = new ArrayList<>();
+
+        public void add(SlotLens toAdd) {
+            this.lenses.add(toAdd);
+        }
+
+        @Override
+        public SlotLens getSlotLens(int index) {
+            return this.lenses.get(index);
+        }
     }
 }
--- src/main/java/org/spongepowered/common/item/inventory/util/ContainerUtil.java
+++ src/main/java/org/spongepowered/common/item/inventory/util/ContainerUtil.java
@@ -333,7 +333,7 @@ public final class ContainerUtil {
             crafting.base = index;
             crafting.grid = ((InventoryCrafting) subInventory);
         }
-        return new DelegatingLens(index, adapterLens, slots);
+        return new DelegatingLens(index, slotList, adapterLens, slots);
     }
 
     /**
