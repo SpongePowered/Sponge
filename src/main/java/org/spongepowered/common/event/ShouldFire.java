2644b8bf13d358b3188a25c0bb78d312d71a198a
Aaron Hill <aa1ronham@gmail.com>
2019-04-26 17:12:33 -0400

    ShouldFire-ify some inventory events (#2188)

--- src/main/java/org/spongepowered/common/event/ShouldFire.java
+++ src/main/java/org/spongepowered/common/event/ShouldFire.java
@@ -41,7 +41,7 @@ public class ShouldFire {
     // You must always check a flag that either corresponds directly
     // to the event you're firing, or to a supertype of the event.
     // For example, when firing DropItemEvent.Dispense, you can check
-    // ShouldFire.DROP_ITEM_EVENT_DISPENSE or ShouldFire.SPAWN_ENTITY_EVENT
+    // ShouldFire.DROP_ITEM_EVENT_DISPENSE or ShouldFire    .SPAWN_ENTITY_EVENT
     // However, you may *not* check ShouldFire.SPAWN_ENTITY_EVENT_CUSTOM,
     // since SpawnEntityEvent.CUSTOM is not in the hierarchy of DropItemEvent.DISPENSE
 
@@ -62,6 +62,9 @@ public class ShouldFire {
     public static boolean CHANGE_BLOCK_EVENT_PLACE = false;
     public static boolean CHANGE_BLOCK_EVENT_POST = false;
     public static boolean CHANGE_BLOCK_EVENT_GROW = false;
+
+    public static boolean CLICK_INVENTORY_EVENT = false;
+    public static boolean CLICK_INVENTORY_EVENT_DOUBLE = false;
 
     public static boolean DROP_ITEM_EVENT = false;
     public static boolean DROP_ITEM_EVENT_DESTRUCT = false;
