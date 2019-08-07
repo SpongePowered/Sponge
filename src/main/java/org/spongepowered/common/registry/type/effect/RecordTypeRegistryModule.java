1dd74a1f93347afecf2cca0a69e9ec6c66f2fe5b
clienthax <clienthax@gmail.com>
2019-08-07 16:17:01 +0100

    Change RecordType Registration to use ItemTypes. Ids from mods seem to be able to change after registration, + removed magic numbers from codebase.

--- src/main/java/org/spongepowered/common/registry/type/effect/RecordTypeRegistryModule.java
+++ src/main/java/org/spongepowered/common/registry/type/effect/RecordTypeRegistryModule.java
@@ -29,13 +29,25 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.collect.ImmutableList;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemRecord;
+import net.minecraft.util.SoundEvent;
+import org.spongepowered.api.effect.sound.SoundType;
 import org.spongepowered.api.effect.sound.SoundTypes;
 import org.spongepowered.api.effect.sound.record.RecordType;
 import org.spongepowered.api.effect.sound.record.RecordTypes;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.registry.CatalogRegistryModule;
+import org.spongepowered.api.registry.RegistrationPhase;
+import org.spongepowered.api.registry.util.AdditionalRegistration;
+import org.spongepowered.api.registry.util.CustomCatalogRegistration;
 import org.spongepowered.api.registry.util.RegisterCatalog;
 import org.spongepowered.api.registry.util.RegistrationDependency;
 import org.spongepowered.common.effect.record.SpongeRecordType;
+import org.spongepowered.common.mixin.core.item.ItemRecordAccessor;
+import org.spongepowered.common.registry.type.ItemTypeRegistryModule;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -43,7 +55,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 
-@RegistrationDependency(SoundRegistryModule.class)
+@RegistrationDependency({SoundRegistryModule.class, ItemTypeRegistryModule.class})
 public final class RecordTypeRegistryModule implements CatalogRegistryModule<RecordType> {
 
     public static RecordTypeRegistryModule getInstance() {
@@ -52,13 +64,12 @@ public final class RecordTypeRegistryModule implements CatalogRegistryModule<Rec
 
     @RegisterCatalog(RecordTypes.class)
     private final Map<String, RecordType> mappings = new HashMap<>();
-    private final Int2ObjectMap<RecordType> byInternalId = new Int2ObjectOpenHashMap<>();
 
     private RecordTypeRegistryModule() {
     }
 
-    public Optional<RecordType> getByInternalId(int internalId) {
-        return Optional.ofNullable(this.byInternalId.get(internalId));
+    public Optional<RecordType> getByItem(Item itemType) {
+        return Optional.of(mappings.get(Item.REGISTRY.getNameForObject(itemType).toString()));
     }
 
     @Override
@@ -78,26 +89,21 @@ public final class RecordTypeRegistryModule implements CatalogRegistryModule<Rec
     private void add(SpongeRecordType recordType) {
         final String id = recordType.getId();
         this.mappings.put(id, recordType);
-        this.byInternalId.put(recordType.getInternalId(), recordType);
-    }
-
-    @Override
-    public void registerDefaults() {
-        this.add(new SpongeRecordType("minecraft:thirteen", "item.record.13.desc", 2256, SoundTypes.RECORD_13));
-        this.add(new SpongeRecordType("minecraft:cat", "item.record.cat.desc", 2257, SoundTypes.RECORD_CAT));
-        this.add(new SpongeRecordType("minecraft:blocks", "item.record.blocks.desc", 2258, SoundTypes.RECORD_BLOCKS));
-        this.add(new SpongeRecordType("minecraft:chirp", "item.record.chirp.desc", 2259, SoundTypes.RECORD_CHIRP));
-        this.add(new SpongeRecordType("minecraft:far", "item.record.far.desc", 2260, SoundTypes.RECORD_FAR));
-        this.add(new SpongeRecordType("minecraft:mall", "item.record.mall.desc", 2261, SoundTypes.RECORD_MALL));
-        this.add(new SpongeRecordType("minecraft:mellohi", "item.record.mellohi.desc", 2262, SoundTypes.RECORD_MELLOHI));
-        this.add(new SpongeRecordType("minecraft:stal", "item.record.stal.desc", 2263, SoundTypes.RECORD_STAL));
-        this.add(new SpongeRecordType("minecraft:strad", "item.record.strad.desc", 2264, SoundTypes.RECORD_STRAD));
-        this.add(new SpongeRecordType("minecraft:ward", "item.record.ward.desc", 2265, SoundTypes.RECORD_WARD));
-        this.add(new SpongeRecordType("minecraft:eleven", "item.record.11.desc", 2266, SoundTypes.RECORD_11));
-        this.add(new SpongeRecordType("minecraft:wait", "item.record.wait.desc", 2267, SoundTypes.RECORD_WAIT));
     }
 
     private static final class Holder {
         final static RecordTypeRegistryModule INSTANCE = new RecordTypeRegistryModule();
     }
+
+    @CustomCatalogRegistration
+    public void customRegistration() {
+        for (Map.Entry<SoundEvent, ItemRecord> recordEntry : ((ItemRecordAccessor) Items.RECORD_CAT).accessor$getRecords().entrySet()) {
+            final ItemRecord recordItem = recordEntry.getValue();
+            final String key = Item.REGISTRY.getNameForObject(recordItem).toString();
+            if(!mappings.containsKey(key)) {
+                this.add(new SpongeRecordType(key, recordItem.getTranslationKey(), (ItemType) recordItem, (SoundType) ((ItemRecordAccessor) recordItem).accessor$getSoundEvent()));
+            }
+        }
+    }
+
 }
