78b0883e022aae3c3f22c1c6da36f83d26f39f81
Daniel Naylor <git@drnaylor.co.uk>
2020-01-03 13:51:26 +0000

    Lower priority of EntityItemMixin_Activation
    * This ensures superclass field references are reobfuscated.
    
    Fixes #2457

--- src/main/java/org/spongepowered/common/mixin/entityactivation/entity/item/EntityItemMixin_Activation.java
+++ src/main/java/org/spongepowered/common/mixin/entityactivation/entity/item/EntityItemMixin_Activation.java
@@ -32,7 +32,7 @@ import org.spongepowered.common.bridge.world.WorldInfoBridge;
 import org.spongepowered.common.mixin.entityactivation.entity.EntityMixin_Activation;
 import org.spongepowered.common.util.Constants;
 
-@Mixin(EntityItem.class)
+@Mixin(value = EntityItem.class, priority = 1002)
 public abstract class EntityItemMixin_Activation extends EntityMixin_Activation {
 
     @Shadow public abstract ItemStack getItem();
