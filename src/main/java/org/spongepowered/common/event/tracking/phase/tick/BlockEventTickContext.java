bc6fa3e519710c5432bb74db3021abf754ff9a52
bloodshot <jdroque@gmail.com>
2020-01-15 16:37:05 -0500

    Fix queued block events not being sent to players.
    Fixes SpongePowered/SpongeForge#3053

--- src/main/java/org/spongepowered/common/event/tracking/phase/tick/BlockEventTickContext.java
+++ src/main/java/org/spongepowered/common/event/tracking/phase/tick/BlockEventTickContext.java
@@ -37,7 +37,7 @@ public class BlockEventTickContext extends LocationBasedTickContext<BlockEventTi
     protected void reset() {
         super.reset();
         this.wasNotCancelled = true;
-        this.eventSucceeded = false;
+        this.eventSucceeded = true;
     }
 
     public void setWasNotCancelled(final boolean wasNotCancelled) {
