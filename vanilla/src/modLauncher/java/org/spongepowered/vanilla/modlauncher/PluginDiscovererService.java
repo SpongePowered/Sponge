02471f8ab714a65709cff9804fb7b7567d96174b
Chris Sanders <zidane@spongepowered.org>
2020-06-30 00:56:45 -0500

    Fix license headers.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- vanilla/src/modLauncher/java/org/spongepowered/vanilla/modlauncher/PluginDiscovererService.java
+++ vanilla/src/modLauncher/java/org/spongepowered/vanilla/modlauncher/PluginDiscovererService.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.spongepowered.vanilla.modlauncher;
 
 import com.google.common.collect.ImmutableList;
@@ -7,7 +31,6 @@ import cpw.mods.modlauncher.api.ITransformationService;
 import cpw.mods.modlauncher.api.ITransformer;
 import org.spongepowered.common.launch.plugin.PluginLoader;
 import org.spongepowered.plugin.PluginEnvironment;
-import org.spongepowered.plugin.PluginFile;
 import org.spongepowered.plugin.PluginKeys;
 
 import java.nio.file.Path;
@@ -53,12 +76,12 @@ public final class PluginDiscovererService implements ITransformationService {
 
         final List<Map.Entry<String, Path>> launchResources = new ArrayList<>();
 
-        for (final Map.Entry<String, Collection<PluginFile>> resourcesEntry : this.pluginLoader.getResources().entrySet()) {
-            final Collection<PluginFile> resources = resourcesEntry.getValue();
+        for (final Map.Entry<String, Collection<Path>> resourcesEntry : this.pluginLoader.getResources().entrySet()) {
+            final Collection<Path> resources = resourcesEntry.getValue();
             launchResources.addAll(
                 resources
                     .stream()
-                    .map(pluginFile -> Maps.immutableEntry(pluginFile.getRootPath().getFileName().toString(), pluginFile.getRootPath()))
+                    .map(pluginFile -> Maps.immutableEntry(pluginFile.getFileName().toString(), pluginFile))
                     .collect(Collectors.toList())
             );
         }
@@ -69,7 +92,7 @@ public final class PluginDiscovererService implements ITransformationService {
     @Override
     public void onLoad(final IEnvironment env, final Set<String> otherServices) {
         this.pluginEnvironment.getLogger().info("SpongePowered PLUGIN Subsystem Version={} Service=ModLauncher", this.pluginEnvironment.getBlackboard().get(PluginKeys.VERSION).get());
-        this.pluginLoader = new PluginLoader(this.pluginEnvironment);
+        this.pluginLoader = new PluginLoader(this.pluginEnvironment, null);
         this.pluginLoader.discoverServices();
         this.pluginLoader.getServices().forEach((k, v) -> this.pluginLoader.getEnvironment().getLogger().info("Plugin language loader '{}' found.", k));
     }
