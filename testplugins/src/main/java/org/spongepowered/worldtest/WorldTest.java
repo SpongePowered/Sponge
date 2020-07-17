43b89c73d959f2a54aa0817444e003369900c3e4
Chris Sanders <zidane@spongepowered.org>
2020-07-17 00:19:55 -0500

    Push up a new testing plugin for worlds.
    I still need to turn on some core mixins/etc but dual needs to fix commands so this works.
    
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

---
+++ testplugins/src/main/java/org/spongepowered/worldtest/WorldTest.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.worldtest;
+
+import com.google.inject.Inject;
+import org.spongepowered.api.command.Command;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.parameter.Parameter;
+import org.spongepowered.api.command.registrar.StandardCommandRegistrar;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
+import org.spongepowered.api.world.storage.WorldProperties;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.jvm.Plugin;
+
+@Plugin("worldtest")
+public final class WorldTest {
+
+    private final PluginContainer plugin;
+
+    @Inject
+    public WorldTest(final PluginContainer plugin) {
+        this.plugin = plugin;
+    }
+
+    @Listener
+    public void onRegisterCommand(final RegisterCommandEvent<StandardCommandRegistrar> event) {
+        event.getRegistrar().register(this.plugin, Command
+                .builder()
+                .setPermission(this.plugin.getMetadata().getId() + ".command.tpw")
+                .setExecutor(context -> {
+                    final ServerPlayer player = context.requireOne(Parameter.playerOrSource().setKey("player").build());
+                    final WorldProperties world = context.requireOne(Parameter.worldProperties().setKey("world").build());
+
+                    world.getWorld().ifPresent(player::transferToWorld);
+                    return CommandResult.success();
+                })
+                .build()
+        , "tpw", "tpworld");
+    }
+}
