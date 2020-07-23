6d77d9311ab7fdfc9220c10a523d53109a21d390
Gabriel Harris-Rouquette <gabizou@me.com>
2020-07-22 23:00:11 -0700

    Add BlockStateBridge in lieu of SpongeImplHooks
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

---
+++ src/mixins/java/org/spongepowered/common/mixin/core/block/BlockStateMixin.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.common.mixin.core.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.common.bridge.block.BlockStateBridge;
+
+@Mixin(BlockState.class)
+public abstract class BlockStateMixin implements BlockStateBridge {
+
+    // @formatter: off
+    @Shadow public abstract Block shadow$getBlock();
+    @Shadow public abstract int shadow$getLightValue();
+    // @formatter: on
+
+    @Override
+    public boolean bridge$hasTileEntity() {
+        return this.shadow$getBlock() instanceof ITileEntityProvider;
+    }
+
+    @Override
+    public @Nullable TileEntity bridge$createNewTileEntity(World world) {
+        return ((ITileEntityProvider) this.shadow$getBlock()).createNewTileEntity(world);
+    }
+
+    @Override
+    public int bridge$getLightValue(ServerWorld world, BlockPos pos) {
+        return this.shadow$getLightValue();
+    }
+}
