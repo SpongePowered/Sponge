ee0d39ae0294c569b4024ca3ab7bcd5b3970b2e2
kashike <kashike@vq.lc>
2020-08-15 15:30:41 -0700

    Fix silly command-based bossbars using the wrong name

--- src/mixins/java/org/spongepowered/common/mixin/core/server/CustomServerBossInfoMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/server/CustomServerBossInfoMixin.java
@@ -25,10 +25,12 @@
 package org.spongepowered.common.mixin.core.server;
 
 import net.minecraft.server.CustomServerBossInfo;
+import net.minecraft.util.text.ITextComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.common.adventure.SpongeAdventure;
 import org.spongepowered.common.mixin.core.world.ServerBossInfoMixin;
 
 @Mixin(CustomServerBossInfo.class)
@@ -39,6 +41,17 @@ public abstract class CustomServerBossInfoMixin extends ServerBossInfoMixin {
         at = @At(value = "FIELD", target = "Lnet/minecraft/server/CustomServerBossInfo;value:I"))
     private int valueRead(final CustomServerBossInfo $this) {
         return (int) (this.bridge$asAdventure().percent() * this.max);
+    }
+
+    @Redirect(
+        method = {"write"},
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/server/CustomServerBossInfo;name:Lnet/minecraft/util/text/ITextComponent;"
+        )
+    )
+    private ITextComponent nameRead(final CustomServerBossInfo $this) {
+        return SpongeAdventure.asVanilla(this.bridge$asAdventure().name());
     }
 
     // Value writes already update the percent field of superclasses, so we don't need to redirect
