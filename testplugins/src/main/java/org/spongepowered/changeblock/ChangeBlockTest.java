3ae08db2e44062b48b9c154d738573d127de27dc
Anselm Brehme <development.anselm@breh.me>
2020-08-17 17:53:52 +0200

    no spam from ChangeBlockTest by default

--- testplugins/src/main/java/org/spongepowered/changeblock/ChangeBlockTest.java
+++ testplugins/src/main/java/org/spongepowered/changeblock/ChangeBlockTest.java
@@ -31,17 +31,20 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.command.Command;
 import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.parameter.CommandContext;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
 import org.spongepowered.plugin.PluginContainer;
 import org.spongepowered.plugin.jvm.Plugin;
+import org.spongepowered.test.LoadableModule;
 
 @Plugin("changeblocktest")
-public class ChangeBlockTest {
+public class ChangeBlockTest implements LoadableModule {
 
     private static final Marker marker = MarkerManager.getMarker("CHANGEBLOCK");
 
@@ -49,6 +52,11 @@ public class ChangeBlockTest {
     @Inject PluginContainer pluginContainer;
 
     private boolean cancelAll = false;
+
+    @Override
+    public void enable(CommandContext ctx) {
+        Sponge.getEventManager().registerListeners(this.pluginContainer, new ChangeBlockListener());
+    }
 
     @Listener
     public void registerCommands(final RegisterCommandEvent<Command.Parameterized> event) {
@@ -64,18 +72,23 @@ public class ChangeBlockTest {
         );
     }
 
-    @Listener
-    public void onChangeBlock(final ChangeBlockEvent.Post post) {
-        this.pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/*************");
-        this.pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/* ChangeBlockEvent");
-        this.pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/");
-        this.pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/ Cause:");
-        for (final Object o : post.getCause()) {
-            this.pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/ - " + o);
+
+    private class ChangeBlockListener {
+        @Listener
+        public void onChangeBlock(final ChangeBlockEvent.Post post) {
+            final Logger pluginLogger = ChangeBlockTest.this.pluginLogger;
+            pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/*************");
+            pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/* ChangeBlockEvent");
+            pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/");
+            pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/ Cause:");
+            for (final Object o : post.getCause()) {
+                pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/ - " + o);
+            }
+            pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/");
+            if (ChangeBlockTest.this.cancelAll && post.getCause().containsType(BlockSnapshot.class)) {
+                post.setCancelled(true);
+            }
         }
-        this.pluginLogger.log(Level.FATAL, ChangeBlockTest.marker, "/");
-        if (this.cancelAll && post.getCause().containsType(BlockSnapshot.class)) {
-            post.setCancelled(true);
-        }
+
     }
 }
