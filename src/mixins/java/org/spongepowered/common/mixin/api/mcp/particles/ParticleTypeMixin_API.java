504a7bf5d176736cf811925e965bc02f0b2192e8
Christian Hughes <xdotdash@gmail.com>
2020-08-28 20:07:55 -0500

    Expose being able to set default options on internal particle types

--- src/mixins/java/org/spongepowered/common/mixin/api/mcp/particles/ParticleTypeMixin_API.java
+++ src/mixins/java/org/spongepowered/common/mixin/api/mcp/particles/ParticleTypeMixin_API.java
@@ -1,31 +1,38 @@
 package org.spongepowered.common.mixin.api.mcp.particles;
 
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.api.ResourceKey;
 import org.spongepowered.api.effect.particle.ParticleOption;
-import org.spongepowered.api.effect.particle.ParticleType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.common.effect.particle.ParticleTypeExtra;
 
-import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
 @Mixin(net.minecraft.particles.ParticleType.class)
-public abstract class ParticleTypeMixin_API implements ParticleType {
+public abstract class ParticleTypeMixin_API implements ParticleTypeExtra {
 
     private ResourceKey impl$key;
+    private Map<ParticleOption<?>, Object> impl$defaultOptions;
 
+    @Override
+    public Map<ParticleOption<?>, Object> getDefaultOptionMap() {
+        return this.impl$defaultOptions;
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
     public <V> Optional<V> getDefaultOption(ParticleOption<V> option) {
         // TODO
-        return Optional.empty();
+        return Optional.ofNullable((V) this.impl$defaultOptions.get(option));
     }
 
     @Override
     public Map<ParticleOption<?>, Object> getDefaultOptions() {
         // TODO
-        return Collections.emptyMap();
+        return ImmutableMap.copyOf(this.impl$defaultOptions);
     }
 
     @SuppressWarnings("ConstantConditions")
