243778bc880ba7f7698ab9b79ac75542e454bc75
Christian Hughes <xdotdash@gmail.com>
2020-08-29 02:53:58 -0500

    Some more code cleanup and add license headers

--- src/mixins/java/org/spongepowered/common/mixin/api/mcp/particles/ParticleTypeMixin_API.java
+++ src/mixins/java/org/spongepowered/common/mixin/api/mcp/particles/ParticleTypeMixin_API.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.spongepowered.common.mixin.api.mcp.particles;
 
 import com.google.common.collect.ImmutableMap;
@@ -8,21 +32,15 @@ import org.spongepowered.api.ResourceKey;
 import org.spongepowered.api.effect.particle.ParticleOption;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.common.effect.particle.ParticleOptionDefaults;
-import org.spongepowered.common.effect.particle.ParticleTypeExtra;
 
 import java.util.Map;
 import java.util.Optional;
 
 @Mixin(net.minecraft.particles.ParticleType.class)
-public abstract class ParticleTypeMixin_API implements ParticleTypeExtra {
+public abstract class ParticleTypeMixin_API implements org.spongepowered.api.effect.particle.ParticleType {
 
     private ResourceKey impl$key = null;
     private ImmutableMap<ParticleOption<?>, Object> impl$defaultOptions = null;
-
-    @Override
-    public Map<ParticleOption<?>, Object> getDefaultOptionMap() {
-        return this.impl$defaultOptions;
-    }
 
     @SuppressWarnings("unchecked")
     @Override
