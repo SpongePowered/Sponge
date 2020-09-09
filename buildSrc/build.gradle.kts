a1832167e3ce45ceca3067ea12d0048824f64c7d
Gabriel Harris-Rouquette <gabizou@me.com>
2020-09-09 11:11:59 -0700

    Enable GitHub Actions to license check and build testplugins and SpongeForge

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,6 @@ repositories {
 
 
 dependencies {
-    implementation("net.minecraftforge.gradle:ForgeGradle:3.0.184")
+    implementation("net.minecraftforge.gradle:ForgeGradle:3.0.185")
     implementation("org.spongepowered:mixingradle:0.7-SNAPSHOT")
 }
