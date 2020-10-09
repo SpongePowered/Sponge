a6779450ea0b7f5de45cdf4fa99fa81cc327ae7c
Anselm Brehme <development.anselm@breh.me>
2020-10-09 22:11:05 +0200

    merge testplugins

--- testplugins/src/main/java/org/spongepowered/test/projectiletest/ProjectileLaunchTest.java
+++ testplugins/src/main/java/org/spongepowered/test/projectiletest/ProjectileLaunchTest.java
@@ -25,37 +25,51 @@
 package org.spongepowered.test.projectiletest;
 
 import com.google.inject.Inject;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.entity.BlockEntity;
 import org.spongepowered.api.block.entity.carrier.Dispenser;
 import org.spongepowered.api.command.Command;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.exception.CommandException;
+import org.spongepowered.api.command.parameter.CommandContext;
 import org.spongepowered.api.command.parameter.Parameter;
 import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityType;
+import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.entity.projectile.Projectile;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
 import org.spongepowered.api.projectile.source.ProjectileSource;
 import org.spongepowered.api.world.ServerLocation;
+import org.spongepowered.api.world.server.ServerWorld;
+import org.spongepowered.math.vector.Vector3d;
 import org.spongepowered.plugin.PluginContainer;
 import org.spongepowered.plugin.jvm.Plugin;
+import org.spongepowered.test.LoadableModule;
 
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Queue;
 
 @Plugin("projectilelaunchtest")
-public class ProjectileLaunchTest {
+public class ProjectileLaunchTest implements LoadableModule {
 
-    private final PluginContainer plugin;
+    @Inject private PluginContainer plugin;
+    private ProjectileTestListener listeners;
 
-    @Inject
-    public ProjectileLaunchTest(final PluginContainer plugin) {
-        this.plugin = plugin;
+    @Override
+    public void enable(CommandContext ctx) {
+        this.listeners = new ProjectileTestListener();
+        Sponge.getEventManager().registerListeners(this.plugin, this.listeners);
     }
 
     @Listener
@@ -158,4 +172,50 @@ public class ProjectileLaunchTest {
                 .build();
         event.register(this.plugin, triggerDispenserCommand, "triggerdispenser");
     }
+
+
+    public static class ProjectileTestListener {
+
+        private Queue<EntityType<? extends Projectile>> projectileTypes = new LinkedList<>();
+
+        public ProjectileTestListener() {
+            this.projectileTypes.add(EntityTypes.SPECTRAL_ARROW.get());
+            this.projectileTypes.add(EntityTypes.ARROW.get());
+            this.projectileTypes.add(EntityTypes.EGG.get());
+            this.projectileTypes.add(EntityTypes.SMALL_FIREBALL.get());
+            this.projectileTypes.add(EntityTypes.FIREWORK_ROCKET.get());
+            this.projectileTypes.add(EntityTypes.SNOWBALL.get());
+            this.projectileTypes.add(EntityTypes.EXPERIENCE_BOTTLE.get());
+            this.projectileTypes.add(EntityTypes.ENDER_PEARL.get());
+            this.projectileTypes.add(EntityTypes.FIREBALL.get());
+            this.projectileTypes.add(EntityTypes.WITHER_SKULL.get());
+            this.projectileTypes.add(EntityTypes.EYE_OF_ENDER.get());
+            //             this.projectileTypes.add(EntityTypes.FISHING_BOBBER.get());
+            this.projectileTypes.add(EntityTypes.POTION.get());
+            this.projectileTypes.add(EntityTypes.LLAMA_SPIT.get());
+            this.projectileTypes.add(EntityTypes.DRAGON_FIREBALL.get());
+            this.projectileTypes.add(EntityTypes.SHULKER_BULLET.get());
+        }
+
+        @Listener
+        public void onClickBlock(InteractBlockEvent.Secondary event, @First ServerPlayer player) {
+            if (event.getInteractionPoint().isPresent()) {
+                final Vector3d interactionPoint = event.getInteractionPoint().get();
+                final ServerWorld world = player.getWorld();
+                final EntityType<? extends Projectile> nextType = this.projectileTypes.poll();
+                this.projectileTypes.offer(nextType);
+                final Optional<? extends BlockEntity> blockEntity = world.getBlockEntity(interactionPoint.toInt());
+                if (blockEntity.isPresent() && blockEntity.get() instanceof Dispenser) {
+                    ((Dispenser) blockEntity.get()).launchProjectile(nextType);
+                } else {
+                    player.launchProjectile(nextType);
+                }
+                event.setCancelled(true);
+                player.sendMessage(Component.text(nextType.key().toString()));
+            }
+
+        }
+
+    }
+
 }
