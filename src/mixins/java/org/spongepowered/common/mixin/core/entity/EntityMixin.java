90e1142488a4d540fa9b3013cb431e1d5f330694
ImMorpheus <ImMorpheus@users.noreply.github.com>
2020-11-15 22:06:43 +0100

    Nope

--- src/mixins/java/org/spongepowered/common/mixin/core/entity/EntityMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/entity/EntityMixin.java
@@ -278,7 +278,6 @@ public abstract class EntityMixin implements EntityBridge, PlatformEntityBridge,
 
             if (!hasMovementContext) {
                 server.popCause();
-                // check
                 server.removeContext(EventContextKeys.MOVEMENT_TYPE);
             }
 
