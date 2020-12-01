8cc13e8d65d98b4759ae3097546ba7ec736a3eaf
Christian Hughes <xdotdash@gmail.com>
2020-12-01 17:16:39 -0600

    Update ResourceKey for requested changes

--- src/main/java/org/spongepowered/common/registry/SpongeResourceKeyFactory.java
+++ src/main/java/org/spongepowered/common/registry/SpongeResourceKeyFactory.java
@@ -31,21 +31,12 @@ import org.spongepowered.api.ResourceKey;
 public final class SpongeResourceKeyFactory implements ResourceKey.Factory {
 
     @Override
-    public ResourceKey resolve(final String formatted, final String defaultNamespace) {
-        if (formatted.contains(":")) {
-            try {
-                final ResourceLocation resourceLocation = new ResourceLocation(formatted);
-                return (ResourceKey) (Object) resourceLocation;
-            } catch (ResourceLocationException e) {
-                throw new IllegalStateException(e);
-            }
-        } else {
-            try {
-                final ResourceLocation resourceLocation = new ResourceLocation(defaultNamespace, formatted);
-                return (ResourceKey) (Object) resourceLocation;
-            } catch (ResourceLocationException e) {
-                throw new IllegalStateException(e);
-            }
+    public ResourceKey resolve(final String formatted) {
+        try {
+            final ResourceLocation resourceLocation = new ResourceLocation(formatted);
+            return (ResourceKey) (Object) resourceLocation;
+        } catch (ResourceLocationException e) {
+            throw new IllegalStateException(e);
         }
     }
 }
