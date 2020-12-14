b295d0354b2708db1289728823ff050cc272568b
ImMorpheus <ImMorpheus@users.noreply.github.com>
2020-12-14 00:03:39 +0000

    Fix ClassInheritanceMultiMapMixin

--- src/mixins/java/org/spongepowered/common/mixin/core/util/ClassInheritanceMultiMapMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/util/ClassInheritanceMultiMapMixin.java
@@ -38,7 +38,7 @@ public abstract class ClassInheritanceMultiMapMixin {
      *
      * @reason Adds support for finding values by searching for one of their interfaces.
      */
-    @Redirect(method = "getByClass",
+    @Redirect(method = "find",
             at = @At(value = "INVOKE", target = "Ljava/lang/Class;isAssignableFrom(Ljava/lang/Class;)Z", remap = false))
     private boolean impl$isAssignableFromOrInterface(final Class<?> baseClass, final Class<?> clazz) {
         return clazz.isInterface() || baseClass.isAssignableFrom(clazz);
