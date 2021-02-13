6d22ad36015c92f7c7ca433f801a284b15f4349a
Anselm Brehme <development.anselm@breh.me>
2021-02-13 10:45:10 +0100

    update and implement ChunkEvent

---
+++ testplugins/src/main/java/org/spongepowered/test/chunk/ChunkEventTest.java
@@ -0,0 +1,81 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.test.chunk;
+
+import com.google.inject.Inject;
+import net.kyori.adventure.identity.Identity;
+import net.kyori.adventure.text.Component;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.parameter.CommandContext;
+import org.spongepowered.api.event.EventContextKeys;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.event.entity.InteractEntityEvent;
+import org.spongepowered.api.event.item.inventory.InteractItemEvent;
+import org.spongepowered.api.event.world.chunk.ChunkEvent;
+import org.spongepowered.api.registry.RegistryTypes;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.jvm.Plugin;
+import org.spongepowered.test.LoadableModule;
+
+@Plugin("chunkeventtest")
+public final class ChunkEventTest implements LoadableModule {
+
+    private final PluginContainer plugin;
+
+    @Inject
+    public ChunkEventTest(final PluginContainer plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void enable(final CommandContext ctx) {
+        Sponge.getEventManager().registerListeners(this.plugin, new ChunkListener());
+    }
+
+
+    public static class ChunkListener {
+        @Listener
+        public void onChunkLoad(ChunkEvent.Load event) {
+            Sponge.getGame().getSystemSubject().sendMessage(Component.text("Load Chunk " + event.getTargetChunk().getChunkPosition() + " in " + event.getChunkWorld().asString()));
+        }
+
+        @Listener
+        public void onChunkSave(ChunkEvent.Save.Pre event) {
+            event.setCancelled(true);
+            Sponge.getGame().getSystemSubject().sendMessage(Component.text("Pre Save Chunk " + event.getChunkPosition() + " in " + event.getChunkWorld().asString()));
+        }
+
+        @Listener
+        public void onChunkSave(ChunkEvent.Save.Post event) {
+            Sponge.getGame().getSystemSubject().sendMessage(Component.text("Post Save Chunk " + event.getChunkPosition() + " in " + event.getChunkWorld().asString()));
+        }
+
+        @Listener
+        public void onChunkUnload(ChunkEvent.Unload event) {
+            Sponge.getGame().getSystemSubject().sendMessage(Component.text("Unload Chunk " + event.getChunkPosition() + " in " + event.getChunkWorld().asString()));
+        }
+    }
+}
