364a4c5a0d303f6cad4e1700c2a414979c45e35d
ImMorpheus <ImMorpheus@users.noreply.github.com>
2021-02-25 20:25:59 +0100

    Bump to 21w08b

--- vanilla/src/installer/java/org/spongepowered/vanilla/installer/Constants.java
+++ vanilla/src/installer/java/org/spongepowered/vanilla/installer/Constants.java
@@ -32,7 +32,7 @@ public final class Constants {
 
     public static final class Libraries {
 
-        public static final String MINECRAFT_VERSION_TARGET = "21w08a";
+        public static final String MINECRAFT_VERSION_TARGET = "21w08b";
         public static final String MINECRAFT_MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
         public static final String MINECRAFT_PATH_PREFIX = "net/minecraft";
         public static final String MINECRAFT_SERVER_JAR_NAME = "minecraft_server";
