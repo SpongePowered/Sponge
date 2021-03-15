68d312758f14f4a6c23817148c62031659ed8ae0
RedNesto <RedNestoMC@gmail.com>
2021-03-15 11:55:39 +0100

    Expose auto spin attack ticks

--- src/main/java/org/spongepowered/common/data/provider/entity/LivingData.java
+++ src/main/java/org/spongepowered/common/data/provider/entity/LivingData.java
@@ -29,6 +29,7 @@ import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.data.type.BodyParts;
 import org.spongepowered.api.effect.potion.PotionEffect;
 import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.util.Ticks;
 import org.spongepowered.common.accessor.world.entity.EntityAccessor;
 import org.spongepowered.common.accessor.world.entity.LivingEntityAccessor;
 import org.spongepowered.common.bridge.world.entity.LivingEntityBridge;
@@ -73,6 +74,9 @@ public final class LivingData {
                             return false;
                         })
                         .delete(LivingEntity::releaseUsingItem)
+                    .create(Keys.AUTO_SPIN_ATTACK_TICKS)
+                        .get(h -> Ticks.of(((LivingEntityAccessor)h).accessor$autoSpinAttackTicks()))
+                        .set((h, v) -> h.startAutoSpinAttack((int) v.getTicks()))
                     .create(Keys.BODY_ROTATIONS)
                         .get(h -> {
                             final double headYaw = h.getYHeadRot();
@@ -128,6 +132,8 @@ public final class LivingData {
                             h.setHealth(v.floatValue());
                             return true;
                         })
+                    .create(Keys.IS_AUTO_SPIN_ATTACK)
+                        .get(LivingEntity::isAutoSpinAttack)
                     .create(Keys.IS_ELYTRA_FLYING)
                         .get(LivingEntity::isFallFlying)
                         .set((h, v) -> ((EntityAccessor) h).invoker$setSharedFlag(Constants.Entity.ELYTRA_FLYING_FLAG, v))
