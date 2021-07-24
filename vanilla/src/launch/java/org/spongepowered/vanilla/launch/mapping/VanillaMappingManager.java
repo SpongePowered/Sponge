64d5f314540d6888bd9ed5309981fc4a192b7e81
LogicFan <38597904+LogicFan@users.noreply.github.com>
2021-07-23 21:16:51 -0400

    fix styles

--- vanilla/src/launch/java/org/spongepowered/vanilla/launch/mapping/VanillaMappingManager.java
+++ vanilla/src/launch/java/org/spongepowered/vanilla/launch/mapping/VanillaMappingManager.java
@@ -26,19 +26,19 @@ package org.spongepowered.vanilla.launch.mapping;
 
 import org.spongepowered.common.launch.mapping.SpongeMappingManager;
 
-public class VanillaMappingManager implements SpongeMappingManager {
-	@Override
-	public String toRuntimeClassName(String srcName) {
-		return srcName;
-	}
+public final class VanillaMappingManager implements SpongeMappingManager {
+    @Override
+    public String toRuntimeClassName(final String srcName) {
+        return srcName;
+    }
 
-	@Override
-	public String toRuntimeFieldName(Class<?> owner, String srcName) {
-		return srcName;
-	}
+    @Override
+    public String toRuntimeFieldName(final Class<?> owner, final String srcName) {
+        return srcName;
+    }
 
-	@Override
-	public String toRuntimeMethodName(Class<?> owner, String srcName, Class<?>... params) {
-		return srcName;
-	}
+    @Override
+    public String toRuntimeMethodName(final Class<?> owner, final String srcName, final Class<?>... params) {
+        return srcName;
+    }
 }
