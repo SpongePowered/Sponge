60643da20d196a098e5a2e932526e5470defe4f2
ImMorpheus <ImMorpheus@users.noreply.github.com>
2021-09-26 20:26:50 +0200

    remove old destroyBlock redirect
    This was split between Vanilla and Forge in a api-8 commit
    the change was later merged into api-9 but this method was not
    removed from api-9.
    
    See https://github.com/SpongePowered/Sponge/commit/55570ff1cfeaf2bb4b115fda58c4ae9d45a89818

--- src/mixins/java/org/spongepowered/common/mixin/tracker/server/level/ServerPlayerGameModeMixin_Tracker.java
+++ src/mixins/java/org/spongepowered/common/mixin/tracker/server/level/ServerPlayerGameModeMixin_Tracker.java
@@ -187,17 +187,5 @@ public abstract class ServerPlayerGameModeMixin_Tracker {
         }
     }
 
-    @Redirect(method = "destroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;mineBlock(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;)V"))
-    public void impl$onMineBlock(final ItemStack itemStack, final Level param0, final BlockState param1, final BlockPos param2, final Player param3) {
-        final PhaseContext<@NonNull ?> context = PhaseTracker.SERVER.getPhaseContext();
-        final TransactionalCaptureSupplier transactor = context.getTransactor();
-        itemStack.mineBlock(param0, param1, param2, param3);
-        // Needs to get logged as a sideeffect under the BlockChange
-        try (EffectTransactor ignored = context.getTransactor().pushEffect(new ResultingTransactionBySideEffect(InventoryEffect.getInstance()))) {
-            transactor.logPlayerInventoryChange(this.player, PlayerInventoryTransaction.EventCreator.STANDARD);
-            this.player.inventoryMenu.broadcastChanges();
-        }
-    }
-
 
 }
