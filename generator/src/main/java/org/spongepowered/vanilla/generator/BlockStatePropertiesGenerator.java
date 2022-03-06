60923d0ba297efb3353f435ba42fd553d16f171b
Anselm Brehme <development.anselm@breh.me>
2022-03-06 19:23:20 +0100

    BlockStateProperties are not in a registry

--- generator/src/main/java/org/spongepowered/vanilla/generator/BlockStatePropertiesGenerator.java
+++ generator/src/main/java/org/spongepowered/vanilla/generator/BlockStatePropertiesGenerator.java
@@ -24,30 +24,41 @@
  */
 package org.spongepowered.vanilla.generator;
 
-import com.github.javaparser.ast.body.TypeDeclaration;
 import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.ChestType;
+import net.minecraft.world.level.block.state.properties.ComparatorMode;
+import net.minecraft.world.level.block.state.properties.DoorHingeSide;
+import net.minecraft.world.level.block.state.properties.DripstoneThickness;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.Half;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.properties.PistonType;
 import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.properties.RailShape;
+import net.minecraft.world.level.block.state.properties.RedstoneSide;
+import net.minecraft.world.level.block.state.properties.SculkSensorPhase;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.block.state.properties.StairsShape;
+import net.minecraft.world.level.block.state.properties.StructureMode;
+import net.minecraft.world.level.block.state.properties.Tilt;
 import org.tinylog.Logger;
 
 import java.io.IOException;
-import java.util.Comparator;
+import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.IdentityHashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
+import java.util.TreeMap;
 
 import javax.lang.model.element.Modifier;
 
@@ -59,19 +70,16 @@ public class BlockStatePropertiesGenerator implements Generator {
     enum PropertyType {
         ENUM(
             EnumProperty.class,
-            "ENUM_STATE_PROPERTY",
             "EnumStateProperties",
-            ParameterizedTypeName.get(BlockStatePropertiesGenerator.inStatePkg("EnumStateProperty"), Types.WILDCARD)
+            BlockStatePropertiesGenerator.inStatePkg("EnumStateProperty")
         ),
         INTEGER(
             IntegerProperty.class,
-            "INTEGER_STATE_PROPERTY",
             "IntegerStateProperties",
             BlockStatePropertiesGenerator.inStatePkg("IntegerStateProperty")
         ),
         BOOLEAN(
             BooleanProperty.class,
-            "BOOLEAN_STATE_PROPERTY",
             "BooleanStateProperties",
             BlockStatePropertiesGenerator.inStatePkg("BooleanStateProperty")
         );
@@ -79,13 +87,11 @@ public class BlockStatePropertiesGenerator implements Generator {
         private static final Map<Class<?>, PropertyType> knownProperties = new HashMap<>();
 
         private final Class<?> propertyType;
-        private final String registryTypeName; // field in RegistryTypes
         private final String catalogClassName; // class, in CatalogedBy
-        private final TypeName valueType; // X in DefaultedRegistryReference<X>
+        private final ClassName valueType; // X in DefaultedRegistryReference<X>
 
-        PropertyType(final Class<?> propertyType, final String registryTypeName, final String catalogClassName, final TypeName valueType) {
+        PropertyType(final Class<?> propertyType, final String catalogClassName, final ClassName valueType) {
             this.propertyType = propertyType;
-            this.registryTypeName = registryTypeName;
             this.catalogClassName = catalogClassName;
             this.valueType = valueType;
         }
@@ -111,6 +117,34 @@ public class BlockStatePropertiesGenerator implements Generator {
         }
     }
 
+    static Map<Class<?>, TypeName> vanillaEnumTypeMapping = new HashMap<>();
+    static {
+        vanillaEnumTypeMapping.put(StructureMode.class, BlockStatePropertiesGenerator.inDataTypePkg("StructureMode"));
+        vanillaEnumTypeMapping.put(PistonType.class, BlockStatePropertiesGenerator.inDataTypePkg("PistonType"));
+        // vanillaEnumTypeMapping.put(BambooLeaves.class, BlockStatePropertiesGenerator.inDataType()); // TODO missing in API (BambooBlock)
+        // vanillaEnumTypeMapping.put(WallSide.class, BlockStatePropertiesGenerator.inDataType()); // TODO WallData IS_CONNECTED_<dir>
+        // vanillaEnumTypeMapping.put(Direction.Axis.class, BlockStatePropertiesGenerator.inDataType()); // TODO conversion via AxisUtil
+        vanillaEnumTypeMapping.put(RailShape.class, BlockStatePropertiesGenerator.inDataTypePkg("RailDirection"));
+        vanillaEnumTypeMapping.put(AttachFace.class, BlockStatePropertiesGenerator.inDataTypePkg("AttachmentSurface"));
+        vanillaEnumTypeMapping.put(Half.class, BlockStatePropertiesGenerator.inDataTypePkg("PortionType"));
+        // vanillaEnumTypeMapping.put(Direction.class, BlockStatePropertiesGenerator.inDataType()); // TODO conversion via DirectionUtil
+        // vanillaEnumTypeMapping.put(BedPart.class, BlockStatePropertiesGenerator.inDataType()); // TODO conversion via PortionTypeUtil
+        // vanillaEnumTypeMapping.put(DoubleBlockHalf.class, BlockStatePropertiesGenerator.inDataType()); // TODO conversion via PortionTypeUtil
+        vanillaEnumTypeMapping.put(Tilt.class, BlockStatePropertiesGenerator.inDataTypePkg("Tilt"));
+        vanillaEnumTypeMapping.put(RedstoneSide.class, BlockStatePropertiesGenerator.inDataTypePkg("WireAttachmentType"));
+        vanillaEnumTypeMapping.put(ChestType.class, BlockStatePropertiesGenerator.inDataTypePkg("ChestAttachmentType"));
+        vanillaEnumTypeMapping.put(SlabType.class, BlockStatePropertiesGenerator.inDataTypePkg("SlabPortion"));
+        // vanillaEnumTypeMapping.put(BellAttachType.class, BlockStatePropertiesGenerator.inDataType()); // TODO missing in API (BellBlock)
+        vanillaEnumTypeMapping.put(SculkSensorPhase.class, BlockStatePropertiesGenerator.inDataTypePkg("SculkSensorState"));
+        vanillaEnumTypeMapping.put(DoorHingeSide.class, BlockStatePropertiesGenerator.inDataTypePkg("DoorHinge"));
+        vanillaEnumTypeMapping.put(NoteBlockInstrument.class, BlockStatePropertiesGenerator.inDataTypePkg("InstrumentType"));
+        vanillaEnumTypeMapping.put(StairsShape.class, BlockStatePropertiesGenerator.inDataTypePkg("StairShape"));
+        vanillaEnumTypeMapping.put(DripstoneThickness.class, BlockStatePropertiesGenerator.inDataTypePkg("DripstoneSegment"));
+        // vanillaEnumTypeMapping.put(FrontAndTop.class, BlockStatePropertiesGenerator.inDataType()); // TODO missing in API (JigsawBlock)
+        vanillaEnumTypeMapping.put(ComparatorMode.class, BlockStatePropertiesGenerator.inDataTypePkg("ComparatorMode"));
+    }
+
+
     @Override
     public String name() {
         return "block state properties";
@@ -118,79 +152,78 @@ public class BlockStatePropertiesGenerator implements Generator {
 
     @Override
     public void generate(final Context ctx) throws IOException {
-        final Map<PropertyType, Map<Property<?>, Set<ResourceLocation>>> propertyUsages = this.computeUsedProperties();
-
-        for (final var typeToProperty : propertyUsages.entrySet()) {
+        for (final var typeToProperty : this.computeUsedProperties().entrySet()) {
             this.writeCatalogClass(ctx, typeToProperty.getKey(), typeToProperty.getValue());
         }
     }
 
-    private Map<PropertyType, Map<Property<?>, Set<ResourceLocation>>> computeUsedProperties() {
-        // get all block state properties
-        final Map<PropertyType, Map<Property<?>, Set<ResourceLocation>>> propertyUsages = new HashMap<>();
+    private Map<Property<?>, String> vanillaProperties() {
+        final Map<Property<?>, String> vanillaMap = new IdentityHashMap<>();
+        for (Field field : BlockStateProperties.class.getDeclaredFields()) {
+            try {
+                final Object property = field.get(null);
+                if (property instanceof Property<?>) {
+                    vanillaMap.put(((Property<?>) property), field.getName());
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return vanillaMap;
+    }
+
+    private Map<PropertyType, Map<String, Property<?>>> computeUsedProperties() {
+        // get properties used in block states
+        final Map<Property<?>, String> vanillaMap = this.vanillaProperties();
+        final Map<PropertyType, Map<String, Property<?>>> propertyUsages = new HashMap<>();
+        final Map<String, Integer> propertyCount = new HashMap<>();
 
         for (final Block block : Registry.BLOCK) {
             for (final Property<?> property : block.defaultBlockState().getProperties()) {
                 final var type = PropertyType.ofProperty(property);
                 if (type == null) {
                     Logger.warn("Unknown property type for state property {} in block {}", property, Registry.BLOCK.getKey(block));
+                    continue;
                 }
 
-                propertyUsages.computeIfAbsent(type, $ -> new IdentityHashMap<>()).computeIfAbsent(property, $ -> new HashSet<>()).add(Registry.BLOCK.getKey(block));
+                String name = vanillaMap.computeIfAbsent(property, p -> {
+                    // attempt to handle non-vanilla properties:
+                    int cnt = propertyCount.computeIfAbsent(property.getName(), n -> 0) + 1;
+                    return property.getName() + "_" + cnt;
+                });
+                propertyUsages.computeIfAbsent(type, $ -> new TreeMap<>()).put(name, property);
             }
         }
         return propertyUsages;
     }
 
-    private void writeCatalogClass(
-        final Context ctx,
-        final PropertyType type,
-        final Map<Property<?>, Set<ResourceLocation>> properties
-    ) throws IOException {
+    private void writeCatalogClass(final Context ctx, final PropertyType type, final Map<String, Property<?>> properties) throws IOException {
         final var clazz = Types.utilityClass(
             type.catalogClassName,
             "<!-- This file is automatically generated. Any manual changes will be overwritten. -->"
         );
         clazz.addAnnotation(Types.suppressWarnings("unused"));
 
-        final var scopeType = RegistryScope.GAME;
-        clazz.addAnnotation(scopeType.registryScopeAnnotation());
-        final var fieldType = ParameterizedTypeName.get(scopeType.registryReferenceType(), type.valueType);
-        final var factoryMethod = scopeType.registryReferenceFactory(type.registryTypeName, type.valueType);
-
-        properties.entrySet().stream()
-            .flatMap(entry -> entry.getValue().stream()
-                .map(usage -> this.makeField(type.catalogClassName, fieldType, factoryMethod, entry.getKey(), usage)))
-            .sorted(Comparator.comparing(field -> field.name))
-            .forEachOrdered(clazz::addField);
-
-        clazz.addMethod(factoryMethod);
+        properties.forEach((name, property) -> clazz.addMethod(this.makeMethod(type.valueType, name, property)));
 
         ctx.write("state", clazz.build());
-
-        // Then fix up before/after comments
-        final var cu = ctx.compilationUnit("state", type.catalogClassName);
-        final TypeDeclaration<?> classPrimaryType = cu.getPrimaryType().get();
-
-        final var fields = classPrimaryType.getFields();
-        if (!fields.isEmpty()) {
-            fields.get(0).setLineComment("@formatter:off");
-        }
-
-        final var constructors = classPrimaryType.getConstructors();
-        if (!constructors.isEmpty()) {
-            constructors.get(0).setLineComment("@formatter:on");
-        }
-
     }
 
-    private FieldSpec makeField(final String ownType, final TypeName fieldType, final MethodSpec factoryMethod, final Property<?> property, final ResourceLocation usage) {
-        // todo: add extra information from property type (int properties)
+    private MethodSpec makeMethod(final ClassName className, final String name, final Property<?> property) {
+        TypeName returnTypeName = className;
+        if (property instanceof EnumProperty) {
+            final TypeName mappedType = vanillaEnumTypeMapping.getOrDefault(property.getValueClass(), Types.WILDCARD);
+            returnTypeName = ParameterizedTypeName.get(className, mappedType);
+        }
 
-        final String fieldName = Types.keyToFieldName(usage.getPath() + '_' + property.getName());
-        return FieldSpec.builder(fieldType, fieldName, Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
-            .initializer("$L.$N($L)", ownType, factoryMethod, Types.resourceKey(Types.NAMESPACE_SPONGE, fieldName.toLowerCase(Locale.ROOT)))
-            .build();
+        return MethodSpec.methodBuilder("property_" + name).addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(returnTypeName)
+                .addCode("return $T.of($S);", className, name)
+                .build();
+    }
+
+    private static ClassName inDataTypePkg(final String name) {
+        return ClassName.get("org.spongepowered.api.data.type", name);
     }
 
     private static ClassName inStatePkg(final String name) {
