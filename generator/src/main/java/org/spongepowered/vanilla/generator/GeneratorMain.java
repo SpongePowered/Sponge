60b87be8f6fa7a2c089e15e58f6ad3383c7fd2d9
ImMorpheus <ImMorpheus@users.noreply.github.com>
2022-11-03 00:48:24 +0100

    Add ItemRarities and FireworkShapes to generator

--- generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
+++ generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
@@ -47,6 +47,8 @@ import net.minecraft.server.packs.repository.ServerPacksSource;
 import net.minecraft.server.packs.resources.CloseableResourceManager;
 import net.minecraft.server.packs.resources.MultiPackResourceManager;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.item.FireworkRocketItem;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.DataPackConfig;
 import org.tinylog.Logger;
 
@@ -176,10 +178,24 @@ public final class GeneratorMain {
                 "CRITERIA"
             ),
             new EnumEntriesValidator<>(
+                 "item",
+                 "FireworkShapes",
+                  FireworkRocketItem.Shape.class,
+                 "getName",
+                 "sponge"
+            ),
+            new EnumEntriesValidator<>(
                  "entity",
                  "EntityCategories",
                  MobCategory.class,
                  "getName",
+                 "sponge"
+            ),
+            new EnumEntriesValidator<>(
+                 "item",
+                 "ItemRarities",
+                 Rarity.class,
+                 "name",
                  "sponge"
             ),
             new EnumEntriesValidator<>(
