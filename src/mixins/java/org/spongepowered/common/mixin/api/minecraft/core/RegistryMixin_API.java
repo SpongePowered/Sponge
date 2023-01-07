ad1de3940146ec39d95ad516a13d176495b0ed21
Joni Aromaa <me@joniaromaa.fi>
2023-01-07 20:13:11 +0200

    Remove the internal usage of optional from Registry#value (#3806)
    * Remove the internal usage of optional from Registry#value

--- src/mixins/java/org/spongepowered/common/mixin/api/minecraft/core/RegistryMixin_API.java
+++ src/mixins/java/org/spongepowered/common/mixin/api/minecraft/core/RegistryMixin_API.java
@@ -52,6 +52,7 @@ public abstract class RegistryMixin_API<T> implements Registry<T> {
     @Shadow public abstract net.minecraft.resources.ResourceKey<? extends net.minecraft.core.Registry<T>> shadow$key();
     @Shadow @Nullable public abstract ResourceLocation shadow$getKey(T p_177774_1_);
     @Shadow public abstract Optional<T> shadow$getOptional(@Nullable net.minecraft.resources.ResourceLocation param0);
+    @Shadow @Nullable public abstract T shadow$get(@Nullable ResourceLocation var1);
     // @formatter:on
 
 
@@ -91,8 +92,12 @@ public abstract class RegistryMixin_API<T> implements Registry<T> {
 
     @Override
     public <V extends T> V value(final ResourceKey key) {
-        return (V) this.shadow$getOptional((ResourceLocation) (Object) Objects.requireNonNull(key, "key"))
-            .orElseThrow(() -> new ValueNotFoundException(String.format("No value was found for key '%s'!", key)));
+        final V value = (V) this.shadow$get((ResourceLocation) (Object) Objects.requireNonNull(key, "key"));
+        if (value != null) {
+            return value;
+        }
+
+        throw new ValueNotFoundException(String.format("No value was found for key '%s'!", key));
     }
 
     @Override
