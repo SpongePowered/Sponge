7759d5262af7e7c077483f9712d1b35a12ffd08c
Anselm Brehme <development.anselm@breh.me>
2023-01-08 15:32:45 +0100

    fix Sponge ChatType registration

--- src/mixins/java/org/spongepowered/common/mixin/core/resources/RegistryDataLoaderMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/resources/RegistryDataLoaderMixin.java
@@ -22,29 +22,42 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.common.mixin.core.network.chat;
+package org.spongepowered.common.mixin.core.resources;
 
+import com.mojang.serialization.Decoder;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.core.Registry;
+import net.minecraft.core.WritableRegistry;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.ChatTypeDecoration;
+import net.minecraft.resources.RegistryDataLoader;
+import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.api.adventure.ChatTypes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChatType.class)
-public class ChatTypeMixin {
+import java.util.Map;
 
-    @Inject(method = "bootstrap", at = @At("HEAD"))
-    private static void impl$onBootstrap(final BootstapContext<ChatType> $$0, final CallbackInfo ci) {
+@Mixin(RegistryDataLoader.class)
+public class RegistryDataLoaderMixin {
+
+    @SuppressWarnings("unchecked")
+    @Inject(method = "loadRegistryContents", at = @At("RETURN"))
+    private static <E> void impl$afterLoadRegistryContents(final RegistryOps.RegistryInfoLookup $$0, final ResourceManager $$1, final ResourceKey<? extends Registry<E>> $$2,
+            final WritableRegistry<E> $$3, final Decoder<E> $$4, final Map<ResourceKey<?>, Exception> $$5, final CallbackInfo ci)
+    {
         final ChatTypeDecoration narration = ChatTypeDecoration.withSender("chat.type.text.narrate");
 
-        $$0.register(ResourceKey.create(Registries.CHAT_TYPE, (ResourceLocation) (Object) ChatTypes.CUSTOM_CHAT.location()), new ChatType(ChatTypeDecoration.withSender("%s%s"), narration));
-        $$0.register(ResourceKey.create(Registries.CHAT_TYPE, (ResourceLocation) (Object) ChatTypes.CUSTOM_MESSAGE.location()), new ChatType(ChatTypeDecoration.teamMessage("%s%s%s"), narration));
-    }
+        if (Registries.CHAT_TYPE.equals($$2)) {
 
+            $$3.register(ResourceKey.create($$2, (ResourceLocation) (Object) ChatTypes.CUSTOM_CHAT.location()), (E) new ChatType(ChatTypeDecoration.withSender("%s%s"), narration), Lifecycle.stable());
+            $$3.register(ResourceKey.create($$2, (ResourceLocation) (Object) ChatTypes.CUSTOM_MESSAGE.location()), (E) new ChatType(ChatTypeDecoration.teamMessage("%s%s%s"), narration), Lifecycle.stable());
+        }
+    }
 }
