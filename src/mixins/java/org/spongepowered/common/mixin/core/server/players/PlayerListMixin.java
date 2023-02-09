2af6fc189efc289effa5098fc48adadb5f9a556d
Jesse <jessemckee97@gmail.com>
2023-02-08 21:34:52 -0600

    revert changes for StoredUserListMixin

--- src/mixins/java/org/spongepowered/common/mixin/core/server/players/PlayerListMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/server/players/PlayerListMixin.java
@@ -117,12 +117,12 @@ import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 
 import javax.annotation.Nullable;
