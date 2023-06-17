13c9946a48b98a73d5e55079cc78d0987994e31c
Lignium <lignium@yandex.ru>
2023-06-17 08:08:03 +0300

    Return to ServerWorld#saveAndFlush() method

--- src/mixins/java/org/spongepowered/common/mixin/api/minecraft/server/level/ServerLevelMixin_API.java
+++ src/mixins/java/org/spongepowered/common/mixin/api/minecraft/server/level/ServerLevelMixin_API.java
@@ -55,7 +55,6 @@ import org.spongepowered.api.scheduler.ScheduledUpdateList;
 import org.spongepowered.api.util.Ticks;
 import org.spongepowered.api.world.BlockChangeFlag;
 import org.spongepowered.api.world.SerializationBehavior;
-import org.spongepowered.api.world.WorldSaveMode;
 import org.spongepowered.api.world.border.WorldBorder;
 import org.spongepowered.api.world.chunk.WorldChunk;
 import org.spongepowered.api.world.generation.ChunkGenerator;
@@ -78,7 +77,6 @@ import org.spongepowered.common.world.storage.SpongeChunkLayout;
 import org.spongepowered.math.vector.Vector3d;
 import org.spongepowered.math.vector.Vector3i;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
@@ -196,13 +194,21 @@ public abstract class ServerLevelMixin_API extends LevelMixin_API<org.spongepowe
         return ((ServerLevelBridge) this).bridge$getLevelSave().getLevelPath(LevelResource.ROOT);
     }
 
-    @Override
-    public boolean save(final WorldSaveMode worldSaveMode) throws IOException {
+    private boolean save(final boolean flush) {
         final SerializationBehavior behavior = ((PrimaryLevelDataBridge) this.serverLevelData).bridge$serializationBehavior().orElse(SerializationBehavior.AUTOMATIC);
         ((ServerLevelBridge) this).bridge$setManualSave(true);
-        final boolean flush = worldSaveMode.equals(WorldSaveMode.SAVE_AND_FLUSH);
         this.shadow$save(null, flush, false);
         return !behavior.equals(SerializationBehavior.NONE);
+    }
+
+    @Override
+    public boolean save() {
+        return save(false);
+    }
+
+    @Override
+    public boolean saveAndFlush() {
+        return save(true);
     }
 
     @Override
