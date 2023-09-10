81522f79b8ccedae6643783db334fe39b487acbb
Lignium <lignium@yandex.ru>
2023-09-10 13:28:47 +0300

    Fix loot table not being removed when RandomizableContainerBlockEntity is cleared

--- src/mixins/java/org/spongepowered/common/mixin/core/world/level/block/entity/RandomizableContainerBlockEntityMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/world/level/block/entity/RandomizableContainerBlockEntityMixin.java
@@ -24,10 +24,23 @@
  */
 package org.spongepowered.common.mixin.core.world.level.block.entity;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
 
 @Mixin(RandomizableContainerBlockEntity.class)
 public abstract class RandomizableContainerBlockEntityMixin extends BaseContainerBlockEntityMixin {
 
+    @Shadow @Nullable protected ResourceLocation lootTable;
+
+    @Inject(method = "clearContent", at = @At("TAIL"))
+    private void impl$clearContent(final CallbackInfo ci) {
+        this.lootTable = null;
+    }
 }
