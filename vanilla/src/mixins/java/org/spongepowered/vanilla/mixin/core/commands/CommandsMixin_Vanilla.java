36a2ffaa4cde88e788f5d207748b966c37a03161
Gabriel Harris-Rouquette <gabizou@me.com>
2023-09-17 20:08:35 -0700

    fix(1.19.4 update): Update CommandsMixin

--- vanilla/src/mixins/java/org/spongepowered/vanilla/mixin/core/commands/CommandsMixin_Vanilla.java
+++ vanilla/src/mixins/java/org/spongepowered/vanilla/mixin/core/commands/CommandsMixin_Vanilla.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of Sponge, licensed under the MIT License (MIT).
+ * This file is part of ${name}, licensed under the MIT License (MIT).
  *
- * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) SpongePowered <https://spongepowered.org>
  * Copyright (c) contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,24 +21,22 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ *
  */
-package org.spongepowered.forge.mixin.core.commands;
+package org.spongepowered.vanilla.mixin.core.commands;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.server.command.CommandHelper;
 import org.spongepowered.api.command.CommandCause;
 import org.spongepowered.api.event.CauseStackManager;
 import org.spongepowered.api.event.EventContextKeys;
 import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.common.bridge.commands.CommandSourceStackBridge;
@@ -49,53 +47,46 @@ import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
-import java.util.function.Function;
 
 @Mixin(Commands.class)
-public abstract class CommandsMixin_Forge {
+public abstract class CommandsMixin_Vanilla {
 
+    // @formatter:off
+    @Shadow private void shadow$fillUsableCommands(final CommandNode<CommandSourceStack> rootCommandSource,
+                                                   final CommandNode<SharedSuggestionProvider> rootSuggestion,
+                                                   final CommandSourceStack source,
+                                                   final Map<CommandNode<CommandSourceStack>, CommandNode<SharedSuggestionProvider>> commandNodeToSuggestionNode) {
+        throw new AssertionError("This shouldn't be callable");
+    }
+    // @formatter:on
+
+    private CauseStackManager.StackFrame impl$initFrame = null;
     private WeakHashMap<ServerPlayer, Map<CommandNode<CommandSourceStack>, List<CommandNode<SharedSuggestionProvider>>>> impl$playerNodeCache;
     private SpongeCommandManager impl$commandManager;
 
-    // The event fired by Forge is fired in SpongeForgeCommandManager at the appropriate time.
-    @Redirect(method = "performCommand",
-        at = @At(value = "INVOKE", target = "Lnet/minecraftforge/eventbus/api/IEventBus;post(Lnet/minecraftforge/eventbus/api/Event;)Z"))
-    private boolean forge$redirectToSpongeCommandManager(IEventBus instance, Event event) {
-        return false;
-    }
-
-
-    @SuppressWarnings("unchecked")
-    @Redirect(method = "sendCommands", at = @At(
-        value = "INVOKE",
-        target = "Lnet/minecraftforge/server/command/CommandHelper;mergeCommandNode(Lcom/mojang/brigadier/tree/CommandNode;Lcom/mojang/brigadier/tree/CommandNode;Ljava/util/Map;Ljava/lang/Object;Lcom/mojang/brigadier/Command;Ljava/util/function/Function;)V",
-        remap = false
-    ))
-    private <S, T> void impl$addNonBrigSuggestions(
-        final CommandNode<S> sourceChild,
-        final CommandNode<T> sourceNode,
-        final Map<CommandNode<S>, CommandNode<T>> resultNode,
-        final S sourceToResult,
-        final Command<T> canUse,
-        final Function<SuggestionProvider<S>, SuggestionProvider<T>> execute,
-        final ServerPlayer player
-    ) {
+    @Redirect(method = "sendCommands", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/Commands;fillUsableCommands(Lcom/mojang/brigadier/tree/CommandNode;Lcom/mojang/brigadier/tree/CommandNode;Lnet/minecraft/commands/CommandSourceStack;Ljava/util/Map;)V")) // TODO SF 1.19.4
+    private void impl$addNonBrigSuggestions(
+        final Commands commands,
+        final CommandNode<CommandSourceStack> p_197052_1_,
+        final CommandNode<SharedSuggestionProvider> p_197052_2_,
+        final CommandSourceStack p_197052_3_,
+        final Map<CommandNode<CommandSourceStack>, CommandNode<SharedSuggestionProvider>> p_197052_4_,
+        final ServerPlayer playerEntity) {
         try (final CauseStackManager.StackFrame frame = PhaseTracker.getCauseStackManager().pushCauseFrame()) {
-            frame.pushCause(player);
-            frame.addContext(EventContextKeys.SUBJECT, (Subject) player);
-            final CommandCause sourceToUse = ((CommandSourceStackBridge) sourceToResult).bridge$withCurrentCause();
+            frame.pushCause(playerEntity);
+            frame.addContext(EventContextKeys.SUBJECT, (Subject) playerEntity);
+            final CommandCause sourceToUse = ((CommandSourceStackBridge) p_197052_3_).bridge$withCurrentCause();
             try {
-                this.impl$playerNodeCache.put(player, new IdentityHashMap<>());
+                this.impl$playerNodeCache.put(playerEntity, new IdentityHashMap<>());
                 // We use this because the redirects should be a 1:1 mapping (which is what this map is for).
-                final IdentityHashMap<CommandNode<S>, CommandNode<T>> idMap = new IdentityHashMap<>(resultNode);
-                CommandHelper.<S, T>mergeCommandNode(sourceChild, sourceNode, idMap, sourceToResult, canUse, execute);
+                final IdentityHashMap<CommandNode<CommandSourceStack>, CommandNode<SharedSuggestionProvider>> idMap = new IdentityHashMap<>(p_197052_4_);
+                this.shadow$fillUsableCommands(p_197052_1_, p_197052_2_, (CommandSourceStack) sourceToUse, idMap);
             } finally {
-                this.impl$playerNodeCache.remove(player);
+                this.impl$playerNodeCache.remove(playerEntity);
             }
             for (final CommandNode<SharedSuggestionProvider> node : this.impl$commandManager.getNonBrigadierSuggestions(sourceToUse)) {
-                sourceNode.addChild((CommandNode<T>) node);
+                p_197052_2_.addChild(node);
             }
         }
     }
-
 }
