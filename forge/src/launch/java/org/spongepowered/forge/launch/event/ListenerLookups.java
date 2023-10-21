b66c2cd9be93d4462e54e45d0caa8db813b251b2
Yeregorix <yeregorixcraft@gmail.com>
2023-10-21 21:15:24 +0200

    Transform listener classes to obtain privileged Lookups

--- forge/src/launch/java/org/spongepowered/forge/launch/event/ListenerLookups.java
+++ forge/src/launch/java/org/spongepowered/forge/launch/event/ListenerLookups.java
@@ -22,25 +22,22 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.spongepowered.vanilla.launch.event;
+package org.spongepowered.forge.launch.event;
 
-import com.google.inject.Singleton;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.spongepowered.common.event.manager.SpongeEventManager;
-import org.spongepowered.plugin.PluginContainer;
-import org.spongepowered.vanilla.launch.plugin.VanillaJavaPluginContainer;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-@Singleton
-public final class VanillaEventManager extends SpongeEventManager {
+public class ListenerLookups {
+    private static final Map<Class<?>, MethodHandles.Lookup> lookups = new ConcurrentHashMap<>();
 
-    @Override
-    protected MethodHandles.@Nullable Lookup getLookup(final PluginContainer plugin, final Class<?> handle) {
-        if (plugin instanceof VanillaJavaPluginContainer vanilla) {
-            return vanilla.lookup();
-        }
-        return null;
+    public static MethodHandles.@Nullable Lookup get(final Class<?> listenerClass) {
+        return ListenerLookups.lookups.get(listenerClass);
     }
 
+    public static void set(final Class<?> listenerClass, final MethodHandles.Lookup lookup) {
+        ListenerLookups.lookups.put(listenerClass, lookup);
+    }
 }
