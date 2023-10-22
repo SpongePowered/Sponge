b693085b1bc67eeaf38fc2298db92e07e3978816
Yeregorix <yeregorixcraft@gmail.com>
2023-10-22 19:44:28 +0200

    Fix ChatTest translator

--- forge/src/mixins/java/org/spongepowered/forge/mixin/core/network/FriendlyByteBufMixin_Forge.java
+++ forge/src/mixins/java/org/spongepowered/forge/mixin/core/network/FriendlyByteBufMixin_Forge.java
@@ -27,12 +27,14 @@ package org.spongepowered.forge.mixin.core.network;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.common.bridge.network.FriendlyByteBufBridge;
 
 @Mixin(FriendlyByteBuf.class)
 public abstract class FriendlyByteBufMixin_Forge implements FriendlyByteBufBridge {
 
-    // @Redirect(method = "writeItemStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/FriendlyByteBuf;writeNbt(Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/network/FriendlyByteBuf;")) // TODO SF 1.19.4
+    @Redirect(method = "writeItemStack", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/FriendlyByteBuf;writeNbt(Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/network/FriendlyByteBuf;"))
     public FriendlyByteBuf renderItemComponents(final FriendlyByteBuf buf, final CompoundTag tag) {
         return buf.writeNbt(bridge$renderItemComponents(tag));
     }
