SpongePowered/Sponge

commit 1fdc9efb278ed9a7438b0e6a62a2a8b34874bdf1

Fix deadlock related to disconnection

src/mixins/java/org/spongepowered/common/mixin/core/network/ConnectionMixin.java
@@ -184,4 +184,9 @@ public abstract class ConnectionMixin extends SimpleChannelInboundHandler<Packet
             }
         }
     }
+
+    @Inject(method = "disconnect", at = @At(value = "INVOKE", target = "Lio/netty/channel/ChannelFuture;awaitUninterruptibly()Lio/netty/channel/ChannelFuture;"), cancellable = true)
+    private void impl$disconnectAsync(final CallbackInfo ci) {
+        ci.cancel(); //This can cause deadlock within the event loop
+    }
 }

commit 57880c54bc2503ef6d983a4bd2291ee8a45c263a

Fix Connection#isConnected race
Regression from 1fdc9ef.

This is a Netty thing so not much can be done. Could effect
other places that only check for channel#isOpen or theoretically
mods but fixing the deadlock is more important.

src/mixins/java/org/spongepowered/common/mixin/core/network/ConnectionMixin.java
@@ -44,6 +44,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.common.SpongeMinecraftVersion;
 import org.spongepowered.common.accessor.network.Connection_PacketHolderAccessor;
 import org.spongepowered.common.bridge.network.ConnectionBridge;
@@ -79,6 +80,8 @@ public abstract class ConnectionMixin extends SimpleChannelInboundHandler<Packet
     @Nullable private net.minecraft.network.chat.Component impl$kickReason;
 
     private ClientType impl$clientType = ClientType.VANILLA;
+
+    private volatile boolean impl$disconnected;
 
     @Override
     public TransactionStore bridge$getTransactionStore() {
@@ -188,5 +191,16 @@ public abstract class ConnectionMixin extends SimpleChannelInboundHandler<Packet
     @Inject(method = "disconnect", at = @At(value = "INVOKE", target = "Lio/netty/channel/ChannelFuture;awaitUninterruptibly()Lio/netty/channel/ChannelFuture;"), cancellable = true)
     private void impl$disconnectAsync(final CallbackInfo ci) {
         ci.cancel(); //This can cause deadlock within the event loop
+
+        //Because we are now disconnecting asynchronously the channel might not
+        //be immediately flagged as closed so special case it
+        this.impl$disconnected = true;
+    }
+
+    @Inject(method = "isConnected", at = @At("HEAD"), cancellable = true)
+    private void impl$onIsConnected(final CallbackInfoReturnable<Boolean> cir) {
+        if (this.impl$disconnected) {
+            cir.setReturnValue(false);
+        }
     }
 }