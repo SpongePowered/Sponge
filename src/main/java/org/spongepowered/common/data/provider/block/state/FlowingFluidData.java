82234dcc4f9a57ae33eae58f0ef96504ee411094
Anselm Brehme <development.anselm@breh.me>
2024-03-02 20:08:05 +0100

    fix data: fluids

--- src/main/java/org/spongepowered/common/data/provider/block/state/FlowingFluidData.java
+++ src/main/java/org/spongepowered/common/data/provider/block/state/FlowingFluidData.java
@@ -26,8 +26,6 @@ package org.spongepowered.common.data.provider.block.state;
 
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.FlowingFluid;
-import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.api.data.Keys;
 import org.spongepowered.common.data.provider.DataProviderRegistrator;
 import org.spongepowered.common.util.BoundedUtil;
@@ -42,14 +40,8 @@ public final class FlowingFluidData {
         registrator
                 .asImmutable(BlockState.class)
                     .create(Keys.FLUID_LEVEL)
-                        .get(h -> ((LiquidBlock) h.getBlock()).getFluidState(h).getAmount())
-                        .set((h, v) -> {
-                            final FluidState fluidState = ((LiquidBlock) h.getBlock()).getFluidState(h);
-                            if (fluidState.hasProperty(FlowingFluid.LEVEL)) {
-                                return BoundedUtil.setInteger(fluidState, v, FlowingFluid.LEVEL).createLegacyBlock();
-                            }
-                            return null;
-                        })
+                        .get(h -> h.getValue(LiquidBlock.LEVEL))
+                        .set((h, v) -> BoundedUtil.setInteger(h, v, LiquidBlock.LEVEL))
                         .supports(h -> h.getBlock() instanceof LiquidBlock);
     }
     // @formatter:on
