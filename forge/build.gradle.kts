e6b9df7b3c6b4e2d6c933c2d4b7ee77f0876d308
Gabriel Harris-Rouquette <gabizou@me.com>
2024-03-28 13:00:07 +0200

    feat: Publish SpongeForge universal jars

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -491,6 +491,7 @@ publishing {
             artifact(tasks["applaunchSourcesJar"])
             artifact(tasks["launchSourcesJar"])
             artifact(tasks["mixinsSourcesJar"])
+            artifact(tasks["universalJar"])
 
             pom {
                 artifactId = project.name.toLowerCase()
