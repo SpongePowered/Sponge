3ba2c5dbdafff41b6e279cb24510ebba9628b581
ImMorpheus <ImMorpheus@users.noreply.github.com>
2024-03-30 15:27:32 +0100

    Run spotlessApply

--- src/mixins/java/org/spongepowered/common/mixin/core/server/level/ServerLevelMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/server/level/ServerLevelMixin.java
@@ -85,7 +85,6 @@ import org.spongepowered.api.world.weather.Weather;
 import org.spongepowered.api.world.weather.WeatherTypes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
