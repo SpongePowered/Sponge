61a1982145b90d439ee759cfce32515ee932c994
MrHell228 <artemkz2016@gmail.com>
2024-06-11 17:10:01 +0300

    fix Viewer#sendWorldType()

--- src/accessors/java/org/spongepowered/common/accessor/client/multiplayer/ClientLevelAccessor.java
+++ src/accessors/java/org/spongepowered/common/accessor/client/multiplayer/ClientLevelAccessor.java
@@ -25,13 +25,17 @@
 package org.spongepowered.common.accessor.client.multiplayer;
 
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.entity.TransientEntitySectionManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ClientLevel.class)
 public interface ClientLevelAccessor {
 
     @Accessor("entityStorage") TransientEntitySectionManager<Entity> accessor$getEntityStorage();
+
+    @Mutable @Accessor("effects") void accessor$effects(DimensionSpecialEffects effects);
 }
