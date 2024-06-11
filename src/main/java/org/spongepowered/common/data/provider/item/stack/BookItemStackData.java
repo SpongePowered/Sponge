ae6f6c92be89708aef7b4b1c5fd623bfbf141265
MrHell228 <artemkz2016@gmail.com>
2024-06-11 17:18:25 +0300

    fix written book

--- src/main/java/org/spongepowered/common/data/provider/item/stack/BookItemStackData.java
+++ src/main/java/org/spongepowered/common/data/provider/item/stack/BookItemStackData.java
@@ -55,8 +55,7 @@ public final class BookItemStackData {
                             return LegacyComponentSerializer.legacySection().deserialize(content.author());
                         })
                         .set((h, v) -> {
-                            final WrittenBookContent content = h.get(DataComponents.WRITTEN_BOOK_CONTENT);
-                            // TODO handle missing data?
+                            final WrittenBookContent content = h.getOrDefault(DataComponents.WRITTEN_BOOK_CONTENT, WrittenBookContent.EMPTY);
                             final String author = LegacyComponentSerializer.legacySection().serialize(v);
                             h.set(DataComponents.WRITTEN_BOOK_CONTENT,
                                     new WrittenBookContent(content.title(), author, content.generation(), content.pages(), content.resolved()));
@@ -71,11 +70,10 @@ public final class BookItemStackData {
                             return content.generation();
                         })
                         .setAnd((h, v) -> {
-                            if (v < 0) {
+                            if (v < 0 || v > 3) {
                                 return false;
                             }
-                            final WrittenBookContent content = h.get(DataComponents.WRITTEN_BOOK_CONTENT);
-                            // TODO handle missing data?
+                            final WrittenBookContent content = h.getOrDefault(DataComponents.WRITTEN_BOOK_CONTENT, WrittenBookContent.EMPTY);
                             h.set(DataComponents.WRITTEN_BOOK_CONTENT,
                                     new WrittenBookContent(content.title(), content.author(), v, content.pages(), content.resolved()));
                             return true;
@@ -90,15 +88,13 @@ public final class BookItemStackData {
                             return content.pages().stream().map(Filterable::raw).map(SpongeAdventure::asAdventure).toList();
                         })
                         .set((h, v) -> {
-                            final WrittenBookContent content = h.get(DataComponents.WRITTEN_BOOK_CONTENT);
-                            // TODO handle missing data?
+                            final WrittenBookContent content = h.getOrDefault(DataComponents.WRITTEN_BOOK_CONTENT, WrittenBookContent.EMPTY);
                             var pages = v.stream().map(SpongeAdventure::asVanilla).map(Filterable::passThrough).toList();
                             h.set(DataComponents.WRITTEN_BOOK_CONTENT,
                                     new WrittenBookContent(content.title(), content.author(), content.generation(), pages, content.resolved()));
                         })
                         .delete(h -> {
-                            final WrittenBookContent content = h.get(DataComponents.WRITTEN_BOOK_CONTENT);
-                            // TODO handle missing data?
+                            final WrittenBookContent content = h.getOrDefault(DataComponents.WRITTEN_BOOK_CONTENT, WrittenBookContent.EMPTY);
                             h.set(DataComponents.WRITTEN_BOOK_CONTENT,
                                     new WrittenBookContent(content.title(), content.author(), content.generation(), Collections.emptyList(), content.resolved()));
                         })
