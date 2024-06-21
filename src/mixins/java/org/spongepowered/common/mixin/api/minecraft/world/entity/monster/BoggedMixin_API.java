b62df0934e6ec86dca5b39d7c3c0780da72fd1e6
Anselm Brehme <development.anselm@breh.me>
2024-06-22 00:07:56 +0200

    fix Bogged

--- src/mixins/java/org/spongepowered/common/mixin/api/minecraft/world/entity/monster/BoggedMixin_API.java
+++ src/mixins/java/org/spongepowered/common/mixin/api/minecraft/world/entity/monster/BoggedMixin_API.java
@@ -24,10 +24,10 @@
  */
 package org.spongepowered.common.mixin.api.minecraft.world.entity.monster;
 
-import org.spongepowered.api.entity.living.monster.skeleton.Stray;
+import org.spongepowered.api.entity.living.monster.skeleton.Bogged;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(net.minecraft.world.entity.monster.Bogged.class)
-public abstract class BoggedMixin_API extends AbstractSkeletonMixin_API implements Stray {
+public abstract class BoggedMixin_API extends AbstractSkeletonMixin_API implements Bogged {
 
 }
