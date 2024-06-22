5a8e348577a90fd09be733e9e3fe1db41fda0617
Morpheus <ImMorpheus@users.noreply.github.com>
2024-06-22 17:23:09 +0000

    Add SculkSensorStates generator (#4047)

--- generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
+++ generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
@@ -71,6 +71,7 @@ import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.WorldDataConfiguration;
 import net.minecraft.world.level.block.state.properties.BambooLeaves;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.properties.SculkSensorPhase;
 import org.tinylog.Logger;
 
 import java.io.IOException;
@@ -756,6 +757,13 @@ public final class GeneratorMain {
                     "entity.display",
                     "BillboardTypes",
                     Display.BillboardConstraints.class,
+                    "getSerializedName",
+                    "sponge"
+            ),
+            new EnumEntriesValidator<>(
+                    "data.type",
+                    "SculkSensorStates",
+                    SculkSensorPhase.class,
                     "getSerializedName",
                     "sponge"
             ),
