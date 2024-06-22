b1ac5d9bf05b660ed935b53424bc8eb833156629
ImMorpheus <ImMorpheus@users.noreply.github.com>
2024-06-22 18:31:24 +0200

    Add EquipmentGroups generator

--- generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
+++ generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
@@ -49,6 +49,7 @@ import net.minecraft.server.packs.resources.MultiPackResourceManager;
 import net.minecraft.world.damagesource.DamageEffects;
 import net.minecraft.world.damagesource.DamageScaling;
 import net.minecraft.world.entity.Display;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.animal.Fox;
 import net.minecraft.world.entity.animal.TropicalFish;
@@ -695,6 +696,13 @@ public final class GeneratorMain {
                     "ItemDisplayTypes",
                     ItemDisplayContext.class,
                     "getSerializedName",
+                    "sponge"
+            ),
+            new EnumEntriesValidator<>(
+                    "item.inventory.equipment",
+                    "EquipmentGroups",
+                    EquipmentSlot.Type.class,
+                    "name",
                     "sponge"
             ),
             new EnumEntriesValidator<>(
