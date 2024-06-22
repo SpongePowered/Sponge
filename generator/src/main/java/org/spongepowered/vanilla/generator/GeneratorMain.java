4df836cdaec45fa784400d00a839e34cf9d0e944
ImMorpheus <ImMorpheus@users.noreply.github.com>
2024-06-22 02:26:49 +0200

    Update blockstate property and blockstate propery keys generators

--- generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
+++ generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
@@ -585,8 +585,7 @@ public final class GeneratorMain {
             ),
             new BlockStateDataProviderGenerator(),
             new BlockStatePropertiesGenerator(),
-            // TODO fix me
-            //new BlockStatePropertyKeysGenerator(),
+            new BlockStatePropertyKeysGenerator(),
             new RegistryEntriesGenerator<>(
                     "world.generation.feature",
                     "PlacedFeatures",
