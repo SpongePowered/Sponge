fb9a737caa867608e39547e4b9445107d4899382
Morpheus <ImMorpheus@users.noreply.github.com>
2024-06-22 17:23:45 +0000

    Add RabbitTypes generator (#4040)

--- generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
+++ generator/src/main/java/org/spongepowered/vanilla/generator/GeneratorMain.java
@@ -56,6 +56,7 @@ import net.minecraft.world.entity.animal.Fox;
 import net.minecraft.world.entity.animal.MushroomCow;
 import net.minecraft.world.entity.animal.Panda;
 import net.minecraft.world.entity.animal.Parrot;
+import net.minecraft.world.entity.animal.Rabbit;
 import net.minecraft.world.entity.animal.TropicalFish;
 import net.minecraft.world.entity.animal.horse.Llama;
 import net.minecraft.world.entity.animal.horse.Markings;
@@ -293,6 +294,13 @@ public final class GeneratorMain {
                  "SpellTypes",
                  SpellcasterIllager.IllagerSpell.class,
                  "name",
+                 "sponge"
+            ),
+            new EnumEntriesValidator<>(
+                 "data.type",
+                 "RabbitTypes",
+                 Rabbit.Variant.class,
+                 "getSerializedName",
                  "sponge"
             ),
             new EnumEntriesValidator<>(
