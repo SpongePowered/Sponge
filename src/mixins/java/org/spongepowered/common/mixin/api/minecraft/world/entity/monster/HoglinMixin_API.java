a035cc9a1771021d25983d6f41d59893734b229b
ImMorpheus <ImMorpheus@users.noreply.github.com>
2024-06-29 22:19:02 +0200

    Update Hoglin reference

--- src/mixins/java/org/spongepowered/common/mixin/api/minecraft/world/entity/monster/HoglinMixin_API.java
+++ src/mixins/java/org/spongepowered/common/mixin/api/minecraft/world/entity/monster/HoglinMixin_API.java
@@ -24,7 +24,7 @@
  */
 package org.spongepowered.common.mixin.api.minecraft.world.entity.monster;
 
-import org.spongepowered.api.entity.living.monster.hoglin.Hoglin;
+import org.spongepowered.api.entity.living.animal.Hoglin;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(net.minecraft.world.entity.monster.hoglin.Hoglin.class)
