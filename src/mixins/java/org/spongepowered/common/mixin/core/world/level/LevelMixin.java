1e8c4d4f18e5482ac3723a951c33540357e32385
Anselm Brehme <development.anselm@breh.me>
2024-07-12 20:17:55 +0200

    fix build

--- src/mixins/java/org/spongepowered/common/mixin/core/world/level/LevelMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/world/level/LevelMixin.java
@@ -42,7 +42,6 @@ import net.minecraft.world.entity.decoration.Painting;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.projectile.ThrownEnderpearl;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.FireworkExplosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Blocks;
