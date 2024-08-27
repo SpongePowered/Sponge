1f1b67a7592968ad808b7e22b2545f433b57d3ac
Anselm Brehme <development.anselm@breh.me>
2024-08-27 19:16:20 +0200

    fix attack crit

--- src/mixins/java/org/spongepowered/common/mixin/core/world/entity/player/PlayerMixin_Attack_Impl.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/world/entity/player/PlayerMixin_Attack_Impl.java
@@ -169,7 +169,7 @@ public abstract class PlayerMixin_Attack_Impl extends LivingEntityMixin_Attack_I
      */
     @ModifyVariable(method = "attack", ordinal = 2,
             slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;isSprinting()Z", ordinal = 1),
-                           to = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/player/Player;walkDist:F")),
+                           to = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getKnownMovement()Lnet/minecraft/world/phys/Vec3;")),
             at = @At(value = "JUMP", opcode = Opcodes.IFEQ)
     )
     public boolean attackImpl$critHook(final boolean isCrit) {
