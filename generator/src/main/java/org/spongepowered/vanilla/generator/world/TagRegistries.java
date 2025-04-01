7c1242b26c88c5fb3e5949cac16274f9d0799ac7
MrHell228 <artemkz2016@gmail.com>
2025-04-01 06:03:00 +0300

    Add missing tags

--- generator/src/main/java/org/spongepowered/vanilla/generator/world/TagRegistries.java
+++ generator/src/main/java/org/spongepowered/vanilla/generator/world/TagRegistries.java
@@ -79,13 +79,40 @@ public class TagRegistries {
                 "tag",
                 "EnchantmenTypeTags"
             ),
-
             new TagGenerator(
                 "DAMAGE_TYPE",
                 Registries.DAMAGE_TYPE,
                 context.relativeClass("event.cause.entity.damage", "DamageType"),
                 "tag",
                 "DamageTypeTags"
+            ),
+            new TagGenerator(
+                "BANNER_PATTERN_SHAPE",
+                Registries.BANNER_PATTERN,
+                context.relativeClass("data.type", "BannerPatternShape"),
+                "tag",
+                "BannerPatternShapeTags"
+            ),
+            new TagGenerator(
+                "CAT_TYPE",
+                Registries.CAT_VARIANT,
+                context.relativeClass("data.type", "CatType"),
+                "tag",
+                "CatTypeTags"
+            ),
+            new TagGenerator(
+                "ART_TYPE",
+                Registries.PAINTING_VARIANT,
+                context.relativeClass("data.type", "ArtType"),
+                "tag",
+                "ArtTypeTags"
+            ),
+            new TagGenerator(
+                "STRUCTURE",
+                Registries.STRUCTURE,
+                context.relativeClass("world.generation.structure", "Structure"),
+                "tag",
+                "StructureTags"
             )
         );
     }
