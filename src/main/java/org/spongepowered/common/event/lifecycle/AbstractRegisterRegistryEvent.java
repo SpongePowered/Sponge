952b0a3e8cf46794930be81ddcab4e7e920a8b83
MrHell228 <artemkz2016@gmail.com>
2025-10-03 22:51:10 +0300

    properly log exception

--- src/main/java/org/spongepowered/common/event/lifecycle/AbstractRegisterRegistryEvent.java
+++ src/main/java/org/spongepowered/common/event/lifecycle/AbstractRegisterRegistryEvent.java
@@ -36,6 +36,7 @@ import org.spongepowered.api.registry.DuplicateRegistrationException;
 import org.spongepowered.api.registry.RegistryHolder;
 import org.spongepowered.api.registry.RegistryRoots;
 import org.spongepowered.api.registry.RegistryType;
+import org.spongepowered.common.SpongeCommon;
 import org.spongepowered.common.event.tracking.PhaseTracker;
 import org.spongepowered.common.registry.RegistryLoader;
 import org.spongepowered.common.registry.SpongeRegistryHolder;
@@ -69,7 +70,7 @@ public abstract class AbstractRegisterRegistryEvent extends AbstractLifecycleEve
 
         final SpongeRegistryHolder holder = this.getHolder();
         final RegistryType<T> type = RegistryType.of(RegistryRoots.SPONGE, key);
-        holder.createRegistry(type, () -> this.wrapWithScopedHolder(holder, defaultValues), isDynamic);
+        holder.createRegistry(type, () -> this.wrapWithScopedHolder(type, holder, defaultValues), isDynamic);
         return type;
     }
 
@@ -82,16 +83,16 @@ public abstract class AbstractRegisterRegistryEvent extends AbstractLifecycleEve
 
         final SpongeRegistryHolder holder = this.getHolder();
         final RegistryType<T> type = RegistryType.of(RegistryRoots.SPONGE, key);
-        holder.createRegistry(type, actualHolder -> this.wrapWithScopedHolder(holder, () -> defaultValues.apply(actualHolder)), isDynamic, dependencies);
+        holder.createRegistry(type, actualHolder -> this.wrapWithScopedHolder(type, holder, () -> defaultValues.apply(actualHolder)), isDynamic, dependencies);
         return type;
     }
 
-    private <T> Map<ResourceKey, T> wrapWithScopedHolder(final SpongeRegistryHolder holder, final Supplier<Map<ResourceKey, T>> defaultValues) {
+    private <T> Map<ResourceKey, T> wrapWithScopedHolder(final RegistryType<T> registry, final SpongeRegistryHolder holder, final Supplier<Map<ResourceKey, T>> defaultValues) {
         return PhaseTracker.getInstance().switchStartupRegistryHolder(
             holder, defaultValues,
             (exception) -> {
                 // If we don't catch it, vanilla will shut the server with "Failed to load datapacks"
-                exception.printStackTrace();
+                SpongeCommon.logger().error("Could not supply default values for registry {}", registry.toString(), exception);
                 return Map.of();
             });
     }
