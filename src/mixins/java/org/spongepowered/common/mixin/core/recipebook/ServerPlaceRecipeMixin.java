1d935e24596019ca4a50ec2bbb7a4c91ce96f449
MrHell228 <artemkz2016@gmail.com>
2025-10-26 20:03:15 +0300

    fail finding slot when exemplary and craft input stacks can't stack

--- src/mixins/java/org/spongepowered/common/mixin/core/recipebook/ServerPlaceRecipeMixin.java
+++ src/mixins/java/org/spongepowered/common/mixin/core/recipebook/ServerPlaceRecipeMixin.java
@@ -27,15 +27,16 @@ package org.spongepowered.common.mixin.core.recipebook;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.Holder;
 import net.minecraft.recipebook.ServerPlaceRecipe;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.StackedItemContents;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.common.bridge.world.item.crafting.PlacementInfoBridge;
 import org.spongepowered.common.item.recipe.crafting.SpongeStackedItemContents;
 
@@ -74,14 +75,25 @@ public abstract class ServerPlaceRecipeMixin {
             : original.call();
     }
 
-    @ModifyArg(
+    @WrapOperation(
         method = "moveItemToGrid",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/entity/player/Inventory;findSlotMatchingCraftingIngredient(Lnet/minecraft/core/Holder;Lnet/minecraft/world/item/ItemStack;)I"
         )
     )
-    private ItemStack impl$adjustMatchingSlotFinder(final ItemStack craftInputStack) {
-        return this.impl$stackList.isEmpty() ? craftInputStack : this.impl$stackList.poll();
+    private int impl$adjustMatchingSlotFinder(
+        final Inventory instance, final Holder<Item> exemplaryItem, final ItemStack craftInputStack,
+        final Operation<Integer> original
+    ) {
+        if (this.impl$stackList.isEmpty()) {
+            return original.call(instance, exemplaryItem, craftInputStack);
+        } else {
+            final ItemStack input = this.impl$stackList.poll();
+            if (!craftInputStack.isEmpty() && !ItemStack.isSameItemSameComponents(craftInputStack, input)) {
+                return -1;
+            }
+            return original.call(instance, exemplaryItem, input);
+        }
     }
 }
