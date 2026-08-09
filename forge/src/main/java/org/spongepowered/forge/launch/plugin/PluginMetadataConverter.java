AutoArchives/spongepowered-sponge

Remove Container and Inheritable from plugin-meta

forge/src/main/java/org/spongepowered/forge/launch/plugin/PluginMetadataConverter.java
@@ -25,28 +25,50 @@
 package org.spongepowered.forge.launch.plugin;
 
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import net.minecraftforge.forgespi.language.IModFileInfo;
 import net.minecraftforge.forgespi.language.IModInfo;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.spongepowered.plugin.metadata.PluginMetadata;
+import org.spongepowered.plugin.metadata.builtin.InheritableMetadata;
 import org.spongepowered.plugin.metadata.builtin.StandardPluginMetadata;
 import org.spongepowered.plugin.metadata.model.PluginContributor;
 import org.spongepowered.plugin.metadata.model.PluginDependency;
 import org.spongepowered.plugin.metadata.model.PluginLinks;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpec;
 
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 
 public final class PluginMetadataConverter {
+    private static final VersionRange ANY_VERSION;
+
+    static {
+        try {
+            ANY_VERSION = VersionRange.createFromVersionSpec("*");
+        } catch (InvalidVersionSpecificationException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @SuppressWarnings("UnstableApiUsage")
     public static PluginMetadata modToPlugin(final ModInfo info) {
-        final StandardPluginMetadata.Builder builder = StandardPluginMetadata.builder();
-        builder
-            .id(info.getModId())
+        final List<IModFileInfo.LanguageSpec> loaders = info.getOwningFile().requiredLanguageLoaders();
+        final PluginLoaderSpec loaderSpec;
+        if (loaders.isEmpty()) {
+            loaderSpec = new PluginLoaderSpec("forge", PluginMetadataConverter.ANY_VERSION);
+        } else {
+            final VersionRange loaderVersion = loaders.getFirst().acceptedVersions();
+            loaderSpec = new PluginLoaderSpec("forge:" + loaders.getFirst().languageName(), loaderVersion == null ? PluginMetadataConverter.ANY_VERSION : loaderVersion);
+        }
+
+        final InheritableMetadata.Builder builder = InheritableMetadata.builder()
             .name(info.getDisplayName())
-            .version(info.getVersion().toString())
+            .version(info.getVersion())
+            .loader(loaderSpec)
             .description(info.getDescription())
-            .entrypoint("unknown")
             .addContributor(new PluginContributor(info.getConfigElement("authors").orElse("unknown").toString(), Optional.empty()))
             .properties(info.getModProperties());
 
@@ -63,7 +85,12 @@ public final class PluginMetadataConverter {
                 false
             ));
         }
-        return builder.build();
+
+        return StandardPluginMetadata.builder()
+            .id(info.getModId())
+            .entrypoint("unknown")
+            .override(builder.build())
+            .build();
     }
 
     private static PluginDependency.LoadOrder orderingToLoad(final IModInfo.Ordering ordering) {

neoforge/src/main/java/org/spongepowered/neoforge/launch/plugin/PluginMetadataConverter.java
@@ -25,27 +25,50 @@
 package org.spongepowered.neoforge.launch.plugin;
 
 import net.neoforged.fml.loading.moddiscovery.ModInfo;
+import net.neoforged.neoforgespi.language.IModFileInfo;
 import net.neoforged.neoforgespi.language.IModInfo;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.spongepowered.plugin.metadata.PluginMetadata;
+import org.spongepowered.plugin.metadata.builtin.InheritableMetadata;
 import org.spongepowered.plugin.metadata.builtin.StandardPluginMetadata;
 import org.spongepowered.plugin.metadata.model.PluginContributor;
 import org.spongepowered.plugin.metadata.model.PluginDependency;
 import org.spongepowered.plugin.metadata.model.PluginLinks;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpec;
 
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 
 public final class PluginMetadataConverter {
+    private static final VersionRange ANY_VERSION;
+
+    static {
+        try {
+            ANY_VERSION = VersionRange.createFromVersionSpec("*");
+        } catch (InvalidVersionSpecificationException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static PluginMetadata modToPlugin(final ModInfo info) {
-        final StandardPluginMetadata.Builder builder = StandardPluginMetadata.builder();
+        final List<IModFileInfo.LanguageSpec> loaders = info.getOwningFile().requiredLanguageLoaders();
+        final PluginLoaderSpec loaderSpec;
+        if (loaders.isEmpty()) {
+            loaderSpec = new PluginLoaderSpec("neoforge", PluginMetadataConverter.ANY_VERSION);
+        } else {
+            final VersionRange loaderVersion = loaders.getFirst().acceptedVersions();
+            loaderSpec = new PluginLoaderSpec("neoforge:" + loaders.getFirst().languageName(), loaderVersion == null ? PluginMetadataConverter.ANY_VERSION : loaderVersion);
+        }
+
+        final InheritableMetadata.Builder builder = InheritableMetadata.builder();
         builder
-            .id(info.getModId())
             .name(info.getDisplayName())
-            .version(info.getVersion().toString())
+            .version(info.getVersion())
+            .loader(loaderSpec)
             .description(info.getDescription())
-            .entrypoint("unknown")
             .addContributor(new PluginContributor(info.getConfigElement("authors").orElse("unknown").toString(), Optional.empty()))
             .properties(info.getModProperties());
 
@@ -62,7 +85,12 @@ public final class PluginMetadataConverter {
                 false
             ));
         }
-        return builder.build();
+
+        return StandardPluginMetadata.builder()
+            .id(info.getModId())
+            .entrypoint("unknown")
+            .override(builder.build())
+            .build();
     }
 
     private static PluginDependency.LoadOrder orderingToLoad(final IModInfo.Ordering ordering) {

src/main/java/org/spongepowered/common/launch/plugin/loader/PluginCandidateFactory.java
@@ -29,7 +29,7 @@ import org.spongepowered.plugin.Environment;
 import org.spongepowered.plugin.PluginLoader;
 import org.spongepowered.plugin.discovery.PluginResource;
 import org.spongepowered.plugin.metadata.PluginMetadata;
-import org.spongepowered.plugin.metadata.model.ContainerLoader;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpec;
 
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -49,7 +49,7 @@ public final class PluginCandidateFactory extends PluginServiceLoader {
     }
 
     public PluginCandidate create(final PluginResource resource, final PluginMetadata metadata) {
-        final ContainerLoader loaderSpec = metadata.container().loader();
+        final PluginLoaderSpec loaderSpec = metadata.loader();
         final PluginLoader loader = this.loaders.get(loaderSpec.name());
         if (loader == null) {
             throw new NoSuchElementException("Cannot find loader " + loaderSpec.name());

SpongePowered/Sponge

forge/src/main/java/org/spongepowered/forge/launch/plugin/PluginMetadataConverter.java
@@ -25,28 +25,50 @@
 package org.spongepowered.forge.launch.plugin;
 
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import net.minecraftforge.forgespi.language.IModFileInfo;
 import net.minecraftforge.forgespi.language.IModInfo;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.spongepowered.plugin.metadata.PluginMetadata;
+import org.spongepowered.plugin.metadata.builtin.InheritableMetadata;
 import org.spongepowered.plugin.metadata.builtin.StandardPluginMetadata;
 import org.spongepowered.plugin.metadata.model.PluginContributor;
 import org.spongepowered.plugin.metadata.model.PluginDependency;
 import org.spongepowered.plugin.metadata.model.PluginLinks;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpecification;
 
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 
 public final class PluginMetadataConverter {
+    private static final VersionRange ANY_VERSION;
+
+    static {
+        try {
+            ANY_VERSION = VersionRange.createFromVersionSpec("*");
+        } catch (InvalidVersionSpecificationException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @SuppressWarnings("UnstableApiUsage")
     public static PluginMetadata modToPlugin(final ModInfo info) {
-        final StandardPluginMetadata.Builder builder = StandardPluginMetadata.builder();
-        builder
-            .id(info.getModId())
+        final List<IModFileInfo.LanguageSpec> loaders = info.getOwningFile().requiredLanguageLoaders();
+        final PluginLoaderSpecification loaderSpec;
+        if (loaders.isEmpty()) {
+            loaderSpec = new PluginLoaderSpecification("forge", PluginMetadataConverter.ANY_VERSION);
+        } else {
+            final VersionRange loaderVersion = loaders.getFirst().acceptedVersions();
+            loaderSpec = new PluginLoaderSpecification("forge:" + loaders.getFirst().languageName(), loaderVersion == null ? PluginMetadataConverter.ANY_VERSION : loaderVersion);
+        }
+
+        final InheritableMetadata.Builder builder = InheritableMetadata.builder()
             .name(info.getDisplayName())
-            .version(info.getVersion().toString())
+            .version(info.getVersion())
+            .loader(loaderSpec)
             .description(info.getDescription())
-            .entrypoint("unknown")
             .addContributor(new PluginContributor(info.getConfigElement("authors").orElse("unknown").toString(), Optional.empty()))
             .properties(info.getModProperties());
 
@@ -63,7 +85,12 @@ public final class PluginMetadataConverter {
                 false
             ));
         }
-        return builder.build();
+
+        return StandardPluginMetadata.builder()
+            .id(info.getModId())
+            .entrypoint("unknown")
+            .override(builder.build())
+            .build();
     }
 
     private static PluginDependency.LoadOrder orderingToLoad(final IModInfo.Ordering ordering) {

neoforge/src/main/java/org/spongepowered/neoforge/launch/plugin/PluginMetadataConverter.java
@@ -25,27 +25,50 @@
 package org.spongepowered.neoforge.launch.plugin;
 
 import net.neoforged.fml.loading.moddiscovery.ModInfo;
+import net.neoforged.neoforgespi.language.IModFileInfo;
 import net.neoforged.neoforgespi.language.IModInfo;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.spongepowered.plugin.metadata.PluginMetadata;
+import org.spongepowered.plugin.metadata.builtin.InheritableMetadata;
 import org.spongepowered.plugin.metadata.builtin.StandardPluginMetadata;
 import org.spongepowered.plugin.metadata.model.PluginContributor;
 import org.spongepowered.plugin.metadata.model.PluginDependency;
 import org.spongepowered.plugin.metadata.model.PluginLinks;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpecification;
 
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 
 public final class PluginMetadataConverter {
+    private static final VersionRange ANY_VERSION;
+
+    static {
+        try {
+            ANY_VERSION = VersionRange.createFromVersionSpec("*");
+        } catch (InvalidVersionSpecificationException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static PluginMetadata modToPlugin(final ModInfo info) {
-        final StandardPluginMetadata.Builder builder = StandardPluginMetadata.builder();
+        final List<IModFileInfo.LanguageSpec> loaders = info.getOwningFile().requiredLanguageLoaders();
+        final PluginLoaderSpecification loaderSpec;
+        if (loaders.isEmpty()) {
+            loaderSpec = new PluginLoaderSpecification("neoforge", PluginMetadataConverter.ANY_VERSION);
+        } else {
+            final VersionRange loaderVersion = loaders.getFirst().acceptedVersions();
+            loaderSpec = new PluginLoaderSpecification("neoforge:" + loaders.getFirst().languageName(), loaderVersion == null ? PluginMetadataConverter.ANY_VERSION : loaderVersion);
+        }
+
+        final InheritableMetadata.Builder builder = InheritableMetadata.builder();
         builder
-            .id(info.getModId())
             .name(info.getDisplayName())
-            .version(info.getVersion().toString())
+            .version(info.getVersion())
+            .loader(loaderSpec)
             .description(info.getDescription())
-            .entrypoint("unknown")
             .addContributor(new PluginContributor(info.getConfigElement("authors").orElse("unknown").toString(), Optional.empty()))
             .properties(info.getModProperties());
 
@@ -62,7 +85,12 @@ public final class PluginMetadataConverter {
                 false
             ));
         }
-        return builder.build();
+
+        return StandardPluginMetadata.builder()
+            .id(info.getModId())
+            .entrypoint("unknown")
+            .override(builder.build())
+            .build();
     }
 
     private static PluginDependency.LoadOrder orderingToLoad(final IModInfo.Ordering ordering) {

src/main/java/org/spongepowered/common/launch/plugin/loader/PluginCandidateFactory.java
@@ -29,7 +29,7 @@ import org.spongepowered.plugin.Environment;
 import org.spongepowered.plugin.PluginLoader;
 import org.spongepowered.plugin.discovery.PluginResource;
 import org.spongepowered.plugin.metadata.PluginMetadata;
-import org.spongepowered.plugin.metadata.model.ContainerLoader;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpecification;
 
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -49,7 +49,7 @@ public final class PluginCandidateFactory extends PluginServiceLoader {
     }
 
     public PluginCandidate create(final PluginResource resource, final PluginMetadata metadata) {
-        final ContainerLoader loaderSpec = metadata.container().loader();
+        final PluginLoaderSpecification loaderSpec = metadata.loader();
         final PluginLoader loader = this.loaders.get(loaderSpec.name());
         if (loader == null) {
             throw new NoSuchElementException("Cannot find loader " + loaderSpec.name());